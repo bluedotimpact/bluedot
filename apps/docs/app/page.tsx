b62b46ca47363e301386cb3226d6a5d1d548ed1f
Adam Jones <domdomegg+git@gmail.com>
2023-12-06 10:05:34 +0000

    Rename repo -> bluedot

--- apps/docs/app/page.tsx
+++ apps/docs/app/page.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import { Card } from "@repo/ui/card";
-import { Code } from "@repo/ui/code";
+import { Card } from "@bluedot/ui/card";
+import { Code } from "@bluedot/ui/code";
 import styles from "./page.module.css";
-import { Button } from "@repo/ui/button";
+import { Button } from "@bluedot/ui/button";
 
 function Gradient({
   conic,
