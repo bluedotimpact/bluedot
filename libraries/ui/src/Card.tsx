4b695c935815c4009877d7f2b16891903f4eba88
Adam Jones <domdomegg+git@gmail.com>
2024-04-26 13:49:10 +0100

    ui: Delete Card component

--- libraries/ui/src/Card.tsx
+++ libraries/ui/src/Card.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { ButtonProps, LinkOrButton } from './Button';
+import { LinkOrButton, LinkOrButtonProps } from './LinkOrButton';
 
 export type CardProps = React.PropsWithChildren<{
   className?: string
@@ -13,7 +13,7 @@ export const Card: React.FC<CardProps> = ({ children, className }) => {
   );
 };
 
-export const CardButton: React.FC<ButtonProps> = ({ className, ...rest }) => {
+export const CardButton: React.FC<LinkOrButtonProps> = ({ className, ...rest }) => {
   return (
     <LinkOrButton className={clsx('border border-neutral-500 rounded px-8 pb-4 text-black transition-all duration-200 inline-block cursor-pointer data-[hovered]:border-bluedot-normal data-[hovered]:bg-bluedot-lighter data-[focus-visible]:border-bluedot-normal data-[focus-visible]:bg-bluedot-lighter data-[pressed]:border-bluedot-normal data-[pressed=true]:bg-bluedot-normal data-[pressed=true]:text-white outline-none [text-align:inherit]', className)} {...rest} />
   );
