5537a076a59630b51a45705d8c2581a1bee3ec39
Adam Jones <domdomegg+git@gmail.com>
2024-05-19 16:09:16 +0100

    login-account-proxy: Better handle slightly malformed payloads from Bubble

--- apps/login-account-proxy/src/pages/api/public/submit.ts
+++ apps/login-account-proxy/src/pages/api/public/submit.ts
@@ -15,8 +15,9 @@ export default apiRoute(async (
   res: NextApiResponse,
 ) => {
   // TODO: better schema validation
-  const data = req.body as SubmitRequest;
+  const data = (typeof req.body === 'string' ? JSON.parse(req.body) : req.body) as SubmitRequest;
   if (typeof data.newEmail !== 'string' || typeof data.password !== 'string' || typeof data.secret !== 'string') {
+    console.error(`Invalid payload: ${JSON.stringify(req.body)}`);
     res.status(400).send({ type: 'error', message: 'Invalid payload' });
     return;
   }
