396801a08923a0b2554a6d06e82c1d13f05c31e2
Adam Jones <domdomegg+git@gmail.com>
2025-01-17 13:47:58 +0000

    Fix lint

--- libraries/ui/src/CourseCard.tsx
+++ libraries/ui/src/CourseCard.tsx
@@ -5,55 +5,64 @@ export type CourseCardProps = React.PropsWithChildren<{
   className?: string,
   title: string,
   description: string,
-  courseType: 'Crash course' | 'Self-paced' | 'In-depth course',
+  courseType: CourseType,
   cardType?: 'Featured' | 'Regular',
   image?: string
 }>;
 
-const courseLength = (courseType: 'Crash course' | 'Self-paced' | 'In-depth course') => {
-  switch(courseType) {
+type CourseType = 'Crash course' | 'Self-paced' | 'In-depth course';
+
+const courseLength = (courseType: CourseType) => {
+  switch (courseType) {
     case 'Crash course':
       return '5 days';
     case 'Self-paced':
       return '12 weeks';
     case 'In-depth course':
       return '12 weeks';
+    default:
+      throw new Error(`Invalid course type: ${courseType satisfies never}`);
   }
-}
+};
 
-export const CourseCard: React.FC<CourseCardProps> = ({ children, className, title, description, courseType = 'Crash course', cardType = 'Regular', image }) => {
+export const CourseCard: React.FC<CourseCardProps> = ({
+  children, className, title, description, courseType = 'Crash course', cardType = 'Regular', image,
+}) => {
   return (
-    cardType === 'Featured' ?
+    cardType === 'Featured'
+      ? (
         <div className={clsx('border border-gray-300 rounded-lg px-8 pb-4 max-w-[323px]', className)}>
-            <p className="uppercase font-semibold">Featured course</p>
-                <img src={image} alt="Course Card Placeholder" className="w-full h-[165px] object-cover rounded-lg" />
-                <h3 className="text-bluedot-normal text-[24px] mb-4 font-serif font-extrabold leading-none">{title}</h3>
-                <p className="text-bluedot-darker text-md mb-4 line-clamp-6">{description}</p>
-                <div className="flex justify-between items-center">
-                    <p className="text-left text-xs">
-                        {courseLength(courseType)}
-                    </p>
-                    <p className="text-right text-xs border border-bluedot-lighter rounded-xl px-4 py-2 font-semibold">
-                        {courseType}
-                    </p>
-                </div>
-            {children}
+          <p className="uppercase font-semibold">Featured course</p>
+          <img src={image} alt="Course Card Placeholder" className="w-full h-[165px] object-cover rounded-lg" />
+          <h3 className="text-bluedot-normal text-[24px] mb-4 font-serif font-extrabold leading-none">{title}</h3>
+          <p className="text-bluedot-darker text-md mb-4 line-clamp-6">{description}</p>
+          <div className="flex justify-between items-center">
+            <p className="text-left text-xs">
+              {courseLength(courseType)}
+            </p>
+            <p className="text-right text-xs border border-bluedot-lighter rounded-xl px-4 py-2 font-semibold">
+              {courseType}
+            </p>
+          </div>
+          {children}
         </div>
-    :
+      )
+      : (
         <div className={clsx('border border-gray-300 rounded-xl px-6 pb-6 max-w-[323px]', className)}>
-            <img src={image} alt="Course Card Placeholder" className="w-full h-[165px] object-cover rounded-xl mt-6 mb-12" />
-            <h3 className="text-bluedot-normal text-[24px] mb-4 font-serif font-extrabold leading-none">{title}</h3>
-            <p className="text-bluedot-darker text-md mb-4 line-clamp-4">{description}</p>
-            <div className="flex justify-between items-center">
-                <p className="text-left text-xs">
-                    {courseLength(courseType)}
-                </p>
-                <p className="text-right text-xs border border-bluedot-lighter rounded-xl px-4 py-2 font-semibold">
-                    {courseType}
-                </p>
-            </div>
-            {children}
+          <img src={image} alt="Course Card Placeholder" className="w-full h-[165px] object-cover rounded-xl mt-6 mb-12" />
+          <h3 className="text-bluedot-normal text-[24px] mb-4 font-serif font-extrabold leading-none">{title}</h3>
+          <p className="text-bluedot-darker text-md mb-4 line-clamp-4">{description}</p>
+          <div className="flex justify-between items-center">
+            <p className="text-left text-xs">
+              {courseLength(courseType)}
+            </p>
+            <p className="text-right text-xs border border-bluedot-lighter rounded-xl px-4 py-2 font-semibold">
+              {courseType}
+            </p>
+          </div>
+          {children}
         </div>
+      )
   );
 };
 
