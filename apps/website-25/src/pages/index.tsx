dfee0daddb69265827bc6124d890380150ff0fc4
tarinrickett <trickett96@gmail.com>
2025-01-22 12:17:14 +0000

    Move page sections into their own components

--- apps/website-25/src/pages/index.tsx
+++ apps/website-25/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import {
-  CourseCard,
   HeroSection,
-  Section,
 } from '@bluedot/ui';
+import CourseSection from '../components/homepage/CourseSection';
+import GraduateSection from '../components/homepage/GraduateSection';
 
 const HomePage = () => {
   return (
@@ -17,36 +17,8 @@ const HomePage = () => {
           <img className="hero-section__logo-text w-52 mr-9" src="/Bluedot_Impact_Logo_Text-Only.svg" alt="BlueDot Impact" />
         </div>
       </HeroSection>
-
-      {/* COURSES */}
-      <Section title="Our courses" subtitle="We run inclusive, blended learning courses that cater to various expertise levels and time availability">
-        <div className="flex flex-row gap-4">
-          <CourseCard
-            title="Alignment Fast Track"
-            description="AI systems are rapidly becoming more capable and more general. Despite AI’s potential to radically improve human society, there are still open questions about how we build AI systems that are controllable, aligned with our intentions and interpretable."
-            courseType="Crash course"
-            image="/images/intro-course.png"
-          />
-          <CourseCard
-            title="Governance Fast-Track"
-            description="Despite AI’s potential to radically improve human society, there are still active debates about how we will wield the AI systems of today and tomorrow. The rise of this powerful technology demands a thoughtful approach to its governance and regulation."
-            courseType="Crash course"
-            image="/images/governance-course.jpg"
-          />
-          <CourseCard
-            title="AI Alignment"
-            description="AI systems are rapidly becoming more capable and more general. Despite AI’s potential to radically improve human society, there are still open questions about how we build AI systems that are controllable, aligned with our intentions and interpretable."
-            courseType="In-depth course"
-            image="/images/alignment-course.png"
-          />
-          <CourseCard
-            title="AI Governance"
-            description="The rise of any powerful technology demands a thoughtful approach to its governance and regulation. There has been increasing interest in how AI governance can and should mitigate extreme risks from AI, but it can be difficult to get up to speed on research and ideas in this area."
-            courseType="In-depth course"
-            image="/images/governance-course.jpg"
-          />
-        </div>
-      </Section>
+      <GraduateSection />
+      <CourseSection />
     </div>
   );
 };
