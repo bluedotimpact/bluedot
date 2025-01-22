a2480a77f3b819d1e3dc40d07e2c54ed355566e6
tarinrickett <trickett96@gmail.com>
2025-01-22 14:15:09 +0000

    Add StorySection (#71)

--- libraries/ui/src/HeroSection.tsx
+++ libraries/ui/src/HeroSection.tsx
@@ -17,7 +17,7 @@ export const HeroSection: React.FC<HeroSectionProps> = ({
           <h1 className="hero-section__title text-bluedot-darker text-[48px] text-center mb-4 font-serif font-extrabold leading-none">{title}</h1>
         )}
         {subtitle && (
-          <h2 className="hero-section__subtitle text-bluedot-darker text-xl text-center">{subtitle}</h2>
+          <h2 className="hero-section__subtitle text-bluedot-darker text-xl font-normal text-center">{subtitle}</h2>
         )}
       </div>
     </div>
