d8e757be791a94e553d102001dcf7a3bb7fd0092
tarinrickett <trickett96@gmail.com>
2025-01-22 12:00:43 +0000

    Add logo to homepage hero

--- apps/website-25/src/pages/index.tsx
+++ apps/website-25/src/pages/index.tsx
@@ -8,10 +8,14 @@ const HomePage = () => {
   return (
     <div>
       <HeroSection
+        className="!items-end pb-14"
         title="The expertise you need to shape safe AI "
         subtitle="We run the world's most trusted AI Safety educational courses, career services and support community. Our programs are developed in collaboration with AI Safety world experts."
       >
-        <p className="text-center">BlueDot Impact</p>
+        <div className="hero-section__logo-container flex flex-col items-center gap-7 mb-3">
+          <img className="hero-section__logo-icon w-20" src="/Bluedot_Impact_Icon.svg" alt="BlueDot Impact" />
+          <img className="hero-section__logo-text w-52 mr-9" src="/Bluedot_Impact_Logo_Text-Only.svg" alt="BlueDot Impact" />
+        </div>
       </HeroSection>
 
       {/* COURSES */}
