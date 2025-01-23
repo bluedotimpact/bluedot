60121dc33876ddf28ae561b19205b64b3037ae5a
Tim Sankara <tim.sankara@gmail.com>
2025-01-23 03:12:08 +0300

    test: add Tag component tests, update border to -lg

---
+++ libraries/ui/src/Tag.test.tsx
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import { Tag } from './Tag';
+
+describe('Tag', () => {
+  test('renders default as expected', () => {
+    const { container } = render(<Tag label="Basic tag" />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with custom className', () => {
+    const { container } = render(
+      <Tag
+        label="Custom tag"
+        className="custom-class"
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders as button when onClick provided', () => {
+    const { container } = render(
+      <Tag
+        label="Interactive tag"
+        onClick={() => {}}
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
