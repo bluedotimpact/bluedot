d44b7b76703d8f77733eecf7b3e91b1f95eadde6
Tim Sankara <tim.sankara@gmail.com>
2025-01-23 22:26:51 +0300

    Remove basestyling from Tag component

--- libraries/ui/src/Tag.tsx
+++ libraries/ui/src/Tag.tsx
@@ -10,14 +10,12 @@ export const Tag: React.FC<TagProps> = ({
   className,
   label,
 }) => {
-  const baseStyles = 'tag inline-flex items-center px-4 py-1.5 rounded-lg border text-xs font-semibold border-bluedot-lighter text-gray-800 text-sm font-medium transition-colors duration-200';
-
   return (
     <span
       role="status"
       className={clsx(
         'tag inline-flex items-center px-4 py-1.5 rounded-lg text-xs font-semibold border border-bluedot-light',
-        className
+        className,
       )}
     >
       {label}
