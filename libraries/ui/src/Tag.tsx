0189b6f92f6af31d92532eabcca3e2e3361ed921
Tim Sankara <tim.sankara@gmail.com>
2025-01-23 22:20:28 +0300

    Update libraries/ui/src/Tag.tsx
    Co-authored-by: tarinrickett <trickett96@gmail.com>

--- libraries/ui/src/Tag.tsx
+++ libraries/ui/src/Tag.tsx
@@ -16,9 +16,8 @@ export const Tag: React.FC<TagProps> = ({
     <span
       role="status"
       className={clsx(
-        'tag__label',
-        baseStyles,
-        className,
+        'tag inline-flex items-center px-4 py-1.5 rounded-lg text-xs font-semibold border border-bluedot-light',
+        className
       )}
     >
       {label}
