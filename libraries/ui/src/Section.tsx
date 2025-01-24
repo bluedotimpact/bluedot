06f3049b3ceb6f5c9ac298de21a5105614a0a6ac
Olivia McGoffin <oliviamcgoffin@gmail.com>
2025-01-24 13:16:49 -0800

    Update Section component to include optional CTALinkOrButton

--- libraries/ui/src/Section.tsx
+++ libraries/ui/src/Section.tsx
@@ -1,27 +1,50 @@
+import React from 'react';
 import clsx from 'clsx';
+import { CTALinkOrButton } from './CTALinkOrButton';
 
 export type SectionProps = React.PropsWithChildren<{
   className?: string,
   title: string,
-  subtitle?: string
+  subtitle?: string,
+  ctaText?: string,
+  ctaUrl?: string,
 }>;
 
 export const Section: React.FC<SectionProps> = ({
-  className, title, subtitle, children,
+  className, title, subtitle, ctaText, ctaUrl, children,
 }) => {
   return (
     <div className={clsx('section mx-16 my-8 max-w-full overflow-hidden', className)}>
-      <div className="section__heading ml-4">
-        <h2 className="section__title text-bluedot-normal text-[48px] mb-4 font-serif font-extrabold leading-none
-            relative after:content-[''] after:absolute after:top-1/2 after:ml-3 after:h-[2px] after:w-full after:bg-bluedot-normal"
-        >
-          {title}
-        </h2>
-        {subtitle && (
-          <p className="section__subtitle text-bluedot-darker text-md mb-4">{subtitle}</p>
+      <div className="section__header ml-4 flex justify-between items-center gap-4">
+        <div className="section__content flex-1">
+          <h2 className={clsx(
+            'section__title text-bluedot-normal text-[48px] mb-4 font-serif font-extrabold leading-none relative',
+            "after:content-[''] after:absolute after:top-1/2 after:translate-y-[-50%] after:ml-8 after:h-[2px] after:bg-bluedot-normal",
+            ctaText && ctaUrl
+              ? 'after:w-[80%]'
+              : 'after:w-[100%]',
+          )}
+          >
+            {title}
+          </h2>
+          {subtitle && (
+            <p className="section__subtitle text-bluedot-darker text-md mb-4">{subtitle}</p>
+          )}
+        </div>
+        {ctaText && ctaUrl && (
+          <CTALinkOrButton
+            variant="secondary"
+            url={ctaUrl}
+            withChevron
+            className="section__cta shrink-0"
+          >
+            {ctaText}
+          </CTALinkOrButton>
         )}
       </div>
-      {children}
+      <div className="section__body">
+        {children}
+      </div>
     </div>
   );
 };
