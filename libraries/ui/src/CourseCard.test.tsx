fb45ea6c3e820b883a49291ecdf212d9775db2e9
Tim Sankara <tim.sankara@gmail.com>
2025-01-24 12:22:18 +0300

    Add CourseCard test

---
+++ libraries/ui/src/CourseCard.test.tsx
@@ -0,0 +1,37 @@
+import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import { CourseCard } from './CourseCard';
+
+describe('CourseCard', () => {
+  const baseProps = {
+    title: 'Introduction to Testing',
+    description: 'Learn proper testing techniques',
+    courseType: 'Crash course',
+    image: '/test-image.jpg'
+  };
+
+  test('renders default as expected', () => {
+    const { container } = render(<CourseCard {...baseProps} />);
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with optional args', () => {
+    const { container } = render(
+      <CourseCard
+        {...baseProps}
+        className="custom-class"
+        cardType="Featured"
+      />
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with optional yield', () => {
+    const { container } = render(
+      <CourseCard {...baseProps}>
+        <button>Enroll Now</button>
+      </CourseCard>
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
