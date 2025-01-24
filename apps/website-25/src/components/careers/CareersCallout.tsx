5564cf2d7f37f8f30e58647d4cae218efdfcdcd4
Tim Sankara <tim.sankara@gmail.com>
2025-01-24 08:30:30 +0300

    Update bg gradient on CareerCallout component

--- apps/website-25/src/components/careers/CareersCallout.tsx
+++ apps/website-25/src/components/careers/CareersCallout.tsx
@@ -7,7 +7,7 @@ const Callout: React.FC = () => {
   const ctaLink = '/careers';
 
   return (
-    <div className="bg-[radial-gradient(circle_at_center,#6687FF_0%,white_100%)] rounded-lg p-8 flex flex-col items-center">
+    <div className="bg-[radial-gradient(ellipse_at_30%,_#fff_0%,_#6687ff_100%)] rounded-lg p-8 m-8 flex flex-col items-center">
       <h2 className="text-black font-bold text-2xl mb-4 text-center">{title}</h2>
       <CTAButton variant="primary" onClick={() => window.open(ctaLink, '_blank')} withChevron>
         {ctaLabel}
