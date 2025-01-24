b85aa53a36be61bc4d2f80ffc3a65fd5f2169794
Olivia McGoffin <oliviamcgoffin@gmail.com>
2025-01-24 13:20:57 -0800

    Update Section test to include optional CTALinkOrButton

--- libraries/ui/src/Section.test.tsx
+++ libraries/ui/src/Section.test.tsx
@@ -8,7 +8,7 @@ describe('Section', () => {
     expect(container).toMatchSnapshot();
   });
 
-  test('renders with optional args', () => {
+  test('renders with optional subtitle', () => {
     const { container } = render(
       <Section
         title="This is the title"
@@ -18,9 +18,34 @@ describe('Section', () => {
     expect(container).toMatchSnapshot();
   });
 
-  test('renders with optional yield', () => {
+  test('renders with children content', () => {
     const { container } = render(
       <Section title="This is the title">
+        <p>This is the yield</p>
+      </Section>,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with CTA link', () => {
+    const { container } = render(
+      <Section
+        title="This is the title"
+        ctaText="Click me"
+        ctaUrl="/some-path"
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with all optional props', () => {
+    const { container } = render(
+      <Section
+        title="This is the title"
+        subtitle="This is the subtitle"
+        ctaText="Click me"
+        ctaUrl="/some-path"
+      >
         <p>This is the yield</p>
       </Section>,
     );
