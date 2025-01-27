5841eae49895a22b1eab28c155d60779840f7f0c
Olivia <oliviamcgoffin@gmail.com>
2025-01-27 09:24:40 -0800

    Update Section component to include optional CTALinkOrButton (#99)
    * Update Section component to include optional CTALinkOrButton
    
    * Update Section test to include optional CTALinkOrButton. Remove snapshot for CTA link

--- libraries/ui/src/Section.test.tsx
+++ libraries/ui/src/Section.test.tsx
@@ -1,29 +1,43 @@
-import { describe, expect, test } from 'vitest';
-import { render } from '@testing-library/react';
+import {
+  describe, expect, test, afterEach,
+} from 'vitest';
+import { render, cleanup } from '@testing-library/react';
 import { Section } from './Section';
 
 describe('Section', () => {
+  afterEach(() => {
+    cleanup();
+  });
+
   test('renders default as expected', () => {
     const { container } = render(<Section title="This is the title" />);
     expect(container).toMatchSnapshot();
   });
 
-  test('renders with optional args', () => {
+  test('renders with all optional props', () => {
     const { container } = render(
       <Section
         title="This is the title"
         subtitle="This is the subtitle"
-      />,
-    );
-    expect(container).toMatchSnapshot();
-  });
-
-  test('renders with optional yield', () => {
-    const { container } = render(
-      <Section title="This is the title">
+        ctaText="Click me"
+        ctaUrl="/some-path"
+      >
         <p>This is the yield</p>
       </Section>,
     );
     expect(container).toMatchSnapshot();
+  });
+
+  test('renders CTA link with correct attributes', () => {
+    const { getByTestId } = render(
+      <Section
+        title="This is the title"
+        ctaText="Click me"
+        ctaUrl="/some-path"
+      />,
+    );
+
+    const ctaLink = getByTestId('cta-link');
+    expect(ctaLink.getAttribute('href')).toBe('/some-path');
   });
 });
