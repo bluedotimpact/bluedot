a732c352c8d682009c1ec3ca81a25aec0dcda7d3
Olivia <oliviamcgoffin@gmail.com>
2025-01-27 10:38:33 -0800

    Add Explore Dropdown Menu (#100)
    * Update Nav to use tailwind styles, make courses required, and update snaps

--- apps/website-25/src/pages/_app.tsx
+++ apps/website-25/src/pages/_app.tsx
@@ -5,13 +5,27 @@ import Head from 'next/head';
 import dynamic from 'next/dynamic';
 import { Footer, Nav } from '@bluedot/ui';
 
+// TODO: add routing to courses when AISafetyFundamentals course is integrated, i.e.'/courses/intro-transformative-ai
+const courses = [
+  { title: 'Intro to Transformative AI', href: 'https://aisafetyfundamentals.com/intro-to-tai/' },
+  { title: 'AI Alignment Fast-Track', href: 'https://aisafetyfundamentals.com/alignment-fast-track/' },
+  { title: 'AI Alignment In-Depth', href: 'https://aisafetyfundamentals.com/alignment/' },
+  { title: 'AI Governance Fast-Track', href: 'https://aisafetyfundamentals.com/governance-fast-track/' },
+  { title: 'Economics of Transformative AI Fast-Track', href: 'https://aisafetyfundamentals.com/economics-of-tai-fast-track/', isNew: true },
+];
+
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   return (
     <>
       <Head>
         <title>BlueDot Impact</title>
       </Head>
-      <Nav logo="/images/logo/BlueDot_Impact_Logo.svg">
+      <Nav
+        logo="/images/logo/BlueDot_Impact_Logo.svg"
+        courses={courses}
+      >
+        <div>Support us</div>
+        {/* TODO: add support us page */}
         <a href="/about">About</a>
         <a href="/careers">Join us</a>
       </Nav>
--- libraries/ui/src/Nav.test.tsx
+++ libraries/ui/src/Nav.test.tsx
@@ -2,16 +2,15 @@ import { describe, expect, test } from 'vitest';
 import { render } from '@testing-library/react';
 import { Nav } from './Nav';
 
-describe('Footer', () => {
-  test('renders default as expected', () => {
-    const { container } = render(<Nav />);
-    expect(container).toMatchSnapshot();
-  });
-
-  test('renders with optional args', () => {
+describe('Nav', () => {
+  test('renders with courses', () => {
     const { container } = render(
       <Nav
         logo="logo.png"
+        courses={[
+          { title: 'Course 1', href: '/course1' },
+          { title: 'Course 2', href: '/course2', isNew: true },
+        ]}
       />,
     );
     expect(container).toMatchSnapshot();
@@ -19,7 +18,11 @@ describe('Footer', () => {
 
   test('renders with optional yield', () => {
     const { container } = render(
-      <Nav>
+      <Nav
+        courses={[
+          { title: 'Course 1', href: '/course1' },
+        ]}
+      >
         <p>This is the yield</p>
       </Nav>,
     );
