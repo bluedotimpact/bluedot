6dc25844d98be6d9135acd1e85e7e12dec79841c
Olivia <oliviamcgoffin@gmail.com>
2025-01-27 09:45:34 -0800

    Update TeamSection to use Card component (#98)
    * Legacy CardButton in use in frontend-example pages
    
    * Update CTALinkOrButton styles, test to render link, update snaps

--- apps/website-25/src/components/about/TeamSection.tsx
+++ apps/website-25/src/components/about/TeamSection.tsx
@@ -1,40 +1,68 @@
-import { Section } from '@bluedot/ui';
+import { Card, Section } from '@bluedot/ui';
+
+const teamMembers = [
+  {
+    imageSrc: '/images/team/adam.jpg',
+    name: 'Adam Jones',
+    role: 'AI Safety',
+    linkedInUrl: 'https://www.linkedin.com/in/domdomegg/',
+  },
+  {
+    imageSrc: '/images/team/dewi.jpg',
+    name: 'Dewi Erwan',
+    role: 'CEO',
+    linkedInUrl: 'https://www.linkedin.com/in/dewierwan/',
+  },
+  {
+    imageSrc: '/images/team/josh.jpg',
+    name: 'Josh Landes',
+    role: 'Community Manager',
+    linkedInUrl: 'https://linkedin.com/in/josh-landes12',
+  },
+  {
+    imageSrc: '/images/team/lilian.jpg',
+    name: 'Li-Lian Ang',
+    role: 'Product',
+    linkedInUrl: 'https://linkedin.com/in/anglilian',
+  },
+  {
+    imageSrc: '/images/team/tarin.jpg',
+    name: 'Tarin Rickett',
+    role: 'Engineering',
+    linkedInUrl: 'https://linkedin.com/in/tarinrickett/',
+  },
+  {
+    imageSrc: '/images/team/vio.jpg',
+    name: 'Viorica Gheorghita',
+    role: 'Product',
+    linkedInUrl: 'https://www.linkedin.com/in/vioricagheorghita/',
+  },
+  {
+    imageSrc: '/images/team/will.jpg',
+    name: 'Will Saunter',
+    role: 'Co-founder',
+    linkedInUrl: 'https://linkedin.com/in/will-saunter',
+  },
+];
 
 const TeamSection = () => {
   return (
-    <Section className="team-section" title="Our team">
-      <div className="team-section__container flex flex-row flex-wrap mt-16 gap-4">
-        <TeamMember name="Adam Jones" title="AI Safety" image="/images/team/adam.jpg" profileUrl="https://www.linkedin.com/in/domdomegg/" />
-        <TeamMember name="Dewi Erwan" title="CEO" image="/images/team/dewi.jpg" profileUrl="https://www.linkedin.com/in/dewierwan/" />
-        <TeamMember name="Josh Landes" title="Community Manager" image="/images/team/josh.jpg" profileUrl="https://www.linkedin.com/in/josh-landes12/" />
-        <TeamMember name="Li-Lian Ang" title="Product" image="/images/team/lilian.jpg" profileUrl="https://www.linkedin.com/in/anglilian/" />
-        <TeamMember name="Tarin Rickett" title="Engineering" image="/images/team/tarin.jpg" profileUrl="https://www.linkedin.com/in/tarin-rickett/" />
-        <TeamMember name="Viorica  Gheroghita" title="Product" image="/images/team/vio.jpg" profileUrl="https://www.linkedin.com/in/vioricagheorghita/" />
-        <TeamMember name="Will Saunter" title="Co-founder" image="/images/team/will.jpg" profileUrl="https://www.linkedin.com/in/will-saunter/" />
+    <Section className="team" title="Our team">
+      <div className="team__grid flex flex-row flex-wrap mt-16 gap-4 max-w-[1400px] mx-auto">
+        {/* TODO: 01/27 Migrate this max-w-[1400px] check to Section definition or global.css when we hear back from UX  */}
+        {teamMembers.map((member) => (
+          <Card
+            key={member.name}
+            imageSrc={member.imageSrc}
+            title={member.name}
+            subtitle={member.role}
+            ctaUrl={member.linkedInUrl}
+            className="team__card"
+            imageClassName="team__card-image"
+          />
+        ))}
       </div>
     </Section>
-  );
-};
-
-const TeamMember = ({
-  name, title, image, profileUrl,
-}: { name: string, title: string, image?: string, profileUrl?: string }) => {
-  return (
-    <div className="team-member pb-6 w-[323px]">
-      <img src={image || '/images/team/tarin.jpg'} alt="Course Card Placeholder" className="team-member__image w-full h-[223px] object-cover rounded-xl mb-3" />
-      <h2 className="team-member__name">{name}</h2>
-      <p className="team-member__title mb-4">{title}</p>
-      <div className="team-member__links flex justify-between items-center">
-        {profileUrl && (
-          <a
-            href={profileUrl}
-            className="team-member__link text-center text-xs text-bluedot-normal border border-bluedot-normal rounded-lg px-4 py-2 font-semibold"
-          >
-            LinkedIn
-          </a>
-        )}
-      </div>
-    </div>
   );
 };
 
--- libraries/ui/src/Card.tsx
+++ libraries/ui/src/Card.tsx
@@ -6,6 +6,8 @@ interface CardProps {
   title: string;
   subtitle?: string;
   ctaUrl?: string;
+  className?: string;
+  imageClassName?: string;
 }
 
 export const Card: React.FC<CardProps> = ({
@@ -13,19 +15,21 @@ export const Card: React.FC<CardProps> = ({
   title,
   subtitle,
   ctaUrl,
+  className = '',
+  imageClassName = '',
 }) => {
   return (
-    <div className="card w-80 h-80 flex flex-col items-start">
-      <div className="card__image-container w-full h-48 mb-8">
+    <div className={`card flex flex-col items-start ${className}`}>
+      <div className="card__image-container w-[323px] h-[223px] mb-3">
         <img
-          className="card__image w-full h-full object-cover rounded-2xl"
+          className={`card__image w-full h-full object-cover rounded-2xl ${imageClassName}`}
           src={imageSrc}
           alt={`${title}`}
         />
       </div>
 
-      <div className="card__content flex flex-col gap-8">
-        <div className="card__text space-y-1">
+      <div className="card__content flex flex-col gap-12">
+        <div className="card__text">
           <h2 className="card__title text-2xl font-semibold text-bluedot-darker">{title}</h2>
           {subtitle && (<p className="card__subtitle text-base text-black">{subtitle}</p>)}
         </div>
