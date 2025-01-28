d3a85b3c5c6274b5626d1efbeb103f784cb17916
Jan Raasch <jan@janraasch.com>
2025-01-28 16:52:08 +0100

    Add storybook app for library/ui components (#108)
    * Run `npx storybook@latest init` in `apps/storybook`
    
    * Add `@bluedot/ui` to `apps/storybook`
    
    * Create `Tag.stories.tsx` & use `@bluedot` default configs
    
    * Eject `next` & `storybook-next` to get styling to work
    
    * Remove unused `globals.css`
    
    * Add `lint`-script
    
    * Remove example stories
    
    See https://github.com/bluedotimpact/bluedot/pull/108#issuecomment-2618804214
    
    * Add `website-25` path to `config.stories`
    
    Co-authored-by: tarinrickett <trickett96@gmail.com>
    
    * Add "autodocs" to `cSpell.words` & order alphabetically
    
    * Add `@storybook/react` to `website-25`-app
    
    * Add `IntroSection`-story
    
    * Revert "Add `IntroSection`-story"
    
    This reverts commit dc377de6fd6a6c24b5e29a08474a5b458e463f50.
    
    * Revert "Add `@storybook/react` to `website-25`-app"
    
    This reverts commit 7269f0a7e8858b27bf1d775177b09b873f3157bf.
    
    * Include only `libraries/ui`-stories (for now :))
    
    See https://github.com/bluedotimpact/bluedot/pull/108#discussion_r1932322082
    
    * Add missing `import React from 'react';`-statements to `ui`-components
    
    * Remove onboarding
    
    * Remove `@chromatic-com/storybook`
    
    * Add `README.md`
    
    ---------
    
    Co-authored-by: tarinrickett <trickett96@gmail.com>

--- libraries/ui/src/CourseCard.tsx
+++ libraries/ui/src/CourseCard.tsx
@@ -1,3 +1,4 @@
+import React from 'react';
 import clsx from 'clsx';
 import { LinkOrButton, LinkOrButtonProps } from './legacy/LinkOrButton';
 import { Tag } from './Tag';
--- libraries/ui/src/Nav.tsx
+++ libraries/ui/src/Nav.tsx
@@ -1,5 +1,5 @@
 import clsx from 'clsx';
-import { useState } from 'react';
+import React, { useState } from 'react';
 import { LinkOrButton, LinkOrButtonProps } from './legacy/LinkOrButton';
 
 export type NavProps = React.PropsWithChildren<{
