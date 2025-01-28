b76a31ae27ea468dc704611f24057918cb81a3dd
tarinrickett <trickett96@gmail.com>
2025-01-28 14:26:55 +0000

    Misc. updates to adopt latest changes

--- apps/website-25/src/components/careers/CareersSection.tsx
+++ apps/website-25/src/components/careers/CareersSection.tsx
@@ -4,19 +4,19 @@ const CareersSection = () => {
   return (
     <Section className="careers-section" title="Careers at BlueDot Impact">
       <div className="careers-section__container flex flex-col gap-8 my-14">
-        <JobListing title="AI Alignment Project Judge" />
-        <JobListing title="Course Operations Specialist" />
-        <JobListing title="Software Engineering Contractor" />
-        <JobListing title="Economics of Transformative AI Teaching Fellow" />
+        <JobListing title="AI Alignment Project Judge" url="https://bluedot.org/ai-alignment-project-judge/" isExternalUrl />
+        <JobListing title="Course Operations Specialist" url="https://bluedot.org/ai-safety-course-operations/" isExternalUrl />
+        <JobListing title="Software Engineering Contractor" url="https://bluedot.org/swe-contractor/" isExternalUrl />
+        <JobListing title="Economics of Transformative AI Teaching Fellow" url="https://bluedot.org/economics-of-tai-teaching-fellow/" isExternalUrl />
       </div>
     </Section>
   );
 };
 
 const JobListing = ({
-  title, location = 'London, Remote', type = 'Permanent', now,
+  title, location = 'London, Remote', type = 'Permanent', now, url, isExternalUrl = false,
 }: {
-  title: string, location?: string, type?: string, now?: boolean
+  title: string, location?: string, type?: string, now?: boolean, url?: string, isExternalUrl?: boolean
 }) => {
   return (
     <div className={
@@ -31,6 +31,8 @@ const JobListing = ({
         className="careers-section__cta-button"
         variant="secondary"
         withChevron
+        url={url}
+        isExternalUrl={isExternalUrl}
       >
         Apply now
       </CTALinkOrButton>
