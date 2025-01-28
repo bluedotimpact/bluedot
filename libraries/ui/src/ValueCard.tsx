9fc9962baadb6ae7edfd40936205f4825902cbdf
Olivia McGoffin <oliviamcgoffin@gmail.com>
2025-01-28 13:56:39 -0800

    Add ValueCard component and icons

---
+++ libraries/ui/src/ValueCard.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+
+interface ValueCardProps {
+  icon: string;
+  title: string;
+  description: string;
+}
+
+export const ValueCard: React.FC<ValueCardProps> = ({ icon, title, description }) => {
+  return (
+    <div className="value-card w-[437px] h-[296px] rounded-2xl border p-6 flex flex-col gap-[27px]">
+      <div className="value-card__icon-wrapper w-[50px] h-[50px] flex items-center justify-center">
+        <img
+          src={icon}
+          alt={title}
+          className="value-card__icon"
+        />
+      </div>
+      <h3 className="value-card__title font-reckless text-2xl font-[450] leading-[26.9px] text-left text-bluedot-normal">
+        {title}
+      </h3>
+      <p className="value-card__description font-roobert text-base font-normal leading-[20.22px] text-left">
+        {description}
+      </p>
+    </div>
+  );
+};
+
+export default ValueCard;
