69a43d72de9a72221703986facb98af1a7c60633
tarinrickett <trickett96@gmail.com>
2025-01-29 16:50:31 +0000

    Add some stories for testing

--- apps/website-25/src/components/about/HistorySection.stories.tsx
+++ apps/website-25/src/components/about/HistorySection.stories.tsx
@@ -1,23 +1,22 @@
 import type { Meta, StoryObj } from '@storybook/react';
 
-import { Tag } from './Tag';
+import HistorySection from './HistorySection';
 
 const meta = {
-  title: 'ui/Tag',
-  component: Tag,
+  title: 'website/HistorySection',
+  component: HistorySection,
   // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
     // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
-} satisfies Meta<typeof Tag>;
+  args: {},
+} satisfies Meta<typeof HistorySection>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {
-    children: 'This is a tag',
-  },
+  args: {},
 };
