73c7d80330005caa39aacbde38deecfc010c666d
Will Howard <w.howard256+github@gmail.com>
2025-02-03 10:47:46 +0000

    Fix semantic styling conflicts

--- libraries/ui/src/CourseCard.tsx
+++ libraries/ui/src/CourseCard.tsx
@@ -138,8 +138,8 @@ export const CourseCard: React.FC<CourseCardProps> = ({
       isEntireCardClickable
       isExternalUrl
       className={clsx(
-        'course-card course-card--regular container-lined p-5',
-        'flex flex-col w-[323px] h-[466px]',
+        'course-card course-card--regular min-w-min-width container-lined p-5',
+        'flex flex-col w-[323px] h-[466px] hover:container-elevated',
         className,
       )}
       imageClassName="course-card__image w-full h-[165px] object-cover rounded-none"
