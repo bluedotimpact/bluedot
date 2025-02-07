46a4be0ec0e80d0142a44177494be35928fd6a5a
Will Howard <w.howard256+github@gmail.com>
2025-02-07 10:13:05 +0000

    Make Footer responsive

--- libraries/ui/src/Footer.tsx
+++ libraries/ui/src/Footer.tsx
@@ -11,73 +11,105 @@ export type FooterProps = React.PropsWithChildren<{
   logo?: string
 }>;
 
+type FooterSectionProps = {
+  title?: string;
+  links?: { href: string; label: string }[];
+};
+
+const FooterSection: React.FC<FooterSectionProps> = ({ title, links }) => (
+  <div className="footer__section">
+    {title && <h3 className="footer__heading text-on-dark mb-4 text-size-m">{title}</h3>}
+    {links && (
+      <ul className="footer__list space-y-2 mb-auto font-normal list-none p-0">
+        {links.map((link) => (
+          <li key={link.href} className="footer__item">
+            <a href={link.href} className="footer__link text-bluedot-lighter hover:text-white hover:cursor-pointer">
+              {link.label}
+            </a>
+          </li>
+        ))}
+      </ul>
+    )}
+  </div>
+);
+
+type FooterSocialProps = {
+  className?: string;
+};
+
+const FooterSocial: React.FC<FooterSocialProps> = ({ className }) => (
+  <div className={clsx('footer__social flex gap-6', className)}>
+    <a href="https://twitter.com/BlueDotImpact" className="footer__social-link link-on-dark" aria-label="Twitter">
+      <FaXTwitter className="size-6" />
+    </a>
+    <a href="https://youtube.com/@bluedotimpact" className="footer__social-link link-on-dark" aria-label="YouTube">
+      <FaYoutube className="size-6" />
+    </a>
+    <a href="https://www.facebook.com/bluedotimpact" className="footer__social-link link-on-dark" aria-label="Facebook">
+      <FaFacebook className="size-6" />
+    </a>
+    <a href="#" className="footer__social-link link-on-dark" aria-label="Instagram">
+      <FaInstagram className="size-6" />
+    </a>
+    {/* TODO: 01/27 Add Instagram link */}
+    <a href="https://www.linkedin.com/company/bluedotimpact/" className="footer__social-link link-on-dark" aria-label="LinkedIn">
+      <FaLinkedin className="size-6" />
+    </a>
+  </div>
+);
+
 export const Footer: React.FC<FooterProps> = ({ className, logo }) => {
   // Footer links are styled differently than default `.link-on-dark`
   const footerLinkClassNames = 'footer__link text-bluedot-lighter hover:text-white hover:cursor-pointer';
 
   return (
     <div className="footer">
-      <footer className={clsx('footer__main bg-bluedot-darker text-white min-h-[397] p-8 flex flex-col', className)}>
-        <div className="footer__content flex flex-col mb-16">
-          <nav className="footer__nav grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
-            <div className="footer__section footer__section--brand flex flex-col justify-between h-[180px]">
+      <footer className={clsx('footer__main bg-bluedot-darker text-white min-h-[397] p-12 flex flex-col', className)}>
+        <div className="footer__content flex flex-col mb-24">
+          <nav className="footer__nav flex flex-col justify-between lg:flex-row gap-24 lg:gap-4">
+            <div className="footer__section footer__section--brand flex flex-row lg:flex-col justify-between">
               {logo ? (
-                <img className="footer__logo h-6" src={logo} alt="BlueDot Impact Logo" />
+                <img className="footer__logo h-6 mr-auto" src={logo} alt="BlueDot Impact Logo" />
               ) : (
                 <p className="footer__logo h-8 text-xl text-white">BlueDot Impact</p>
               )}
-
-              <div className="footer__social flex gap-6 justify-center">
-                <a href="https://twitter.com/BlueDotImpact" className="footer__social-link link-on-dark" aria-label="Twitter">
-                  <FaXTwitter className="size-6" />
-                </a>
-                <a href="https://youtube.com/@bluedotimpact" className="footer__social-link link-on-dark" aria-label="YouTube">
-                  <FaYoutube className="size-6" />
-                </a>
-                <a href="https://www.facebook.com/bluedotimpact" className="footer__social-link link-on-dark" aria-label="Facebook">
-                  <FaFacebook className="size-6" />
-                </a>
-                <a href="#" className="footer__social-link link-on-dark" aria-label="Instagram">
-                  <FaInstagram className="size-6" />
-                </a>
-                {/* TODO: 01/27 Add Instagram link */}
-                <a href="https://www.linkedin.com/company/bluedotimpact/" className="footer__social-link link-on-dark" aria-label="LinkedIn">
-                  <FaLinkedin className="size-6" />
-                </a>
-              </div>
+              <FooterSocial className="hidden sm:flex" />
             </div>
 
-            <div className="footer__section">
-              <p className="footer__heading text-on-dark font-[650] mb-4">BlueDot Impact</p>
-              <ul className="footer__list space-y-2 mb-auto font-normal">
-                <li className="footer__item"><a href="/about" className={footerLinkClassNames}>About us</a></li>
-                <li className="footer__item"><a href="/support-us" className={footerLinkClassNames}>Support us</a></li>
-                <li className="footer__item"><a href="/careers" className={footerLinkClassNames}>Join us</a></li>
-                <li className="footer__item"><a href="/contact" className={footerLinkClassNames}>Contact us</a></li>
-              </ul>
-            </div>
+            <div className="grid grid-cols-1 gap-12 sm:grid-cols-3 sm:gap-5">
+              <FooterSection
+                title="BlueDot Impact"
+                links={[
+                  { href: '/about', label: 'About us' },
+                  { href: '/support-us', label: 'Support us' },
+                  { href: '/careers', label: 'Join us' },
+                  { href: '/contact', label: 'Contact us' },
+                ]}
+              />
 
-            {/* // TODO: 01/27 add routing to courses when AISafetyFundamentals course is integrated, i.e.'/courses/intro-transformative-ai */}
-            <div className="footer__section">
-              <p className="footer__heading text-on-dark font-[650] mb-4">Explore</p>
-              <ul className="footer__list space-y-2 text-base font-normal">
-                <li className="footer__item"><a href="https://aisafetyfundamentals.com/intro-to-tai/" className={footerLinkClassNames}>Intro to Transformative AI</a></li>
-                <li className="footer__item"><a href="https://aisafetyfundamentals.com/alignment-fast-track/" className={footerLinkClassNames}>AI Alignment Fast Track</a></li>
-                <li className="footer__item"><a href="https://aisafetyfundamentals.com/alignment/" className={footerLinkClassNames}>AI Alignment In-Depth</a></li>
-                <li className="footer__item"><a href="https://aisafetyfundamentals.com/governance/" className={footerLinkClassNames}>AI Governance In-Depth</a></li>
-              </ul>
-            </div>
+              {/* // TODO: 01/27 add routing to courses when AISafetyFundamentals course is integrated, i.e.'/courses/intro-transformative-ai */}
+              <FooterSection
+                title="Explore"
+                links={[
+                  { href: 'https://aisafetyfundamentals.com/intro-to-tai/', label: 'Intro to Transformative AI' },
+                  { href: 'https://aisafetyfundamentals.com/alignment-fast-track/', label: 'AI Alignment Fast Track' },
+                  { href: 'https://aisafetyfundamentals.com/alignment/', label: 'AI Alignment In-Depth' },
+                  { href: 'https://aisafetyfundamentals.com/governance/', label: 'AI Governance In-Depth' },
+                ]}
+              />
 
-            <div className="footer__section">
-              {/* TODO: 01/27 add routing to resources when resources are integrated */}
-              {/* <h3 className="text-lg mb-4">Resources</h3>
-              <ul className="space-y-2">
-                <li><a href="/announcements">Announcements and stories</a></li>
-                <li><a href="/useful-links">Useful links</a></li>
-                <li><a href="/community-projects">Community projects</a></li>
-                <li><a href="/privacy-policy">Privacy Policy</a></li>
-              </ul> */}
+              {/* TODO add comments back in, and comment out the below */}
+              {/* <FooterSection
+                title="Resources"
+                links={[
+                  { href: '/announcements', label: 'Announcements and stories' },
+                  { href: '/useful-links', label: 'Useful links' },
+                  { href: '/community-projects', label: 'Community projects' },
+                  { href: '/privacy-policy', label: 'Privacy Policy' },
+                ]}
+              /> */}
             </div>
+            <FooterSocial className="sm:hidden" />
           </nav>
         </div>
         <p className="footer__copyright text-sm text-center text-bluedot-lighter">
