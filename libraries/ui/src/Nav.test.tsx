6f7373f17c38bc1a56b388b8b6a53f143cf8b831
Will Howard <w.howard256+github@gmail.com>
2025-02-10 20:06:35 +0000

    Make Nav responsive (#208)
    * Make Nav responsive
    
    * Styling tweak
    
    * Update story
    
    * Tweaks
    
    * Remove unused import
    
    * Remove another unused import
    
    * Address simple comments
    
    * Address more comments
    
    * Resolve hover state issue (#232)
    
    * Fix lint
    
    * Address comments

--- libraries/ui/src/Nav.test.tsx
+++ libraries/ui/src/Nav.test.tsx
@@ -1,5 +1,5 @@
 import { describe, expect, test } from 'vitest';
-import { render } from '@testing-library/react';
+import { render, waitFor, fireEvent } from '@testing-library/react';
 import { Nav } from './Nav';
 
 describe('Nav', () => {
@@ -23,9 +23,34 @@ describe('Nav', () => {
           { title: 'Course 1', href: '/course1' },
         ]}
       >
-        <p>This is the yield</p>
+        <p id="yield-element">This is the yield</p>
       </Nav>,
     );
+    const yieldElement = container.querySelector('#yield-element');
+    expect(yieldElement).not.toBeNull();
+
     expect(container).toMatchSnapshot();
+  });
+
+  test('clicking the hamburger button expands the mobile nav drawer', async () => {
+    const { container } = render(
+      <Nav courses={[{ title: 'Course 1', href: '/course1' }]} />,
+    );
+
+    const hamburgerButton = container.querySelector('.nav__menu--mobile-tablet');
+    expect(hamburgerButton).not.toBeNull();
+
+    const navDrawer = container.querySelector('.nav__drawer');
+    expect(navDrawer).not.toBeNull();
+
+    // Initially, the nav__drawer should have a max height of 0 (closed state).
+    expect(navDrawer!.className).toMatch(/max-h-0/);
+
+    fireEvent.click(hamburgerButton!);
+
+    // The nav__drawer now has a max height that is not 0 (expanded state).
+    await waitFor(() => {
+      expect(navDrawer!.className).not.toMatch(/max-h-0/);
+    });
   });
 });
