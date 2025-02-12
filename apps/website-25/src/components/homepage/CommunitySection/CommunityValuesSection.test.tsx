09066074ac63418b030f207f69ae48777cd293f5
Will Howard <w.howard256+github@gmail.com>
2025-02-12 18:22:53 +0000

    Address comments

--- apps/website-25/src/components/homepage/CommunitySection/CommunityValuesSection.test.tsx
+++ apps/website-25/src/components/homepage/CommunitySection/CommunityValuesSection.test.tsx
@@ -5,11 +5,9 @@ import CommunityValuesSection from './CommunityValuesSection';
 describe('CommunityValuesSection', () => {
   test('renders as expected', () => {
     const { container } = render(<CommunityValuesSection />);
-    expect(container).toMatchSnapshot();
-  });
 
-  test('displays correct number of projects', () => {
-    const { container } = render(<CommunityValuesSection />);
+    expect(container).toMatchSnapshot();
+
     const testimonialCards = container.querySelectorAll('.community-values-section__value');
     expect(testimonialCards).toHaveLength(4);
   });
