86ad6f7bc2ce41e648f96add8266d55cd5faf3e3
tarinrickett <trickett96@gmail.com>
2025-02-17 08:47:59 +0000

    Mini Header to Join page too

--- apps/website-25/src/pages/careers.tsx
+++ apps/website-25/src/pages/careers.tsx
@@ -8,15 +8,17 @@ import Head from 'next/head';
 import CareersSection from '../components/careers/CareersSection';
 import CultureSection from '../components/careers/CultureSection';
 import ValuesSection from '../components/careers/ValuesSection';
+import { HeroMiniTitle } from '@bluedot/ui/src/HeroSection';
 
 const CareersPage = () => {
   return (
     <div>
       <Head>
-        <title>Join the team at BlueDot Impact</title>
+        <title>Join our team | BlueDot Impact</title>
         <meta name="description" content="Join us in our mission to ensure humanity safely navigates the transition to transformative AI." />
       </Head>
       <HeroSection>
+        <HeroMiniTitle>Join Us</HeroMiniTitle>
         <HeroH1>Join us in our mission to ensure humanity safely navigates the transition to transformative AI.</HeroH1>
         <HeroCTAContainer>
           <CTALinkOrButton url="#open-roles-anchor" withChevron>See open roles</CTALinkOrButton>
