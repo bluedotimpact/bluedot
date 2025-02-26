012b6e986707f4e1e1eb7bea8a3d709e057d304b
Adam Jones <domdomegg+git@gmail.com>
2025-02-26 02:12:41 +0000

    infra: Add room

--- apps/room/src/lib/api/db.ts
+++ apps/room/src/lib/api/db.ts
@@ -6,7 +6,7 @@ export const rooms: Room[] = [
     id: 'tv',
     name: 'BlueDot TV',
     status: {
-      currentUrl: 'https://rooms.bluedot.org/display/tv',
+      currentUrl: 'https://room.bluedot.org/display/tv',
       lastHeartbeatAt: 0,
       lastUpdatedAt: 0,
     },
