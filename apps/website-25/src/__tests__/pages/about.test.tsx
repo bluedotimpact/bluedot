cb3a74f59058b750d73d2f312e0222c71f42e0f4
Will Howard <w.howard256+github@gmail.com>
2025-02-27 21:07:53 +0000

    Add 404 page (#475)
    * Add 404 page
    
    * Add BEM classNames
    
    * Add comment
    
    * Update snapshot
    
    * Address comments

--- apps/website-25/src/__tests__/pages/about.test.tsx
+++ apps/website-25/src/__tests__/pages/about.test.tsx
@@ -3,7 +3,7 @@ import { describe, expect, test } from 'vitest';
 import AboutPage from '../../pages/about';
 
 describe('AboutPage', () => {
-  test('should render the error message correctly', () => {
+  test('should render correctly', () => {
     const { container } = render(<AboutPage />);
     expect(container).toMatchSnapshot();
   });
