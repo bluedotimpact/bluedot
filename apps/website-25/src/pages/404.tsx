ab5757471030d190956959a13c238dc8c9d25f03
Will Howard <w.howard256+github@gmail.com>
2025-02-27 21:01:49 +0000

    Address comments

--- apps/website-25/src/pages/404.tsx
+++ apps/website-25/src/pages/404.tsx
@@ -22,11 +22,11 @@ const Error404Page = () => {
       <Head>
         <title>{currentRoute.title} | BlueDot Impact</title>
       </Head>
-      <HeroSection className="404__hero overflow-hidden">
+      <HeroSection className="404-hero overflow-hidden">
         {/* The background on the Rive animation slightly mismatches the HeroSection
             background. The box-shadow here is to blend the two together */}
         <div
-          className="404__animation-container w-[400px] max-w-[80vw] aspect-2/1 shadow-[0_0_120px_120px_#011664]"
+          className="404-hero__animation-container w-[400px] max-w-[80vw] aspect-2/1 shadow-[0_0_120px_120px_#011664]"
         >
           <Rive
             src="/animations/herobluedot_hero.riv"
