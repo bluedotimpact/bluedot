6a9067e87a4e59d4c757be3469fa69e59310e7d4
Will Howard <w.howard256+github@gmail.com>
2025-03-12 10:37:52 +0000

    Fix keyboard navigation in Nav

--- apps/website-25/src/components/Nav.tsx
+++ apps/website-25/src/components/Nav.tsx
@@ -48,13 +48,13 @@ const ExploreSection: React.FC<{
   >
     <div
       className={clsx(
-        'nav-explore-section___dropdown-content flex flex-col gap-[14px] w-fit overflow-hidden text-pretty',
+        'nav-explore-section___dropdown-content flex flex-col gap-[14px] text-pretty px-6',
         innerClassName,
       )}
     >
       <h3 className="nav-explore-section__dropdown-title font-bold">Our courses</h3>
       {courses?.map((course) => (
-        <a key={course.href} href={course.href} className="nav-explore-section__dropdown-link">
+        <a key={course.href} href={course.href} tabIndex={expanded ? 0 : -1} className="nav-explore-section__dropdown-link">
           {course.isNew && (
             <span className="nav-explore-section__new-badge text-bluedot-normal font-black pr-2">
               New!
@@ -82,10 +82,25 @@ const NavLinks: React.FC<{
   className,
   isScrolled,
 }) => {
+  const navLinkContainerClasses = clsx(
+    'nav-links grid [&>*]:w-fit items-center',
+    exploreSectionInline ? 'grid-cols-1 gap-y-9' : 'grid-cols-subgrid grid-rows-subgrid row-[1/3] col-[3/7]',
+    className,
+  );
   const navLinkClasses = clsx('nav-link-animation', isScrolled && 'nav-link-animation-dark');
 
+  const exploreSection = (
+    <ExploreSection
+      expanded={exploreExpanded}
+      courses={courses}
+      className="nav-links__explore-section col-span-full row-[2]"
+      innerClassName={exploreSectionInline ? 'pt-6' : 'pb-9'}
+      isScrolled={isScrolled}
+    />
+  );
+
   return (
-    <div className={clsx('nav-links flex gap-9 [&>*]:w-fit', className)}>
+    <div className={navLinkContainerClasses}>
       <div>
         <button
           type="button"
@@ -95,16 +110,11 @@ const NavLinks: React.FC<{
           Explore
           <DropdownIcon expanded={exploreExpanded} />
         </button>
-        {exploreSectionInline && (
-          <ExploreSection
-            expanded={exploreExpanded}
-            courses={courses}
-            className="nav-links__explore-section"
-            innerClassName="pl-6 pt-6"
-            isScrolled={isScrolled}
-          />
-        )}
+        {exploreSectionInline && exploreSection}
       </div>
+      {/* `exploreSection` is placed here in the DOM to make it the next step after "Explore"
+          when tabbing through the header. Visually, it is moved below by setting row-[2] */}
+      {!exploreSectionInline && exploreSection}
       <a href={ROUTES.about.url} className={clsx('nav-links__link', navLinkClasses, isCurrentPath(ROUTES.about.url) && 'font-bold')}>About us</a>
       <a href={ROUTES.joinUs.url} className={clsx('nav-links__link', navLinkClasses, isCurrentPath(ROUTES.joinUs.url) && 'font-bold')}>Join us</a>
       <a href="https://bluedot.org/blog/" className={clsx('nav-links__link', navLinkClasses)}>Blog</a>
@@ -171,8 +181,8 @@ export const Nav: React.FC<NavProps> = ({
     >
       <ClickAwayListener onClickAway={() => setExpandedSections('none')}>
         <div className="nav__container section-base">
-          <div className="nav__bar w-full grid grid-cols-2 lg:grid-cols-[20%_60%_20%] items-center h-[72px] sm:h-[100px]">
-            <a href="/" className="nav__logo-link shrink-0 w-[200px]">
+          <div className="nav__bar w-full grid grid-cols-2 gap-x-9 lg:grid-cols-[20%_1fr_auto_auto_auto_auto_1fr_20%] items-center grid-rows-[72px] sm:grid-rows-[100px]">
+            <a href="/" className="nav__logo-link shrink-0 w-[200px] col-[1]">
               {logo ? (
                 <img
                   className={clsx(
@@ -191,9 +201,9 @@ export const Nav: React.FC<NavProps> = ({
               exploreExpanded={exploreExpanded}
               courses={courses}
               isScrolled={isScrolled}
-              className="nav__links--desktop hidden lg:flex mx-auto"
+              className="nav__links--desktop hidden lg:grid mx-auto"
             />
-            <div className="nav__actions flex gap-space-between ml-auto">
+            <div className="nav__actions flex gap-space-between ml-auto col-[8]">
               <CTAButtons className="nav__login--tablet-desktop gap-6 hidden sm:flex" />
               <HamburgerButton
                 open={navExpanded}
@@ -206,18 +216,9 @@ export const Nav: React.FC<NavProps> = ({
           <div
             className={clsx(
               'nav__drawer transition-[max-height,opacity] duration-300 relative overflow-hidden px-3',
-              navExpanded ? 'max-h-[700px] opacity-100' : 'max-h-0 opacity-0',
+              navExpanded ? 'max-h-[800px] opacity-100' : 'max-h-0 opacity-0',
             )}
           >
-            {/* Desktop Explore section */}
-            <ExploreSection
-              expanded={exploreExpanded}
-              courses={courses}
-              className="nav__drawer-content--desktop"
-              innerClassName="pb-10 hidden lg:flex mx-auto"
-              isScrolled={isScrolled}
-            />
-            {/* Mobile & Tablet content (including Explore) */}
             <div className="nav__drawer-content--mobile-tablet flex flex-col grow font-medium pb-8 pt-2 lg:hidden">
               <NavLinks
                 onToggleExplore={onToggleExplore}
