1c4169439cb85a5af30b3ae8c87f1822cc23b6fe
Ang Li-Lian <43257301+anglilian@users.noreply.github.com>
2025-03-13 22:33:38 +0800

    Anglilian patch 1 (#517)
    * Update constants.ts with Big MOOC
    
    * Update constants.ts
    
    Changed order and add "new" to AI Impact course
    
    * Update constants.ts
    
    Change link to AI Impact

--- libraries/ui/src/constants.ts
+++ libraries/ui/src/constants.ts
@@ -12,6 +12,15 @@ export type CourseType = 'Crash course' | 'Self-paced' | 'In-depth course';
 
 export const COURSES: Course[] = [
   {
+    title: 'The AI Impact',
+    description: 'A self-paced, 2-hour course designed for people with no technical background to learn how AI will reshape our world.',
+    courseType: 'Self-paced',
+    courseLength: '',
+    imageSrc: '/images/courses/ai-impact.png',
+    href: 'https://course.bluedot.org/ai-impact/',
+    isNew: true,
+  },
+  {
     title: 'AI Safety: Intro to Transformative AI',
     description: 'The risks and opportunities of advanced AI are evolving at unprecedented speed—and so is the need for capable individuals to shape its trajectory. This intensive 5-day course is for those who want to rapidly develop their understanding of transformative AI and its impact on humanity.',
     courseType: 'Crash course',
@@ -26,7 +35,6 @@ export const COURSES: Course[] = [
     courseLength: '9 weeks',
     imageSrc: '/images/courses/econ.jpg',
     href: 'https://aisafetyfundamentals.com/economics-of-tai/',
-    isNew: true,
   },
   {
     title: 'AI Alignment',
