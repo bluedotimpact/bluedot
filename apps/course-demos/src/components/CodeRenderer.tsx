322f3bd528948cf8b966bb939a13b4082cdcc4d4
Adam Jones <domdomegg+git@gmail.com>
2025-03-18 17:02:42 +0000

    course-demos: Be able to save demo outputs, and share them on social media (#522)

---
+++ apps/course-demos/src/components/CodeRenderer.tsx
@@ -0,0 +1,65 @@
+import React, { useState, useEffect } from 'react';
+import {
+  SandpackCodeEditor, SandpackLayout, SandpackPreview, SandpackProvider,
+} from '@codesandbox/sandpack-react';
+import { P, Button } from '@bluedot/ui';
+
+interface CodeRendererProps {
+  code: string;
+  height?: string;
+  hidePreview?: boolean;
+}
+
+export const CodeRenderer: React.FC<CodeRendererProps> = ({ code, height, hidePreview = false }) => {
+  const files = {
+    '/App.js': {
+      code: `import React from 'react';
+
+${code}
+
+export default function App() {
+  return (
+    <Component />
+  );
+}
+`,
+      readOnly: true,
+    },
+  };
+
+  const [view, setView] = useState<'code' | 'load_preview' | 'preview'>(hidePreview ? 'code' : 'preview');
+  // This is a hack to refresh the SandpackProvider so the preview refreshes
+  useEffect(() => {
+    if (view === 'load_preview') {
+      setView('preview');
+    }
+  }, [view]);
+
+  return (
+    <>
+      {view !== 'load_preview'
+    && (
+    <SandpackProvider
+      template="react"
+      files={files}
+      options={{
+        externalResources: ['https://cdn.tailwindcss.com'],
+      }}
+    >
+      <SandpackLayout>
+        {view === 'preview' && <SandpackPreview showOpenInCodeSandbox={false} style={{ height }} />}
+        {view === 'code' && <SandpackCodeEditor showRunButton={false} style={{ height }} />}
+      </SandpackLayout>
+    </SandpackProvider>
+    )}
+      {view === 'load_preview' && <div style={{ height, margin: '0 1px' }} />}
+      {!hidePreview && (
+      <nav className="justify-end items-center flex gap-2">
+        <P className="!my-0">Show:</P>
+        <Button className={view === 'code' ? 'bg-bluedot-lighter' : ''} onPress={() => setView('code')}>Code</Button>
+        <Button className={view === 'preview' ? 'bg-bluedot-lighter' : ''} onPress={() => setView('load_preview')}>Preview</Button>
+      </nav>
+      )}
+    </>
+  );
+};
---
+++ apps/course-demos/src/components/ProgressDots.tsx
@@ -0,0 +1,9 @@
+export const ProgressDots: React.FC = () => {
+  return (
+    <div className="flex justify-center space-x-2 my-6">
+      <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '0ms' }} />
+      <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '150ms' }} />
+      <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '300ms' }} />
+    </div>
+  );
+};
