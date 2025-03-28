94e366f2b1d684b86b59acf6cb950827864b8b17
tarinrickett <trickett96@gmail.com>
2025-03-28 15:57:26 +0000

    Add background to LandingHero

--- apps/website-25/src/pages/lander/LandingHero.tsx
+++ apps/website-25/src/pages/lander/LandingHero.tsx
@@ -9,6 +9,7 @@ export type LandingHeroProps = React.PropsWithChildren<{
   ctaUrl: string,
 }>;
 
+<<<<<<< Updated upstream
 const LandingHero: React.FC<LandingHeroProps> = ({
   className,
   pretitle,
@@ -31,6 +32,31 @@ const LandingHero: React.FC<LandingHeroProps> = ({
           <CTALinkOrButton url={ctaUrl} variant="primary">
             Sign up for free
           </CTALinkOrButton>
+=======
+export const LandingHero: React.FC<LandingHeroProps> = ({
+    className,
+    pretitle,
+    title,
+    subtitle,
+    ctaUrl
+  }) => {
+    return (
+      <div className={clsx('landing-hero bg-canvas flex flex-row justify-center items-center w-full px-spacing-x bg-[url("/images/lander/hero-bkg.png")] bg-cover bg-center', className)}>
+        {/* Top margin is nav height (82px) */}
+        <div className="landing-hero__content max-w-[865px] mt-[82px] py-16">
+          <h1 className="landing-hero__title text-center font-serif flex flex-col items-center">
+            {pretitle && <span className="landing-hero__pretitle text-6xl font-normal">{pretitle}</span>}
+            <span className="landing-hero__title-text text-9xl font-bold text-bluedot-darker">{title}</span>
+          </h1>
+          <p className="landing-hero__subtitle text-center text-lg mt-6 font-serif">
+            {subtitle}
+          </p>
+          <div className="flex justify-center mt-8">
+            <CTALinkOrButton url={ctaUrl} variant="primary">
+              Sign up for free
+            </CTALinkOrButton>
+          </div>
+>>>>>>> Stashed changes
         </div>
       </div>
     </div>
