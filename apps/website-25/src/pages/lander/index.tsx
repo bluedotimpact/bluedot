166f3965b1e47f14cbce283917d8b49dc94b4c05
Adam Jones <domdomegg+git@gmail.com>
2025-03-28 14:22:36 +0000

    website-25: Add new lander page (#531)

---
+++ apps/website-25/src/pages/lander/index.tsx
@@ -0,0 +1,35 @@
+import { useEffect } from 'react';
+import { useRouter } from 'next/router';
+
+const landingPages = [
+  '/lander/classic',
+];
+
+const LandingPageRouter = () => {
+  const router = useRouter();
+
+  useEffect(() => {
+    // Only run on the client side
+    if (typeof window !== 'undefined') {
+      const landingPageIndex = Math.floor(Math.random() * landingPages.length);
+      const landingPageBaseUrl = landingPages[landingPageIndex];
+
+      // Get the current query parameters
+      const queryParams = new URLSearchParams(window.location.search);
+
+      // Redirect to the selected landing page with all query parameters
+      // Using object format to fix TypeScript error
+      router.replace({
+        pathname: landingPageBaseUrl,
+        search: queryParams.toString(),
+      });
+    }
+  }, [router]);
+
+  // Return null as we're just redirecting
+  return null;
+};
+
+LandingPageRouter.rawLayout = true;
+
+export default LandingPageRouter;
