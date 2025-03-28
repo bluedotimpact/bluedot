efc1715bcb75249579b05e8a67dc3b5545fb2ca4
Adam Jones <domdomegg+git@gmail.com>
2025-03-28 20:16:33 +0000

    landing: Pass through other params to miniextension prefills (#549)

--- apps/website-25/src/components/lander/getCtaUrl.ts
+++ apps/website-25/src/components/lander/getCtaUrl.ts
@@ -1,6 +1,10 @@
 export const getCtaUrl = (variant: string) => {
-  const queryParams = new URLSearchParams(window.location.search);
-  queryParams.append('prefill_Variant', variant);
-  const ctaUrl = `https://web.miniextensions.com/aGd0mXnpcN1gfqlnYNZc?prefill_Topic=Let%27s%20Make%20The%20Future%20Awesome%20Course&${queryParams.toString()}`;
+  const thisPageQueryParams = new URLSearchParams(window.location.search);
+  const miniextensionsQueryParams = new URLSearchParams();
+  thisPageQueryParams.forEach((value, key) => {
+    miniextensionsQueryParams.append(`prefill_${key}`, value);
+  });
+  miniextensionsQueryParams.append('prefill_Variant', variant);
+  const ctaUrl = `https://web.miniextensions.com/aGd0mXnpcN1gfqlnYNZc?prefill_Topic=Let%27s%20Make%20The%20Future%20Awesome%20Course&${miniextensionsQueryParams.toString()}`;
   return ctaUrl;
 };
