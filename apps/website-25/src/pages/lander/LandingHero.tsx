743b24f2948770495ab1276413ad1561836def2b
tarinrickett <trickett96@gmail.com>
2025-03-28 16:07:08 +0000

    Update Hero background

--- apps/website-25/src/pages/lander/LandingHero.tsx
+++ apps/website-25/src/pages/lander/LandingHero.tsx
@@ -17,14 +17,14 @@ const LandingHero: React.FC<LandingHeroProps> = ({
   ctaUrl,
 }) => {
   return (
-    <div className={clsx('landing-hero bg-canvas flex flex-row justify-center items-center w-full px-spacing-x', className)}>
+    <div className={clsx('landing-hero bg-canvas flex flex-row justify-center items-center w-full px-spacing-x bg-[url("/images/lander/hero-bkg.png")] bg-cover bg-center', className)}>
       {/* Top margin is nav height (82px) */}
       <div className="landing-hero__content max-w-[865px] mt-[82px] py-16">
         <h1 className="landing-hero__title text-center font-serif flex flex-col items-center">
           {pretitle && <span className="landing-hero__pretitle text-6xl font-normal">{pretitle}</span>}
           <span className="landing-hero__title-text text-9xl font-bold text-bluedot-darker">{title}</span>
         </h1>
-        <p className="landing-hero__subtitle text-center text-size-l mt-4 font-serif">
+        <p className="landing-hero__subtitle text-center text-size-l mt-6 font-serif">
           {subtitle}
         </p>
         <div className="flex justify-center mt-8">
