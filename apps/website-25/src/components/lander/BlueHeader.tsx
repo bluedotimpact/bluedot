3eccadee08f4c00d13aa8f224f944d391dd7cc86
Adam Jones <domdomegg+git@gmail.com>
2025-03-28 19:11:14 +0000

    lander: fix dewi/will section spacing (#548)

--- apps/website-25/src/components/lander/BlueHeader.tsx
+++ apps/website-25/src/components/lander/BlueHeader.tsx
@@ -2,7 +2,6 @@ import clsx from 'clsx';
 
 const BlueHeader: React.FC<React.PropsWithChildren<{ className?: string }>> = ({ children, className }) => {
   return (
-  // TODO: add reckless neue bold
     <h2 className={clsx('text-size-xl font-serif font-bold text-center mb-12 text-bluedot-dark', className)}>{children}</h2>
   );
 };
