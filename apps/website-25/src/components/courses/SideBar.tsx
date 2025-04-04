e45016c1b89d5185d68946b3f6949a4f32df19f1
tarinrickett <trickett96@gmail.com>
2025-04-04 08:46:47 +0000

    Stronger current unit indicator in sidebar

--- apps/website-25/src/components/courses/SideBar.tsx
+++ apps/website-25/src/components/courses/SideBar.tsx
@@ -11,18 +11,21 @@ const SideBar: React.FC<SideBarProps> = ({
   units,
   currentUnit,
 }) => {
+  const isCurrentUnit = (unit: CourseUnit) => {
+    return currentUnit.title === unit.title;
+  };
   return (
     <div className="sidebar w-full md:w-[332px] flex flex-col">
       <div className="sidebar__content flex flex-col gap-6">
         {units.map((unit) => (
           <div
-            className="sidebar__unit border border-color-divider rounded-radius-md p-4 flex flex-col gap-2"
+            className={`sidebar__unit border ${isCurrentUnit(unit) ? 'border-color-primary' : 'border-color-divider'} rounded-radius-md p-4 flex flex-col gap-2`}
           >
             <div className="sidebar__unit-item flex flex-col gap-2">
               <p className="sidebar__title uppercase text-size-xs font-bold">{unit.title}</p>
               <a href={unit.href} className="sidebar__description subtitle-sm">{unit.description}</a>
             </div>
-            {currentUnit.title === unit.title && unit.chapters && (
+            {isCurrentUnit(unit) && unit.chapters && (
               <div className="sidebar__chapters flex flex-col gap-4 border-t-2 border-color-primary pt-4">
                 {unit.chapters.map((chapter) => (
                   <div className="sidebar__chapter">
