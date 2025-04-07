f12568c8b114d7edf99a6faae8e4954d28845d71
tarinrickett <trickett96@gmail.com>
2025-04-07 13:36:26 +0000

    [Incident] Revert Rive on homepage

--- apps/website-25/src/components/homepage/HomeHeroContent.tsx
+++ apps/website-25/src/components/homepage/HomeHeroContent.tsx
@@ -2,16 +2,12 @@ import {
   HeroH1,
   HeroH2,
 } from '@bluedot/ui';
-import Rive, { Fit, Layout } from '@rive-app/react-canvas';
 import clsx from 'clsx';
 
 const HomeHeroContent: React.FC<{ className?: string }> = ({ className }) => (
   <div className={clsx('home-hero-content pb-5 bg-bluedot-darker', className)}>
-    <div className="home-hero-content__animation-container w-[350px] max-w-[80vw] aspect-4/3 mx-auto">
-      <Rive
-        src="/animations/herobluedot_hero.riv"
-        layout={new Layout({ fit: Fit.Cover })}
-      />
+    <div className="home-hero-content__logo-container flex flex-col items-center gap-7 mb-8 slide-up-fade-in">
+      <img className="home-hero-content__logo-icon w-20" src="/images/logo/BlueDot_Impact_Icon_White.svg" alt="BlueDot Impact" />
     </div>
     <HeroH1 className="home-hero-content__heading slide-up-fade-in">
       The expertise you need to shape safe AI
