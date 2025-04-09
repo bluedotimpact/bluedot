56f7eddbd505cfc9203c4562c63c76a342bfc287
tarinrickett <trickett96@gmail.com>
2025-04-09 15:44:52 +0000

    Add SocialShare component

---
+++ apps/website-25/src/components/courses/SocialShare.stories.tsx
@@ -0,0 +1,26 @@
+import type { Meta, StoryObj } from '@storybook/react';
+
+import SocialShare from './SocialShare';
+
+const meta = {
+  title: 'website/courses/SocialShare',
+  component: SocialShare,
+  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
+  tags: ['autodocs'],
+  parameters: {
+    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
+    layout: 'fullscreen',
+  },
+  args: {},
+} satisfies Meta<typeof SocialShare>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    courseUrl: 'https://course.bluedot.org/future-of-ai',
+    referralCode: '5SR7C4',
+    text: 'I\'ve just completed a free, 2-hour course on the future of AI and its impacts on society. Here are my takeaways:',
+  },
+};
