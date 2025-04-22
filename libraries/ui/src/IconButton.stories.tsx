67890d39d76698cc0eda17ba8b121c885f21e6af
Will Howard <w.howard256+github@gmail.com>
2025-04-22 18:36:53 +0100

    Update Nav for logged-in users (#649)
    * Update Nav for logged-in users
    
    * Revert unnecessary changes
    
    * Address comments
    
    * Refactor expanded section state

---
+++ libraries/ui/src/IconButton.stories.tsx
@@ -0,0 +1,23 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import React from 'react';
+import { IconButton, HamburgerIcon, HamburgerButtonProps } from './IconButton';
+
+const IconButtonWrapper: React.FC<Omit<HamburgerButtonProps, 'open' | 'setOpen'>> = (props) => {
+  const [open, setOpen] = React.useState(false);
+  return <IconButton {...props} open={open} setOpen={setOpen} />;
+};
+
+const meta = {
+  title: 'ui/IconButton',
+  component: IconButtonWrapper,
+  tags: ['autodocs'],
+} satisfies Meta<typeof IconButtonWrapper>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    Icon: <HamburgerIcon />,
+  },
+};
