b39e5a6cf7d55391b357066891a720aa25874b64
tarinrickett <trickett96@gmail.com>
2025-04-23 15:43:41 +0000

    Add image URL handling to Embed

--- apps/website-25/src/components/courses/Embed.tsx
+++ apps/website-25/src/components/courses/Embed.tsx
@@ -11,19 +11,24 @@ const Embed: React.FC<EmbedProps> = ({
   className,
 }) => {
   const isYouTube = url.startsWith('https://www.youtube.com/') || url.startsWith('https://www.youtube-nocookie.com/');
+  const isImage = url.endsWith('.png') || url.endsWith('.jpg') || url.endsWith('.jpeg') || url.endsWith('.gif');
 
   return (
-    // eslint-disable-next-line jsx-a11y/iframe-has-title
-    <iframe
-      src={isYouTube ? url.replace('https://www.youtube.com/', 'https://www.youtube-nocookie.com/') : url}
-      // Width and height should be overriden in css
-      width="100%"
-      height="100%"
-      frameBorder="0"
-      allowFullScreen
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      className={clsx('embed rounded-lg', isYouTube ? 'aspect-video h-auto' : 'h-[450px]', className)}
-    />
+    isImage ? (
+      <img src={url} alt="" className={clsx('embed rounded-lg', className)} />
+    ) : (
+      // eslint-disable-next-line jsx-a11y/iframe-has-title
+      <iframe
+        src={isYouTube ? url.replace('https://www.youtube.com/', 'https://www.youtube-nocookie.com/') : url}
+        // Width and height should be overriden in css
+        width="100%"
+        height="100%"
+        frameBorder="0"
+        allowFullScreen
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        className={clsx('embed rounded-lg', isYouTube ? 'aspect-video h-auto' : 'h-[450px]', className)}
+      />
+    )
   );
 };
 
