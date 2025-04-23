855d1a2be99582c8a7be11bfcf0e7e5d62eb616c
tarinrickett <trickett96@gmail.com>
2025-04-23 18:01:11 +0100

    Update icons for Safari browsers (#693)

--- libraries/ui/src/IconButton.tsx
+++ libraries/ui/src/IconButton.tsx
@@ -2,7 +2,7 @@ import clsx from 'clsx';
 import React from 'react';
 
 export const HamburgerIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="8 8 32 32" fill="none" className={className}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" className={className}>
     <path d="M16 18H32" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
     <path d="M16 24H32" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
     <path d="M16 30H32" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
@@ -10,7 +10,7 @@ export const HamburgerIcon: React.FC<{ className?: string }> = ({ className }) =
 );
 
 const CloseIcon: React.FC<{ className?: string }> = ({ className }) => (
-  <svg xmlns="http://www.w3.org/2000/svg" viewBox="8 8 32 32" fill="none" className={className}>
+  <svg xmlns="http://www.w3.org/2000/svg" width="48" height="48" viewBox="0 0 48 48" fill="none" className={className}>
     <path d="M18 29.3135L29.3137 17.9998" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
     <path d="M18 18.3135L29.3137 29.6272" stroke="currentColor" strokeWidth="2" strokeLinecap="round" />
   </svg>
