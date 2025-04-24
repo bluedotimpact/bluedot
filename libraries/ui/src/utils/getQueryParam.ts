47dc4b8001c82dd65639bb9775a914dd21e172ba
tarinrickett <trickett96@gmail.com>
2025-04-24 12:38:27 +0000

    Lint issues

--- libraries/ui/src/utils/getQueryParam.ts
+++ libraries/ui/src/utils/getQueryParam.ts
@@ -1,8 +1,7 @@
 export const getQueryParam = (url: string, key: string) => {
-    if (!url) {
-      throw new Error('URL is required');
-    }
-    const urlObj = new URL(url, window.location.origin);
-    return urlObj.searchParams.get(key);
+  if (!url) {
+    throw new Error('URL is required');
+  }
+  const urlObj = new URL(url, window.location.origin);
+  return urlObj.searchParams.get(key);
 };
-  
