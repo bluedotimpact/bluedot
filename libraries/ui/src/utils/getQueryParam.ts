5b7265fdb464471c87259816b5754aeca0bd4722
tarinrickett <trickett96@gmail.com>
2025-04-24 12:33:43 +0000

    Add getQueryParam util

---
+++ libraries/ui/src/utils/getQueryParam.ts
@@ -0,0 +1,8 @@
+export const getQueryParam = (url: string, key: string) => {
+    if (!url) {
+      throw new Error('URL is required');
+    }
+    const urlObj = new URL(url, window.location.origin);
+    return urlObj.searchParams.get(key);
+};
+  
