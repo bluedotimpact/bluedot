820ddf131e197776a244aae08888130bf45ed920
Adam Jones <domdomegg+git@gmail.com>
2025-04-25 10:50:59 +0100

    Add ProfileCourseCard with share button

--- apps/website-25/src/pages/profile.tsx
+++ apps/website-25/src/pages/profile.tsx
@@ -17,6 +17,9 @@ import { GetCourseRegistrationsResponse } from './api/course-registrations';
 import { GetCoursesResponse } from './api/courses';
 import { ROUTES } from '../lib/routes';
 import { A, H3, P } from '../components/Text';
+import Congratulations from '../components/courses/Congratulations';
+import SocialShare from '../components/courses/SocialShare';
+import { Course, CourseRegistration, User } from '../lib/api/db/tables';
 
 const CURRENT_ROUTE = ROUTES.profile;
 
@@ -81,22 +84,43 @@ const ProfilePage = withAuth(({ auth }) => {
               <CTALinkOrButton url={ROUTES.courses.url}>Join a course</CTALinkOrButton>
             </div>
           )}
-          {enrolledCourses.length > 0 && (enrolledCourses.map(({ course, courseRegistration }) => (
-            <div className="profile__course-progress flex flex-row justify-between items-center gap-4 container-lined bg-white p-8 mb-4" key={course.id}>
-              <div className="flex flex-col gap-2">
-                <H3>{course.title}</H3>
-                {courseRegistration.certificateId && <P>🎉 Completed! <A href={addQueryParam(ROUTES.certification.url, 'id', courseRegistration.certificateId)}>View your certificate</A></P>}
-              </div>
-              <CTALinkOrButton url={course.path} variant="primary">
-                Continue
-              </CTALinkOrButton>
-            </div>
-          )))}
+          {enrolledCourses.length > 0 && (enrolledCourses.map(({ course, courseRegistration }) => <ProfileCourseCard key={courseRegistration.id} course={course} courseRegistration={courseRegistration} user={userData.user} />))}
         </Section>
       </>
       )}
     </div>
   );
 });
+
+interface ProfileCourseCardProps {
+  course: Course;
+  courseRegistration: CourseRegistration;
+  user: User;
+}
+
+const ProfileCourseCard: React.FC<ProfileCourseCardProps> = ({ course, courseRegistration, user }) => {
+  return (
+    <div className="profile__course-progress flex flex-row justify-between items-center gap-4 container-lined bg-white p-8 mb-4" key={course.id}>
+      <div className="flex flex-col gap-2">
+        <P className="uppercase text-size-xs font-bold text-slate-600">{courseRegistration.certificateId ? 'Completed' : 'In progress'}</P>
+        <H3>{course.title}</H3>
+        {courseRegistration.certificateId && (
+        <>
+          <P><A href={addQueryParam(ROUTES.certification.url, 'id', courseRegistration.certificateId)}>View your certificate</A></P>
+          <P>Share the course</P>
+          <SocialShare
+            coursePath={course.path}
+            referralCode={user.referralId}
+            text={`🎉 I just completed the ${course.title} course from BlueDot Impact! It’s free, self-paced, and packed with insights. Check it out and sign up with my link below:`}
+          />
+        </>
+        )}
+      </div>
+      <CTALinkOrButton url={course.path} variant="primary">
+        Continue
+      </CTALinkOrButton>
+    </div>
+  );
+};
 
 export default ProfilePage;
