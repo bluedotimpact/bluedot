8a050e6f1f30ca7c6ec5993672d5ee8943739114
tarinrickett <trickett96@gmail.com>
2025-04-26 06:46:25 +0100

    Create ui/UnitCard (#754)
    * Create ui/UnitCard
    
    * Missed an update
    
    * Missed export
    
    * Lint errors
    
    * Lint errors

---
+++ libraries/ui/src/UnitCard.test.tsx
@@ -0,0 +1,30 @@
+import { describe, expect, test } from 'vitest';
+import { render } from '@testing-library/react';
+import { UnitCard } from './UnitCard';
+
+describe('UnitCard', () => {
+  const defaultProps = {
+    title: 'Title',
+    description: 'Description',
+    url: 'https://bluedot.org/courses/what-the-fish/1',
+    unitNumber: '1',
+  };
+
+  test('renders default as expected', () => {
+    const { container } = render(
+      <UnitCard {...defaultProps} />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+
+  test('renders with optional args', () => {
+    const { container } = render(
+      <UnitCard
+        {...defaultProps}
+        duration={10}
+        isCurrentUnit
+      />,
+    );
+    expect(container).toMatchSnapshot();
+  });
+});
