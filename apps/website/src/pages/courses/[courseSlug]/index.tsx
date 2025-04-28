69c00cecc795dbeaabfa711faaa492bdf0492609
tarinrickett <trickett96@gmail.com>
2025-04-28 06:26:20 +0000

    Proposal for FOAI lander redesign

--- apps/website/src/pages/courses/[courseSlug]/index.tsx
+++ apps/website/src/pages/courses/[courseSlug]/index.tsx
@@ -1,23 +1,25 @@
 import {
-  HeroSection,
+  Breadcrumbs,
+  CTALinkOrButton,
+  ErrorSection,
   HeroCTAContainer,
   HeroH1,
-  CTALinkOrButton,
+  HeroSection,
   ProgressDots,
   Section,
-  CourseCard,
-  Breadcrumbs,
-  ErrorSection,
+  SlideList,
+  UnitCard,
 } from '@bluedot/ui';
 import Head from 'next/head';
 import useAxios from 'axios-hooks';
 import { useRouter } from 'next/router';
-import { FaAward, FaStar, FaStopwatch } from 'react-icons/fa6';
+
 import { ROUTES } from '../../../lib/routes';
 import { GetCourseResponse } from '../../api/courses/[courseSlug]';
-import { LandingPageContent } from '../../../components/lander/LandingPageBase';
-import ClassicHero from '../../../components/lander/ClassicHero';
 import MarkdownExtendedRenderer from '../../../components/courses/MarkdownExtendedRenderer';
+import FutureOfAiLander from '../../../components/lander/FutureOfAiLander';
+import { Unit } from '../../../lib/api/db/tables';
+import GraduateSection from '../../../components/homepage/GraduateSection';
 
 const CoursePage = () => {
   const { query: { courseSlug } } = useRouter();
@@ -26,89 +28,85 @@ const CoursePage = () => {
     return 'Invalid course slug';
   }
 
-  if (courseSlug === 'future-of-ai') {
-    return <FutureOfAILander />;
-  }
-
-  return <StandardCoursePage courseSlug={courseSlug} />;
-};
-
-const FutureOfAILander = () => {
-  const ctaUrl = `${ROUTES.courses.url}/future-of-ai/1`;
-
-  return (
-    <>
-      <ClassicHero>
-        <ClassicHero.Title>The AI course that gives you a say in our future.</ClassicHero.Title>
-        <ClassicHero.Subtitle>
-          The AI knowledge you need, explained simply – empowering you to build a future that works for all of us.
-        </ClassicHero.Subtitle>
-        <HeroCTAContainer>
-          <CTALinkOrButton url={ctaUrl}>Start learning for free</CTALinkOrButton>
-        </HeroCTAContainer>
-        <ClassicHero.Features>
-          <ClassicHero.Feature icon={FaStar}>4.7/5 rating</ClassicHero.Feature>
-          <ClassicHero.Feature icon={FaStopwatch}>2 hours</ClassicHero.Feature>
-          <ClassicHero.Feature icon={FaAward}>Get certified</ClassicHero.Feature>
-        </ClassicHero.Features>
-      </ClassicHero>
-      <LandingPageContent ctaUrl={ctaUrl} />
-    </>
-  );
-};
-
-const StandardCoursePage = ({ courseSlug }: { courseSlug: string }) => {
   const [{ data, loading, error }] = useAxios<GetCourseResponse>({
     method: 'get',
     url: `/api/courses/${courseSlug}`,
   });
 
+  
+    return (
+      <div>
+        {loading && <ProgressDots />}
+        {error && <ErrorSection error={error} />}
+        {data?.course && (
+          // Custom lander case for Future of AI
+          courseSlug === 'future-of-ai' ? (
+            <FutureOfAiLander courseData={data}/>
+          ) : (
+            <StandardCoursePage courseData={data} />
+        )
+      )}
+    </div>
+  );
+};
+
+export const CourseUnitsSection = ({ units }: { units: Unit[] }) => {
+  return (
+    <Section title="What you'll learn" titleLevel="h3">
+      <div className="course-serp__content">
+        {/* Units must be sorted to ensure correct order */}
+        <SlideList
+          maxItemsPerSlide={4}
+          minItemWidth={300}
+          className="course-units-section__units"
+        >
+          {units.sort((a, b) => Number(a.unitNumber) - Number(b.unitNumber)).map((unit) => (
+            <div className="max-w-[350px] h-full">
+              <UnitCard
+                key={unit.unitNumber}
+                className="course-units-section__unit h-full"
+                description={unit.description}
+                duration={unit.duration}
+                title={unit.title}
+                unitNumber={unit.unitNumber}
+                url={unit.path}
+              />
+            </div>
+          ))}
+        </SlideList>
+      </div>
+    </Section>
+  );
+};
+
+const StandardCoursePage = ({ courseData }: { courseData: GetCourseResponse }) => {
   return (
     <div>
-      {loading && <ProgressDots />}
-      {error && <ErrorSection error={error} />}
-      {data?.course && (
+      {courseData?.course && (
         <>
           <Head>
-            <title>{`${data?.course.title} | BlueDot Impact`}</title>
-            <meta name="description" content={data?.course.description} />
+            <title>{`${courseData?.course.title} | BlueDot Impact`}</title>
+            <meta name="description" content={courseData?.course.description} />
           </Head>
           <HeroSection>
-            <HeroH1>{data.course.title}</HeroH1>
-            <MarkdownExtendedRenderer className="invert my-8">{data.course.description}</MarkdownExtendedRenderer>
-            {data.units?.[0]?.path && (
+            <HeroH1>{courseData.course.title}</HeroH1>
+            <MarkdownExtendedRenderer className="invert my-8">{courseData.course.description}</MarkdownExtendedRenderer>
+            {courseData.units?.[0]?.path && (
             <HeroCTAContainer>
-              <CTALinkOrButton url={data.units[0].path}>Browse the curriculum for free</CTALinkOrButton>
+              <CTALinkOrButton url={courseData.units[0].path}>Browse the curriculum for free</CTALinkOrButton>
             </HeroCTAContainer>
             )}
           </HeroSection>
           <Breadcrumbs
             className="course-serp__breadcrumbs"
             route={{
-              title: data.course.title,
-              url: data.course.path,
+              title: courseData.course.title,
+              url: courseData.course.path,
               parentPages: [ROUTES.home, ROUTES.courses],
             }}
           />
-          <Section
-            className="course-serp"
-          >
-            {loading && <ProgressDots />}
-            <div className="course-serp__content grid sm:grid-cols-2 lg:grid-cols-4 gap-space-between items-stretch">
-              {/* Units must be sorted to ensure correct order */}
-              {data?.units?.sort((a, b) => Number(a.unitNumber) - Number(b.unitNumber)).map((unit) => (
-                <div className="max-w-[350px]">
-                  <CourseCard
-                    key={unit.unitNumber}
-                    title={`Unit ${unit.unitNumber}: ${unit.title}`}
-                    url={unit.path}
-                    courseType="Self-paced"
-                    courseLength={`${unit.duration} mins`}
-                  />
-                </div>
-              ))}
-            </div>
-          </Section>
+          <GraduateSection />
+          <CourseUnitsSection units={courseData.units} />
         </>
       )}
     </div>
