0659a9625e83ee0f1fd8d398e6309c7237bf480f
Adam Jones <domdomegg+git@gmail.com>
2025-04-29 15:21:26 +0100

    website: blog-specific fast-follows

--- apps/website/src/pages/blog/index.tsx
+++ apps/website/src/pages/blog/index.tsx
@@ -1,25 +1,15 @@
 import {
-  Section,
   Breadcrumbs,
-  ErrorSection,
-  ProgressDots,
   HeroSection,
 } from '@bluedot/ui';
 import Head from 'next/head';
-import useAxios from 'axios-hooks';
 import { HeroH1, HeroMiniTitle } from '@bluedot/ui/src/HeroSection';
 import { ROUTES } from '../../lib/routes';
-import { GetBlogsResponse } from '../api/cms/blogs';
 import BlogListSection from '../../components/blog/BlogListSection';
 
 const CURRENT_ROUTE = ROUTES.blog;
 
 const BlogPage = () => {
-  const [{ data, loading, error }] = useAxios<GetBlogsResponse>({
-    method: 'get',
-    url: '/api/cms/blogs',
-  });
-
   return (
     <div>
       <Head>
@@ -31,9 +21,7 @@ const BlogPage = () => {
         <HeroH1>Insights on our work and focus areas</HeroH1>
       </HeroSection>
       <Breadcrumbs route={CURRENT_ROUTE} />
-      {loading && <Section title="Blog Articles"><ProgressDots /></Section>}
-      {error && <ErrorSection error={error} />}
-      {data?.blogs && <BlogListSection blogs={data.blogs} />}
+      <BlogListSection />
     </div>
   );
 };
