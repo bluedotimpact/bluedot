3121f7e19e5bd6ced3158ce0c0b8349ee0e73adf
Adam Jones <domdomegg+git@gmail.com>
2025-04-29 15:30:00 +0100

    website: blog-specific fast-follows (#834)

--- apps/website/src/components/blog/BlogListSection.tsx
+++ apps/website/src/components/blog/BlogListSection.tsx
@@ -1,32 +1,55 @@
-import { Card, CTALinkOrButton, Section } from '@bluedot/ui';
+import {
+  Card, CTALinkOrButton, ErrorSection, ProgressDots, Section,
+} from '@bluedot/ui';
 import { isMobile } from 'react-device-detect';
+import useAxios from 'axios-hooks';
 import { P } from '../Text';
 import { CmsBlog } from '../../lib/api/db/tables';
+import { GetBlogsResponse } from '../../pages/api/cms/blogs';
+import { ROUTES } from '../../lib/routes';
 
 export type BlogListSectionProps = {
-  blogs: Omit<CmsBlog, 'body'>[]
+  maxItems?: number | undefined,
 };
 
-const BlogListSection = ({ blogs }: BlogListSectionProps) => {
+const BlogListSection = ({ maxItems }: BlogListSectionProps) => {
+  const [{ data, loading, error }] = useAxios<GetBlogsResponse>({
+    method: 'get',
+    url: '/api/cms/blogs',
+  });
+
+  if (error) {
+    return <ErrorSection error={error} />;
+  }
+
+  if (loading) {
+    return <Section title="Blog Articles"><ProgressDots /></Section>;
+  }
+
   return (
     <Section className="blog-list-section" title="Latest articles">
       <div id="blog-articles-anchor" className="invisible relative bottom-48" />
-      {blogs.length === 0 ? (
+      {data?.blogs.length === 0 ? (
         <P>
           No blog posts available at the moment.
         </P>
       ) : (
         <div className="blog-list__container flex flex-col gap-8">
-          {blogs.map((blog) => (
+          {data?.blogs.slice(0, maxItems).map((blog) => (
             <BlogListItem key={blog.id} blog={blog} />
           ))}
         </div>
+      )}
+      {maxItems && data && data.blogs.length > maxItems && (
+        <CTALinkOrButton url={ROUTES.blog.url} variant="secondary" withChevron className="mt-8">
+          Read more blog articles
+        </CTALinkOrButton>
       )}
     </Section>
   );
 };
 
-const BlogListItem = ({ blog }: {
+export const BlogListItem = ({ blog }: {
   blog: Omit<CmsBlog, 'body'>
 }) => {
   const url = `/blog/${blog.slug}`;
