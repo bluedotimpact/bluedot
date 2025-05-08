fb86925385b0229ed4d49bb72cdd026d470b51f1
Adam Jones <domdomegg+git@gmail.com>
2025-05-08 12:14:11 +0100

    website: Add Organization schema.org markup (#907)

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -14,6 +14,29 @@ const HomePage = () => {
       <Head>
         <title>BlueDot Impact | Industry-leading free AI courses and career support</title>
         <meta name="description" content="Learn for free about AI safety and how to ensure humanity safely navigates the transition to transformative AI. Join 4,000+ professionals building careers at organizations like Anthropic, OpenAI, and the UK’s AI Safety Institute." />
+        <script
+          type="application/ld+json"
+          // eslint-disable-next-line react/no-danger
+          dangerouslySetInnerHTML={{
+            __html: JSON.stringify({
+              '@context': 'https://schema.org',
+              '@type': 'Organization',
+              name: 'BlueDot Impact',
+              url: 'https://bluedot.org',
+              logo: 'https://bluedot.org/images/logo/icon-on-blue.svg',
+              naics: '611430',
+              sameAs: [
+                'https://twitter.com/bluedotimpact',
+                'https://linkedin.com/company/bluedotimpact',
+                'https://github.com/bluedotimpact',
+                'https://www.youtube.com/@bluedotimpact',
+                'https://www.instagram.com/bluedotimpact',
+                'https://www.facebook.com/bluedotimpact',
+                'https://www.tiktok.com/@bluedotimpact',
+              ],
+            }),
+          }}
+        />
       </Head>
       <HeroSection>
         <HomeHeroContent />
