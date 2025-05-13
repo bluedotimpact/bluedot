777673e0cc43c63ab75c7544e45af680a5ed0974
tarinrickett <trickett96@gmail.com>
2025-05-13 17:17:26 +0000

    Update ValuesSection to include new graphics

--- apps/website/src/components/join-us/ValuesSection.test.tsx
+++ apps/website/src/components/join-us/ValuesSection.test.tsx
@@ -1,10 +1,18 @@
 import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import { describe, expect, test, vi } from 'vitest';
+import * as deviceDetect from 'react-device-detect';
 import ValuesSection from './ValuesSection';
 
 describe('ValuesSection', () => {
   test('renders default as expected', () => {
     const { container } = render(<ValuesSection />);
     expect(container).toMatchSnapshot();
+  });
+
+  test('renders mobile as expected', () => {
+    vi.spyOn(deviceDetect, 'isMobile', 'get').mockReturnValue(true);
+    const { container } = render(<ValuesSection />);
+    expect(container).toMatchSnapshot();
+    vi.clearAllMocks();
   });
 });
