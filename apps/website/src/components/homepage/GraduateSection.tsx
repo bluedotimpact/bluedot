d92fd91e1a7dbcd29ba20bb716c1775198efa15f
tarinrickett <trickett96@gmail.com>
2025-05-13 16:19:02 +0000

    Update GraduateSection with new logos

--- apps/website/src/components/homepage/GraduateSection.tsx
+++ apps/website/src/components/homepage/GraduateSection.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import { FaceTiles, Section } from '@bluedot/ui';
 import { P } from '../Text';
 
@@ -5,6 +6,21 @@ const faces = [
   { src: '/images/graduates/matthew.png', alt: 'Matthew' },
   { src: '/images/graduates/sarah.png', alt: 'Sarah' },
   { src: '/images/graduates/kendrea.png', alt: 'Kendrea' },
+];
+
+const logos = [
+  { src: '/images/third-party-logos/openai.svg', alt: 'OpenAI' },
+  { src: '/images/third-party-logos/', alt: 'AI Security Institute' },
+  { src: '/images/third-party-logos/', alt: 'United Nations' },
+  { src: '/images/third-party-logos/anthropic.svg', alt: 'Anthropic', customClassName: '!h-3' },
+  { src: '/images/third-party-logos/', alt: 'Amnesty International' },
+  { src: '/images/third-party-logos/', alt: 'Time' },
+  { src: '/images/third-party-logos/deepmind.svg', alt: 'Google DeepMind' },
+  { src: '/images/third-party-logos/', alt: 'NATO' },
+  { src: '/images/third-party-logos/', alt: 'OECD' },
+  { src: '/images/third-party-logos/', alt: 'Stanford University: Human Centered Artificial Intelligence Institute' },
+  { src: '/images/third-party-logos/', alt: 'Apple' },
+  { src: '/images/third-party-logos/', alt: 'Harvard Kennedy School' },
 ];
 
 const GraduateSection = () => {
@@ -17,20 +33,14 @@ const GraduateSection = () => {
         </div>
         <div className="w-full inline-flex flex-nowrap overflow-hidden [mask-image:_linear-gradient(to_right,transparent_0,_black_128px,_black_calc(100%-200px),transparent_100%)]">
           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll list-none">
-            <li><img className="h-3" src="/images/third-party-logos/anthropic.svg" alt="Anthropic" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/exrisk.svg" alt="Centre for the Study of Existential Risk" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/openai.svg" alt="OpenAI" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/deepmind.svg" alt="Google DeepMind" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/govai.png" alt="Centre for Governance of AI" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/metr.png" alt="METR" /></li>
+            {logos.map((logo) => (
+              <li key={logo.src}><img className={clsx('h-6', logo.customClassName)} src={logo.src} alt={logo.alt} /></li>
+            ))}
           </ul>
           <ul className="flex items-center justify-center md:justify-start [&_li]:mx-8 [&_img]:max-w-none animate-infinite-scroll list-none" aria-hidden="true">
-            <li><img className="h-3" src="/images/third-party-logos/anthropic.svg" alt="Anthropic" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/exrisk.svg" alt="Centre for the Study of Existential Risk" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/openai.svg" alt="OpenAI" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/deepmind.svg" alt="Google DeepMind" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/govai.png" alt="Centre for Governance of AI" /></li>
-            <li><img className="h-6" src="/images/third-party-logos/metr.png" alt="METR" /></li>
+            {logos.map((logo) => (
+              <li key={logo.src}><img className={clsx('h-6', logo.customClassName)} src={logo.src} alt={logo.alt} /></li>
+            ))}
           </ul>
         </div>
       </div>
