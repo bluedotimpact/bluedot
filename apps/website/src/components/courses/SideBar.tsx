632d8ea8f70cf86caebd4d6d749114a2f02fbe46
Tarin <tarin@bluedot.org>
2025-05-20 11:16:30 -0700

    Course UI Redesign

--- apps/website/src/components/courses/SideBar.tsx
+++ apps/website/src/components/courses/SideBar.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
-import { UnitCard } from '@bluedot/ui';
+import clsx from 'clsx';
+
+import { FaChevronRight } from 'react-icons/fa6';
 import { Unit } from '../../lib/api/db/tables';
 
 type SideBarProps = {
@@ -8,6 +10,39 @@ type SideBarProps = {
   currentUnitNumber: number;
 };
 
+type SideBarCollapsibleProps = {
+  unit: Unit;
+  isCurrentUnit: boolean;
+};
+
+const SideBarCollapsible: React.FC<SideBarCollapsibleProps> = ({
+  unit,
+  isCurrentUnit,
+}) => {
+  return (
+    <details
+      open={isCurrentUnit}
+      className="sidebar-collapsible max-w-max-width border-b border-color-divider last:border-b-0 group marker:hidden [&_summary::-webkit-details-marker]:hidden"
+    >
+      <summary className={clsx('sidebar-collapsible__header flex justify-between w-full p-4 text-left group-open:border-b border-color-divider', unit.menuText && 'cursor-pointer')}>
+        <p className="sidebar-collapsible__title font-bold">{unit.unitNumber}. {unit.title}</p>
+        {unit.menuText && (
+          <span className="sidebar-collapsible__button flex items-center">
+            <FaChevronRight className="size-3 transition-transform group-open:rotate-90" />
+          </span>
+        )}
+      </summary>
+      {unit.menuText && (
+        <a
+          className={clsx('sidebar-collapsible__content block p-4 hover:bg-bluedot-lightest', isCurrentUnit && 'border-l-4 border-color-primary bg-bluedot-lightest')}
+          href={unit.path}
+        >
+          {unit.menuText}
+        </a>
+      )}
+    </details>
+  );
+};
 const SideBar: React.FC<SideBarProps> = ({
   units,
   currentUnitNumber,
@@ -16,15 +51,18 @@ const SideBar: React.FC<SideBarProps> = ({
     return currentUnitNumber === Number(unit.unitNumber);
   };
   return (
-    <div className="sidebar w-full md:w-[332px] flex flex-col">
-      <div className="sidebar__content flex flex-col gap-6">
+    <div className="sidebar w-full md:w-[320px] flex flex-col">
+      <div className="sidebar__header-container flex flex-row gap-2 pb-6">
+        <img src="/icons/course.svg" className="size-11" alt="" />
+        <div className="sidebar__title-container flex flex-col">
+          <p className="sidebar__course-header text-size-sm text-charcoal-light">Course</p>
+          <p className="sidebar__course-title bluedot-h4">The Future of AI</p>
+        </div>
+      </div>
+      <div className="sidebar__content flex flex-col container-lined bg-white">
         {units.map((unit) => (
-          <UnitCard
-            key={unit.unitNumber}
-            description={unit.menuText}
-            title={unit.title}
-            unitNumber={unit.unitNumber}
-            url={unit.path}
+          <SideBarCollapsible
+            unit={unit}
             isCurrentUnit={isCurrentUnit(unit)}
           />
         ))}
--- apps/website/src/components/courses/UnitLayout.tsx
+++ apps/website/src/components/courses/UnitLayout.tsx
@@ -1,14 +1,14 @@
 import React from 'react';
+import Head from 'next/head';
 import { isMobile } from 'react-device-detect';
+import clsx from 'clsx';
 import {
-  HeroSection,
-  HeroH1,
   Section,
   CTALinkOrButton,
   Breadcrumbs,
 } from '@bluedot/ui';
-import { HeroMiniTitle } from '@bluedot/ui/src/HeroSection';
-import Head from 'next/head';
+import { FaArrowRight, FaArrowLeft } from 'react-icons/fa6';
+
 import SideBar from './SideBar';
 import { Unit } from '../../lib/api/db/tables';
 import MarkdownExtendedRenderer from './MarkdownExtendedRenderer';
@@ -16,12 +16,55 @@ import Congratulations from './Congratulations';
 import { ROUTES } from '../../lib/routes';
 import UnitFeedback from './UnitFeedback';
 import CertificateLinkCard from './CertificateLinkCard';
+import { A, H1, P } from '../Text';
 
 type UnitLayoutProps = {
   // Required
   unit: Unit;
   unitNumber: number;
   units: Unit[];
+};
+
+type MobileHeaderProps = {
+  className?: string;
+  unit: Unit;
+  prevUnit?: Unit;
+  nextUnit?: Unit;
+};
+
+const MobileHeader: React.FC<MobileHeaderProps> = ({
+  className,
+  unit,
+  prevUnit,
+  nextUnit,
+}) => {
+  return (
+    <div className={clsx('mobile-unit-header bg-color-canvas border-b border-color-divider w-full p-3', className)}>
+      <nav className="mobile-unit-header__nav flex flex-row justify-between">
+        <div className="mobile-unit-header__prev-unit-container size-8">
+          {prevUnit && (
+            <A className="mobile-unit-header__prev-unit-cta flex flex-row items-center gap-1 no-underline" href={prevUnit?.path} aria-label="Previous unit">
+              <img src="/icons/bubble-arrow.svg" alt="Previous unit" className="size-8" />
+            </A>
+          )}
+        </div>
+        <div className="mobile-unit-header__course-container flex flex-row gap-2 items-center">
+          <img src="/icons/course.svg" className="size-8" alt="" />
+          <div className="mobile-unit-header__course-title-container flex flex-col">
+            <p className="mobile-unit-header__course-header text-size-xxs text-charcoal-light">{unit.courseTitle}</p>
+            <p className="mobile-unit-header__course-title bluedot-h4 text-size-xs">{unit.title}</p>
+          </div>
+        </div>
+        <div className="mobile-unit-header__next-unit-container size-8">
+          {nextUnit && (
+            <A className="mobile-unit-header__next-unit-cta flex flex-row items-center gap-1 no-underline" href={nextUnit?.path} aria-label="Next unit">
+              <img src="/icons/bubble-arrow.svg" alt="Next unit" className="size-8 rotate-180" />
+            </A>
+          )}
+        </div>
+      </nav>
+    </div>
+  );
 };
 
 const UnitLayout: React.FC<UnitLayoutProps> = ({
@@ -44,24 +87,41 @@ const UnitLayout: React.FC<UnitLayoutProps> = ({
         <title>{`${unit.courseTitle}: Unit ${unitNumber}`}</title>
         <meta name="description" content={unit.title} />
       </Head>
-      <HeroSection className="unit__hero">
-        <HeroMiniTitle>{unit.courseTitle}</HeroMiniTitle>
-        <HeroH1>{unit.title}</HeroH1>
-      </HeroSection>
+
       <Breadcrumbs
-        className="unit__breadcrumbs sticky top-[72px] md:top-[100px] z-10"
+        className="unit__breadcrumbs hidden md:block md:sticky md:top-[100px] z-10"
         route={{
           title: unit.courseTitle,
           url: unit.coursePath,
           parentPages: [ROUTES.home, ROUTES.courses],
         }}
-      />
+      >
+        <div className="unit__breadcrumbs-cta-container flex flex-row items-center gap-6">
+          {prevUnit && (
+            <A className="unit__breadcrumbs-cta flex flex-row items-center gap-1 no-underline" href={prevUnit?.path} aria-label="Previous unit">
+              <FaArrowLeft className="size-3" /> Prev
+            </A>
+          )}
+          {nextUnit && (
+            <A className="unit__breadcrumbs-cta flex flex-row items-center gap-1 no-underline" href={nextUnit?.path} aria-label="Next unit">
+              Next <FaArrowRight className="size-3" />
+            </A>
+          )}
+        </div>
+      </Breadcrumbs>
+
+      <MobileHeader className="unit__mobile-header md:hidden sticky top-[72px] z-10" unit={unit} prevUnit={prevUnit} nextUnit={nextUnit} />
+
       <Section className="unit__main">
-        <div className="unit__content-container flex flex-col md:flex-row gap-16">
+        <div className="unit__content-container flex flex-col md:flex-row">
           {!isMobile && (
             <SideBar units={units} currentUnitNumber={unitNumber} />
           )}
-          <div className="unit__content flex flex-col flex-1 max-w-[728px] gap-6">
+          <div className="unit__content flex flex-col flex-1 max-w-[680px] mx-auto gap-6">
+            <div className="unit__title-container">
+              <P className="unit__course-title text-size-sm mb-2">Unit {unit.unitNumber}</P>
+              <H1 className="unit__title font-serif text-[32px]">{unit.title}</H1>
+            </div>
             <MarkdownExtendedRenderer>
               {unit.content}
             </MarkdownExtendedRenderer>
@@ -80,16 +140,9 @@ const UnitLayout: React.FC<UnitLayoutProps> = ({
               </>
             ) : (
               <div className="unit__cta-container flex flex-row justify-between mt-6 mx-1">
-                {prevUnit && (
-                  <CTALinkOrButton className="unit__cta-link mr-auto" url={prevUnit.path} variant="secondary" withBackChevron>
-                    Previous unit
-                  </CTALinkOrButton>
-                )}
-                {nextUnit && (
-                  <CTALinkOrButton className="unit__cta-link ml-auto" url={nextUnit.path} variant="primary" withChevron>
-                    Next unit
-                  </CTALinkOrButton>
-                )}
+                <CTALinkOrButton className="unit__cta-link ml-auto" url={nextUnit.path} variant="primary" withChevron>
+                  Complete and continue
+                </CTALinkOrButton>
               </div>
             )}
           </div>
