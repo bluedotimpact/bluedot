7d83df21ab8018993a4c3c00e65e9a6653d488c8
Adam Jones <domdomegg+git@gmail.com>
2025-06-17 19:58:22 +0100

    postgres-demo: Delete app (#1037)
    All the functionality that it demonstrates is also visible in app-template now after #1027

--- apps/postgres-demo/src/lib/api/db/index.ts
+++
@@ -1,4 +0,0 @@
-import { PgAirtableDb } from '@bluedot/db';
-import env from '../env';
-
-export const db = new PgAirtableDb({ pgConnString: env.PG_URL, airtableApiKey: env.AIRTABLE_PERSONAL_ACCESS_TOKEN });
