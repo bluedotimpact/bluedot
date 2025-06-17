d4c2c3ded16499464f3ae7c3f67850312b213775
Adam Jones <domdomegg+git@gmail.com>
2025-06-17 16:47:03 +0100

    course-demos: Move onto postgres read replica

--- apps/course-demos/src/lib/api/db.ts
+++ apps/course-demos/src/lib/api/db.ts
@@ -1,32 +1,10 @@
-import { AirtableTs, Item, Table } from 'airtable-ts';
+import { PgAirtableDb } from '@bluedot/db';
 import { z } from 'zod';
 import env from './env';
 
-export default new AirtableTs({
-  apiKey: env.AIRTABLE_PERSONAL_ACCESS_TOKEN,
+export default new PgAirtableDb({
+  pgConnString: env.PG_URL,
+  airtableApiKey: env.AIRTABLE_PERSONAL_ACCESS_TOKEN,
 });
 
 export const demoTypes = z.literal('generate-react-component');
-
-export type SharedDemoOutput = {
-  id: string,
-  type: z.infer<typeof demoTypes>,
-  data: string,
-  createdAt: number,
-} & Item;
-
-export const sharedDemoOutputTable: Table<SharedDemoOutput> = {
-  name: 'SharedComponents',
-  baseId: 'appPs3sb9BrYZN69z',
-  tableId: 'tbl9WE3N4B0OjocEB',
-  mappings: {
-    type: 'fldpAHTnZOJc8wbFV',
-    data: 'fldsh10gNDXfkosfJ',
-    createdAt: 'fldNfaTMCepyRY3Nj',
-  },
-  schema: {
-    type: 'string',
-    data: 'string',
-    createdAt: 'number',
-  },
-};
