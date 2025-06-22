b17def48bfdea480b8bdeb98291975ec1df35046
Martin-C-Listwan <171297502+Martin-C-Listwan@users.noreply.github.com>
2025-06-22 13:32:15 +0300

    Removed regex requirement for name

--- apps/website/src/lib/schemas/user/me.schema.ts
+++ apps/website/src/lib/schemas/user/me.schema.ts
@@ -3,9 +3,8 @@ import z from 'zod';
 export const meRequestBodySchema = z.object({
   name: z.string()
     .trim()
-  // 50 characters for a name seemed reasonable
+    // 50 characters for a name seemed reasonable
     .max(50, 'Name must be under 50 characters')
-    .regex(/^[\p{L}\s\-'.]+$/u, 'Name can only contain letters, spaces, hyphens, apostrophes, and periods')
     .optional(),
   referredById: z.string()
     .trim()
