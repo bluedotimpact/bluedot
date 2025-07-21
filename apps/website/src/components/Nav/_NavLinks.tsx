bdcf636ad1033623336f7e747f379ef9064ccee3
tolgadur <mail@tolgadur.com>
2025-07-21 15:31:05 +0000

    feat: add were hiring to navbar

--- apps/website/src/components/Nav/_NavLinks.tsx
+++ apps/website/src/components/Nav/_NavLinks.tsx
@@ -78,6 +78,7 @@ export const NavLinks: React.FC<{
       />
       <A href={ROUTES.blog.url} className={NAV_LINK_CLASSES(isScrolled, isCurrentPath(ROUTES.blog.url))}>Blog</A>
       <A href="https://lu.ma/aisafetycommunityevents?utm_source=website&utm_campaign=nav" className={NAV_LINK_CLASSES(isScrolled)}>Events</A>
+      <A href={ROUTES.joinUs.url} className={clsx(NAV_LINK_CLASSES(isScrolled), 'text-bluedot-primary font-medium')}>We're hiring!</A>
     </div>
   );
 };
