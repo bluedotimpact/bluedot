b1c0ebf680952a6fbddbec0c1348ba1762cd322f
Martin <martinc.listwan@gmail.com>
2025-08-06 17:17:23 -0400

    [Bug] Inter wasn't being used on the main website (#1171)
    * Inter is now used on the website as expected
    * Made the UI library 650 font globally convert to 600 as per figma design

--- apps/website/src/pages/_app.tsx
+++ apps/website/src/pages/_app.tsx
@@ -10,6 +10,7 @@ import { AnnouncementBanner } from '../components/AnnouncementBanner';
 import { CookieBanner } from '../components/CookieBanner';
 import { CircleWidget } from '../components/community/CircleWidget';
 import { useCourses } from '../lib/hooks/useCourses';
+import { inter } from '../lib/fonts';
 
 const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
   const fromSiteParam = useRouter().query.from_site as string;
@@ -19,43 +20,45 @@ const App: React.FC<AppProps> = ({ Component, pageProps }: AppProps) => {
 
   return (
     <PostHogProvider>
-      <Head>
-        <title>BlueDot Impact</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
-        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
-      </Head>
-      {'rawLayout' in Component && Component.rawLayout
-        ? <Component {...pageProps} />
-        : (
-          <>
-            <Nav logo="/images/logo/BlueDot_Impact_Logo.svg" />
-            {fromSite && (
-              <AnnouncementBanner ctaText="Learn more" ctaUrl="/blog/course-website-consolidation">
-                <b>Welcome from {fromSite === 'aisf' ? 'AI Safety Fundamentals' : 'Biosecurity Fundamentals'}!</b> We've consolidated our course sites in the BlueDot Impact platform to provide a more consistent and higher-quality experience.
+      <div className={inter.className}>
+        <Head>
+          <title>BlueDot Impact</title>
+          <link rel="icon" href="/favicon.ico" />
+          <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png" />
+        </Head>
+        {'rawLayout' in Component && Component.rawLayout
+          ? <Component {...pageProps} />
+          : (
+            <>
+              <Nav logo="/images/logo/BlueDot_Impact_Logo.svg" />
+              {fromSite && (
+                <AnnouncementBanner ctaText="Learn more" ctaUrl="/blog/course-website-consolidation">
+                  <b>Welcome from {fromSite === 'aisf' ? 'AI Safety Fundamentals' : 'Biosecurity Fundamentals'}!</b> We've consolidated our course sites in the BlueDot Impact platform to provide a more consistent and higher-quality experience.
+                </AnnouncementBanner>
+              )}
+              <AnnouncementBanner ctaText="Reserve your free spot" ctaUrl="https://lu.ma/sa52ofdf?utm_source=website&utm_campaign=banner" hideAfter={new Date('2025-04-25T18:30:00+01:00')}>
+                <b>Don't miss this Friday: </b>Planning a career in the age of A(G)I - an online panel with Luke Drago, Josh Landes & Ben Todd
               </AnnouncementBanner>
-            )}
-            <AnnouncementBanner ctaText="Reserve your free spot" ctaUrl="https://lu.ma/sa52ofdf?utm_source=website&utm_campaign=banner" hideAfter={new Date('2025-04-25T18:30:00+01:00')}>
-              <b>Don't miss this Friday: </b>Planning a career in the age of A(G)I - an online panel with Luke Drago, Josh Landes & Ben Todd
-            </AnnouncementBanner>
-            <main className="bluedot-base">
-              <Component {...pageProps} />
-            </main>
-            {!hideFooter && (
-              <Footer
-                courses={courses.map((course) => ({
-                  path: course.path,
-                  title: course.title,
-                }))}
-                loading={loading}
-                logo="/images/logo/BlueDot_Impact_Logo_White.svg"
-              />
-            )}
-          </>
-        )}
-      <CookieBanner />
-      <GoogleTagManager />
-      <CircleWidget />
+              <main className="bluedot-base">
+                <Component {...pageProps} />
+              </main>
+              {!hideFooter && (
+                <Footer
+                  courses={courses.map((course) => ({
+                    path: course.path,
+                    title: course.title,
+                  }))}
+                  loading={loading}
+                  logo="/images/logo/BlueDot_Impact_Logo_White.svg"
+                />
+              )}
+            </>
+          )}
+        <CookieBanner />
+        <GoogleTagManager />
+        <CircleWidget />
+      </div>
     </PostHogProvider>
   );
 };
