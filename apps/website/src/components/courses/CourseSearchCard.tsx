e647b11b1d58d7073a8c0125baf6fb86b1906941
Dewi Erwan <dewierwan@gmail.com>
2025-08-12 16:55:10 +0000

    Feat: Fixing and simplifying the courses directory

--- apps/website/src/components/courses/CourseSearchCard.tsx
+++ apps/website/src/components/courses/CourseSearchCard.tsx
@@ -36,42 +36,34 @@ export const CourseSearchCard: React.FC<CourseSearchCardProps> = ({
     <ClickTarget
       url={url}
       className={clsx(
-        'course-search-card flex flex-col gap-6 container-lined p-6 max-w-[828px] size-full transition-transform duration-200 hover:scale-[1.01] hover:container-elevated',
+        'course-search-card flex flex-col container-lined p-6 max-w-[828px] size-full transition-transform duration-200 hover:scale-[1.01] hover:container-elevated',
         className,
       )}
     >
-      <div className="course-search-card__image-container h-[200px] w-full">
+      <div className="course-search-card__image-container h-[200px] w-full mb-4">
         <img className="course-search-card__image size-full object-cover rounded-lg" src={imageSrc || defaultImageSrc} alt={title} />
       </div>
-      <div className="course-search-card__content flex flex-col justify-between gap-space-between flex-1">
-        <div className="course-search-card__text">
-          <H3 className="course-search-card__title mb-4">
+      <div className="course-search-card__content flex flex-col flex-1">
+        <div className="course-search-card__text flex-1">
+          <H3 className="course-search-card__title mb-3">
             {title}
           </H3>
           {description && (
-            <P className="course-search-card__description overflow-hidden text-bluedot-black text-ellipsis mb-4">
+            <P className="course-search-card__description text-bluedot-black">
               {description}
             </P>
           )}
         </div>
-        <div className="course-search-card__footer flex flex-wrap gap-4">
-          <div className="flex gap-2">
-            {courseLength && (
-              <Tag className="course-search-card__level">{courseLength}</Tag>
-            )}
-            {cadence && (
-              <Tag className="course-search-card__duration">{cadence}</Tag>
-            )}
-          </div>
-          {typeof averageRating === 'number' && (
-            <div className="course-card__footer-left ml-auto text-size-xs text-bluedot-black gap-1 flex flex-col items-end justify-end">
-              <span className="course-card__rating flex gap-[3px] items-start font-bold pb-2">
+        {typeof averageRating === 'number' && (
+          <div className="course-search-card__footer flex justify-end mt-4">
+            <div className="course-card__footer-left text-size-xs text-bluedot-black gap-1 flex flex-col items-end">
+              <span className="course-card__rating flex gap-[3px] items-start font-bold">
                 {displayAverageRating(averageRating)}{' '}
                 <FaStar className="mt-px" />
               </span>
             </div>
-          )}
-        </div>
+          </div>
+        )}
       </div>
       {children}
     </ClickTarget>
