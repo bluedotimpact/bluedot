9ced83e36a90e3c89616f141f6f57441d7f819be
Will Howard <w.howard256+github@gmail.com>
2025-08-21 23:09:58 +0100

    Fix type of unitNumber in groupDiscussionTable (#1222)

--- apps/website/src/pages/api/courses/[courseSlug]/[unitNumber]/groupDiscussion.ts
+++ apps/website/src/pages/api/courses/[courseSlug]/[unitNumber]/groupDiscussion.ts
@@ -38,7 +38,7 @@ export default makeApiRoute({
       and(
         sql`${groupDiscussionTable.pg.participantEmailsExpected} @> ARRAY[${auth.email}]`,
         sql`${groupDiscussionTable.pg.courseSite} ~ ${`${courseSlug}/?$`}`,
-        eq(groupDiscussionTable.pg.unitNumber, unitNumber),
+        eq(groupDiscussionTable.pg.unitNumber, parseInt(unitNumber, 10)),
         sql`${groupDiscussionTable.pg.endDateTime} > ${cutoffTimeSeconds}`,
       ),
     )
