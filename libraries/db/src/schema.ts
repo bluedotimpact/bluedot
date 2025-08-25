f5a1c33ef82a065f8e838d13a9fb11c39faa883c
Will Howard <w.howard256+github@gmail.com>
2025-08-25 19:47:21 +0200

    More schema changes to support group switching

--- libraries/db/src/schema.ts
+++ libraries/db/src/schema.ts
@@ -261,6 +261,10 @@ export const groupTable = pgAirtable('group', {
   baseId: COURSE_RUNNER_BASE_ID,
   tableId: 'tblyiJSPoniwhi17T',
   columns: {
+    groupName: {
+      pgColumn: text().notNull(),
+      airtableId: 'fldv3jHyWGjR0LxLp',
+    },
     groupDiscussions: {
       pgColumn: text().array().notNull(),
       airtableId: 'fldwEeC65sHvGGRGb',
@@ -268,6 +272,14 @@ export const groupTable = pgAirtable('group', {
     round: {
       pgColumn: text().notNull(),
       airtableId: 'fldtzy3nSP0piVApO',
+    },
+    participants: {
+      pgColumn: text().array().notNull(),
+      airtableId: 'fldcEa25oCDAmgDqm',
+    },
+    whoCanSwitchIntoThisGroup: {
+      pgColumn: text().array().notNull(),
+      airtableId: 'fldVQihgKyx6nJIR5',
     },
   },
 });
@@ -311,6 +323,10 @@ export const groupDiscussionTable = pgAirtable('group_discussion', {
     unitNumber: {
       pgColumn: numeric({ mode: 'number' }),
       airtableId: 'fldbNYACt7S5J2QlU',
+    },
+    unit: {
+      pgColumn: text(),
+      airtableId: 'fldVo5h9rqsEeGSRU',
     },
     zoomLink: {
       pgColumn: text(),
@@ -378,6 +394,21 @@ export const groupSwitchingTable = pgAirtable('group_switching', {
   },
 });
 
+export const courseRunnerBucketTable = pgAirtable('course_runner_bucket', {
+  baseId: COURSE_RUNNER_BASE_ID,
+  tableId: 'tbl7Pevw79fDA7EmX',
+  columns: {
+    groups: {
+      pgColumn: text().array().notNull(),
+      airtableId: 'flduxhuBAiLVdfolQ',
+    },
+    round: {
+      pgColumn: text(),
+      airtableId: 'fld0afnDgsNXYim7V',
+    },
+  },
+});
+
 // Note: This is actually a sync of the "Course registration" table
 // from APPLICATIONS_BASE_ID, rather than the "User" table
 export const meetPersonTable = pgAirtable('meet_person', {
@@ -421,6 +452,10 @@ export const roundTable = pgAirtable('round', {
     course: {
       pgColumn: text().notNull(),
       airtableId: 'fldvx7D6Uw0VxMPr0',
+    },
+    maxParticipantsPerGroup: {
+      pgColumn: numeric({ mode: 'number' }),
+      airtableId: 'fldoIzHNm8NzjAefW',
     },
   },
 });
