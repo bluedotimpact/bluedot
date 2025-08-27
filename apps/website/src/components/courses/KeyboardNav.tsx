6852b8c10d515470d0b8d49a7d99ecc9cfb85bd4
Josh Stein <josh.e.stein@gmail.com>
2025-08-27 14:12:45 +0200

    Color transitions, duration-200

--- apps/website/src/components/courses/KeyboardNav.tsx
+++ apps/website/src/components/courses/KeyboardNav.tsx
@@ -12,7 +12,7 @@ const shortcuts = [
 const KeyboardNav = () => {
   return (
     <DialogTrigger>
-      <Button className="flex cursor-pointer items-center justify-center gap-1.5 rounded-md bg-gray-200 p-2 hover:bg-gray-300">
+      <Button className="transition-color flex cursor-pointer items-center justify-center gap-1.5 rounded-md bg-gray-200 p-2 duration-200 hover:bg-gray-300">
         <FiCommand className="size-4" />
         Shortcuts
       </Button>
