92a0ab682ed522686ff40867a8c3ad2e6a5bd072
Ang Li-Lian <anglilian@bluedot.org>
2025-08-28 23:03:42 +0100

    Add course builder unit record ID for profile page displaying upcoming discussions

--- libraries/db/src/schema.ts
+++ libraries/db/src/schema.ts
@@ -348,6 +348,10 @@ export const groupDiscussionTable = pgAirtable('group_discussion', {
       pgColumn: text(),
       airtableId: 'fld6z76NthgaFf8EY',
     },
+    courseBuilderUnitRecordId: {
+      pgColumn: text(),
+      airtableId: 'fld87QFyiHceHbpKG',
+    },
   },
 });
 
