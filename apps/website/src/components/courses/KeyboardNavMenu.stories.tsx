f6770966b6885f13505a66c77dce209049359393
Josh Stein <josh.e.stein@gmail.com>
2025-08-28 11:56:14 +0200

    New stories for custom shortcuts and custom title

--- apps/website/src/components/courses/KeyboardNavMenu.stories.tsx
+++ apps/website/src/components/courses/KeyboardNavMenu.stories.tsx
@@ -18,3 +18,19 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {},
 };
+
+export const CustomShortcuts: Story = {
+  args: {
+    shortcuts: [
+      { action: 'Save', keys: ['Ctrl/Cmd', 'S'] },
+      { action: 'Copy', keys: ['Ctrl/Cmd', 'C'] },
+      { action: 'Help', keys: ['?'] },
+    ],
+  },
+};
+
+export const CustomTitle: Story = {
+  args: {
+    popoverTitle: 'Keyboard shortcuts',
+  },
+};
