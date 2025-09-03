bd5f587177a9ad99cfc5222ca118bc26b2c59832
Josh Stein <josh.e.stein@gmail.com>
2025-09-03 11:47:58 +0200

    Prettify

--- apps/website/src/components/courses/FeedbackSection.tsx
+++ apps/website/src/components/courses/FeedbackSection.tsx
@@ -50,9 +50,7 @@ type LikeDislikeProps = {
 const FeedbackSection = ({ leadingText = 'How did you like this unit?' }: LikeDislikeProps) => {
   return (
     <div className="inline-flex items-center gap-4 [--feedback-gray:#13132E]">
-      <span className="text-(--feedback-gray)/60">
-        {leadingText}
-      </span>
+      <span className="text-(--feedback-gray)/60">{leadingText}</span>
       <div className="flex items-center gap-1">
         <CTALinkOrButton className="flex cursor-pointer items-center gap-1.5 !rounded-md bg-white p-2 hover:bg-gray-200 hover:text-gray-500">
           <span className="flex items-center gap-2 text-(--feedback-gray)/60">
