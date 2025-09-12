424adc5ac190cd2ef24e62d645ebca9de9e60152
Josh Stein <josh.e.stein@gmail.com>
2025-09-12 16:08:07 +0200

    Join with newlines

--- apps/website/src/pages/projects/sitemap.xml.ts
+++ apps/website/src/pages/projects/sitemap.xml.ts
@@ -13,7 +13,7 @@ export const getServerSideProps: GetServerSideProps = async ({ res }) => {
     <changefreq>monthly</changefreq>
     <priority>0.7</priority>
   </url>`;
-  });
+  }).join('\n');
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
