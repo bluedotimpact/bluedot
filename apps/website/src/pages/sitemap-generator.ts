4178a6cd963b739fedbdb090e9eac313caa14c07
Josh Stein <josh.e.stein@gmail.com>
2025-09-12 16:26:19 +0200

    Return of home route

--- apps/website/src/pages/sitemap-generator.ts
+++ apps/website/src/pages/sitemap-generator.ts
@@ -42,6 +42,12 @@ export const getStaticProps: GetStaticProps = () => {
 
   const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
 <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+  <url>
+    <loc>${BASE_URL}</loc>
+    <lastmod>${getLastModified('src/pages/index.tsx')}</lastmod>
+    <changefreq>daily</changefreq>
+    <priority>1.0</priority>
+  </url>
 ${urls}
 </urlset>`;
 
