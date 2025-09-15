bda0b4e8c200ef551742b6d8fc7f90da52863991
Josh Stein <josh.e.stein@gmail.com>
2025-09-15 17:10:10 +0200

    Test `createSigninRequest` with `redirect_to` query param

--- libraries/ui/src/Login.test.tsx
+++ libraries/ui/src/Login.test.tsx
@@ -1,4 +1,4 @@
-import { render } from '@testing-library/react';
+import { render, waitFor } from '@testing-library/react';
 import {
   describe,
   test,
@@ -6,6 +6,7 @@ import {
   beforeEach,
   vi,
 } from 'vitest';
+import { OidcClient } from 'oidc-client-ts';
 import { LoginRedirectPage, loginPresets } from './Login';
 import { Navigate } from './Navigate';
 import { useAuthStore } from './utils/auth';
@@ -23,7 +24,21 @@ vi.mock('./utils/getQueryParam', () => ({
   getQueryParam: vi.fn(),
 }));
 
+const mockCreateSigninRequest = vi.fn(() => Promise.resolve({
+  url: OIDC_PROVIDER_URL,
+}));
+
+vi.mock('oidc-client-ts', () => {
+  return {
+    OidcClient: vi.fn().mockImplementation(() => ({
+      createSigninRequest: mockCreateSigninRequest,
+      processSigninResponse: vi.fn(),
+    })),
+  };
+});
+
 const CUSTOM_REDIRECT_PATH = '/custom-path';
+const OIDC_PROVIDER_URL = 'https://mock-oidc-provider.com/';
 
 describe('LoginRedirectPage', () => {
   const mockLoginPreset = loginPresets.keycloak;
@@ -41,10 +56,37 @@ describe('LoginRedirectPage', () => {
     expect(Navigate).toHaveBeenCalledWith({
       url: CUSTOM_REDIRECT_PATH,
     }, expect.anything());
+
+    expect(OidcClient).not.toHaveBeenCalled();
   });
 
-  test.skip('if not authed, should createSigninRequest with with redirect_to query param', async () => {
-    // TODO #720
+  test('if not authed, should createSigninRequest with redirect_to query param', async () => {
+    vi.mocked(useAuthStore).mockReturnValue(null);
+    vi.mocked(getQueryParam).mockImplementation((_url, param) => {
+      if (param === 'redirect_to') {
+        return CUSTOM_REDIRECT_PATH;
+      }
+      return null;
+    });
+
+    render(<LoginRedirectPage loginPreset={mockLoginPreset} />);
+
+    // `waitFor` is needed because createSigninRequest is called in a useEffect
+    await waitFor(() => {
+      expect(OidcClient).toHaveBeenCalledTimes(1);
+      expect(OidcClient).toHaveBeenCalledWith(mockLoginPreset.oidcSettings);
+
+      expect(mockCreateSigninRequest).toHaveBeenCalledTimes(1);
+      expect(mockCreateSigninRequest).toHaveBeenCalledWith({
+        request_type: 'si:r',
+        state: {
+          redirectTo: CUSTOM_REDIRECT_PATH,
+          attribution: expect.any(Object),
+        },
+      });
+
+      expect(window.location.href).toBe(OIDC_PROVIDER_URL);
+    });
   });
 
   test.skip('if not authed and no redirect_to, should createSigninRequest with default redirect path', async () => {
