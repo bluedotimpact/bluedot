e9bdedb8b751de78b600789a5e146ce385a2a9c9
Will Howard <w.howard256+github@gmail.com>
2025-09-17 12:22:40 +0100

    Fix build

--- apps/website/src/components/courses/GroupSwitchModal.tsx
+++ apps/website/src/components/courses/GroupSwitchModal.tsx
@@ -521,7 +521,7 @@ const UserIcon = ({ className }: { className?: string }) => (
 );
 
 type GroupSwitchOptionProps = {
-  id: string;
+  id?: string;
   groupName: string;
   dateTime: number | null;
   spotsLeft: number | null;
