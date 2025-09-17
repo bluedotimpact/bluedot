5285f20b6861623f69ae7a6915e43840fa719bcd
Josh Stein <josh.e.stein@gmail.com>
2025-09-17 16:16:09 +0200

    `getDiscussionSubtext`

--- apps/website/src/components/courses/GroupSwitchModal.tsx
+++ apps/website/src/components/courses/GroupSwitchModal.tsx
@@ -227,6 +227,45 @@ const GroupSwitchModal: React.FC<GroupSwitchModalProps> = ({
     );
   };
 
+  const getDiscussionSubtext = (discussion: GetGroupSwitchingAvailableResponse['discussionsAvailable'][string][number]) => {
+    const baseStyle = 'text-[12px] leading-none font-medium text-[#666C80]';
+
+    if (discussion.userIsParticipant) {
+      if (selectedDiscussionId) {
+        // When there is another discussion selected
+        return <span className={baseStyle}>You are not attending this discussion</span>;
+      }
+      return (
+        <div className="flex items-center gap-1 text-[#0037FF]">
+          <span className={cn(baseStyle, 'text-inherit')}>You are attending this discussion</span>
+          <FaCheck size={10} />
+        </div>
+      );
+    }
+
+    if (discussion.discussion.id === selectedDiscussionId) {
+      return <span className={cn(baseStyle, 'text-[#0037FF]')}>You are joining this group for this unit.</span>;
+    }
+
+    let spotsLeft = 'No spots left';
+    if (discussion.spotsLeft && discussion?.spotsLeft > 0) {
+      spotsLeft = `${discussion.spotsLeft} spot${discussion.spotsLeft > 1 ? 's' : ''} left`;
+    }
+    return (
+      <div className="flex items-center gap-1 text-[#666C80]">
+        <svg xmlns="http://www.w3.org/2000/svg" width="12" height="12" viewBox="0 0 12 12" fill="none">
+          <path
+            d="M10 10.5V9.5C10 8.96957 9.78929 8.46086 9.41421 8.08579C9.03914 7.71071 8.53043 7.5 8 7.5H4C3.46957 7.5 2.96086 7.71071 2.58579 8.08579C2.21071 8.46086 2 8.96957 2 9.5V10.5M8 3.5C8 4.60457 7.10457 5.5 6 5.5C4.89543 5.5 4 4.60457 4 3.5C4 2.39543 4.89543 1.5 6 1.5C7.10457 1.5 8 2.39543 8 3.5Z"
+            stroke="currentColor"
+            strokeLinecap="round"
+            strokeLinejoin="round"
+          />
+        </svg>
+        <span className={baseStyle}>{spotsLeft}</span>
+      </div>
+    );
+  };
+
   return (
     <Modal isOpen setIsOpen={(open: boolean) => !open && handleClose()} title={title} bottomDrawerOnMobile>
       <div className="w-full max-w-[600px]">
