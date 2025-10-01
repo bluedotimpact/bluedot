646f61dfc09237387f04dc62be232c1e08fd3cf4
Will Howard <w.howard256+github@gmail.com>
2025-10-01 13:51:47 +0200

    Revert getAllPublishedJobs change

--- apps/website/src/pages/api/cms/jobs/index.ts
+++ apps/website/src/pages/api/cms/jobs/index.ts
@@ -10,17 +10,6 @@ export type GetJobsResponse = {
   jobs: Omit<CmsJobPosting, 'body'>[],
 };
 
-export const getAllPublishedJobs = async () => {
-  const allJobs = await db.scan(jobPostingTable, { publicationStatus: 'Published' });
-
-  // Sort jobs alphabetically by title and remove the body field from each job to make the response lighter
-  const jobSummaries = allJobs
-    .sort((a, b) => (a.title).localeCompare(b.title))
-    .map(({ body, ...rest }) => rest);
-
-  return jobSummaries;
-};
-
 export default makeApiRoute({
   requireAuth: false,
   responseBody: z.object({
@@ -28,10 +17,15 @@ export default makeApiRoute({
     jobs: z.array(z.any()),
   }),
 }, async () => {
-  const jobs = await getAllPublishedJobs();
+  const allJobs = await db.scan(jobPostingTable, { publicationStatus: 'Published' });
+
+  // Sort jobs alphabetically by title and remove the body field from each job to make the response lighter
+  const jobSummaries = allJobs
+    .sort((a, b) => (a.title).localeCompare(b.title))
+    .map(({ body, ...rest }) => rest);
 
   return {
     type: 'success' as const,
-    jobs,
+    jobs: jobSummaries,
   };
 });
