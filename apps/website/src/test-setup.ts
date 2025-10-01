21cb2716c5b26f9ad2e296b910e214327dfd0bc6
Will Howard <w.howard256+github@gmail.com>
2025-10-01 08:49:24 +0200

    Fix timezone as UTC in test

---
+++ apps/website/src/test-setup.ts
@@ -0,0 +1,6 @@
+// Global test setup file
+// This file is run before all tests in apps/website
+
+// Fix timezone as UTC
+// eslint-disable-next-line turbo/no-undeclared-env-vars
+process.env.TZ = 'UTC';
