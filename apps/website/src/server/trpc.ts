4f0bc0392bbc1eae86d0be3fcc8d7b44aabc8a53
Josh Stein <josh.e.stein@gmail.com>
2025-10-06 18:28:57 +0200

    `protectedProcedure` checks auth

--- apps/website/src/server/trpc.ts
+++ apps/website/src/server/trpc.ts
@@ -1,9 +1,17 @@
-import { initTRPC } from '@trpc/server';
+import { initTRPC, TRPCError } from '@trpc/server';
+import { Context } from './context';
 
 // Avoid exporting the entire t-object since it's not very descriptive.
 // For instance, the use of a t variable is common in i18n libraries.
-const t = initTRPC.create();
+const t = initTRPC.context<Context>().create();
 
 // Base router and procedure helpers
 export const { router } = t;
 export const { procedure: publicProcedure } = t;
+
+export const protectedProcedure = t.procedure.use(({ ctx, next }) => {
+  if (!ctx.auth) {
+    throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Authentication required' });
+  }
+  return next({ ctx: { ...ctx, auth: ctx.auth } });
+});
