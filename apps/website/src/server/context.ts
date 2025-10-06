aee59bc597b7130cfa989d76463e7deb68983f0d
Josh Stein <josh.e.stein@gmail.com>
2025-10-06 18:28:08 +0200

    Start working on auth context middleware

---
+++ apps/website/src/server/context.ts
@@ -0,0 +1,26 @@
+import { loginPresets } from '@bluedot/ui';
+import { TRPCError } from '@trpc/server';
+import * as trpcNext from '@trpc/server/adapters/next';
+
+export const createContext = async ({ req }: trpcNext.CreateNextContextOptions) => {
+  const token = req.headers.authorization?.slice('Bearer '.length).trim();
+
+  let auth: { sub: string, email: string } | null = null;
+
+  if (token) {
+    try {
+      auth = await loginPresets.keycloak.verifyAndDecodeToken(token);
+    } catch (error) {
+      // TODO: do we need this fallback check to googleBlueDot?
+      // try {
+      //   auth = await loginPresets.googleBlueDot.verifyAndDecodeToken(token);
+      // } catch {
+      // }
+      throw new TRPCError({ code: 'UNAUTHORIZED', message: 'Invalid access token' });
+    }
+  }
+
+  return { auth };
+};
+
+export type Context = Awaited<ReturnType<typeof createContext>>;
