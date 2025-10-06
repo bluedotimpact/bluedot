ad87b5431e489e44c8df22cf0b0e35dc38b65ac6
Josh Stein <josh.e.stein@gmail.com>
2025-10-06 13:52:20 +0200

    tRPC hooks

---
+++ apps/website/src/utils/trpc.ts
@@ -0,0 +1,41 @@
+import { httpBatchLink } from '@trpc/client';
+import { createTRPCNext } from '@trpc/next';
+import type { AppRouter } from '../server/routers/_app';
+
+function getBaseUrl() {
+  if (typeof window !== 'undefined') {
+    // browser should use relative path
+    return '';
+  }
+  if (process.env.SITE_URL) {
+    return `https://${process.env.SITE_URL}`;
+  }
+
+  return `http://localhost:${process.env.PORT ?? 3000}`;
+}
+
+export const trpc = createTRPCNext<AppRouter>({
+  config() {
+    return {
+      links: [
+        httpBatchLink({
+          /**
+           * If you want to use SSR, you need to use the server's full URL
+           * @see https://trpc.io/docs/v11/ssr
+           * */
+          url: `${getBaseUrl()}/api/trpc`,
+          // You can pass any HTTP headers you wish here
+          async headers() {
+            return {
+              // authorization: getAuthCookie(),
+            };
+          },
+        }),
+      ],
+    };
+  },
+  /**
+   * @see https://trpc.io/docs/v11/ssr
+   * */
+  ssr: false,
+});
