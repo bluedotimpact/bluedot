c83bb31fff1eb89afca2a53b998ff85da17a264e
Josh Stein <josh.e.stein@gmail.com>
2025-10-07 11:05:41 +0200

    Only send slack alerts for critical errors

--- apps/website/src/pages/api/trpc/[trpc].ts
+++ apps/website/src/pages/api/trpc/[trpc].ts
@@ -12,6 +12,18 @@ export default trpcNext.createNextApiHandler({
     const { error, type, path } = opts;
     const message = `tRPC Error: ${error.code} | ${error.message}\nType: ${type} | Path: ${path}`;
 
+    const criticalErrors = [
+      'TOO_MANY_REQUESTS', // HTTP 429
+      'INTERNAL_SERVER_ERROR', // HTTP 500
+      'NOT_IMPLEMENTED', // HTTP 501
+      'BAD_GATEWAY', // HTTP 502
+      'SERVICE_UNAVAILABLE', // HTTP 503
+      'GATEWAY_TIMEOUT', // HTTP 504
+    ];
+
+    // Only alert on critical errors
+    if (!criticalErrors.includes(error.code)) return;
+
     slackAlert(env, [message]).catch((slackError) => {
       // eslint-disable-next-line no-console
       console.error('Failed to send Slack alert:', slackError);
