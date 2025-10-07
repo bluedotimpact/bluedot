fb3e1da9f8d3b2c218107b36f90cfcf6115ac9fe
Josh Stein <josh.e.stein@gmail.com>
2025-10-07 10:27:37 +0200

    Add `slackAlert` in tRPC `onError` handler

--- apps/website/src/pages/api/trpc/[trpc].ts
+++ apps/website/src/pages/api/trpc/[trpc].ts
@@ -1,4 +1,6 @@
+import { slackAlert } from '@bluedot/utils/src/slackNotifications';
 import * as trpcNext from '@trpc/server/adapters/next';
+import env from '../../../lib/api/env';
 import { createContext } from '../../../server/context';
 import { appRouter } from '../../../server/routers/_app';
 
@@ -6,4 +8,13 @@ import { appRouter } from '../../../server/routers/_app';
 export default trpcNext.createNextApiHandler({
   router: appRouter,
   createContext,
+  onError(opts) {
+    const { error, type, path } = opts;
+    const message = `tRPC Error: ${error.code} | ${error.message}\nType: ${type} | Path: ${path}`;
+
+    slackAlert(env, [message]).catch((slackError) => {
+      // eslint-disable-next-line no-console
+      console.error('Failed to send Slack alert:', slackError);
+    });
+  },
 });
