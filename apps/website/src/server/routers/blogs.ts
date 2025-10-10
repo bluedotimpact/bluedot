8060a335f22280dde7433d0d5ba0b349d7318583
Josh Stein <josh.e.stein@gmail.com>
2025-10-10 16:31:50 +0200

    Remove old API route

--- apps/website/src/server/routers/blogs.ts
+++ apps/website/src/server/routers/blogs.ts
@@ -2,18 +2,22 @@ import { blogTable } from '@bluedot/db';
 import db from '../../lib/api/db';
 import { publicProcedure, router } from '../trpc';
 
+export const getAllPublishedBlogs = async () => {
+  const allBlogs = await db.scan(blogTable, { publicationStatus: 'Published' });
+
+  // Sort by publishedAt descending and remove the body field from each blog to make the response lighter
+  return allBlogs
+    .sort((a, b) => {
+      if (a.isFeatured !== b.isFeatured) {
+        return b.isFeatured ? 1 : -1;
+      }
+      return (b.publishedAt || 0) - (a.publishedAt || 0);
+    })
+    .map(({ body, ...rest }) => rest);
+};
+
 export const blogsRouter = router({
   getBlogs: publicProcedure.query(async () => {
-    const publishedBlogs = await db.scan(blogTable, { publicationStatus: 'Published' });
-
-    // Sort by publishedAt descending and remove the body field from each blog to make the response lighter
-    return publishedBlogs
-      .sort((a, b) => {
-        if (a.isFeatured !== b.isFeatured) {
-          return b.isFeatured ? 1 : -1;
-        }
-        return (b.publishedAt || 0) - (a.publishedAt || 0);
-      })
-      .map(({ body, ...rest }) => rest);
+    return getAllPublishedBlogs();
   }),
 });
