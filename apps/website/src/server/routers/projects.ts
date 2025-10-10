44df9932f9dfb0b0608014640015baa87351e2fc
Josh Stein <josh.e.stein@gmail.com>
2025-10-10 17:00:50 +0200

    Inline return

--- apps/website/src/server/routers/projects.ts
+++ apps/website/src/server/routers/projects.ts
@@ -6,11 +6,9 @@ export const getAllPublishedProjects = async () => {
   const allProjects = await db.scan(projectTable, { publicationStatus: 'Published' });
 
   // Sort by publishedAt descending and remove the body field from each project to make the response lighter
-  const projectSummaries = allProjects
+  return allProjects
     .sort((a, b) => (b.publishedAt || 0) - (a.publishedAt || 0))
     .map(({ body, ...rest }) => rest);
-
-  return projectSummaries;
 };
 
 export const projectsRouter = router({
