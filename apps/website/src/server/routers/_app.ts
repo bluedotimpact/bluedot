c29eb6c0dcb729b7b8048356fce4fe8625e6482f
Josh Stein <josh.e.stein@gmail.com>
2025-10-13 16:16:44 +0200

    `courseRegistrationRouter`

--- apps/website/src/server/routers/_app.ts
+++ apps/website/src/server/routers/_app.ts
@@ -1,8 +1,10 @@
 import { router } from '../trpc';
+import { courseRegistrationRouter } from './course-registration';
 import { jobsRouter } from './jobs';
 import { usersRouter } from './users';
 
 export const appRouter = router({
+  courseRegistrations: courseRegistrationRouter,
   jobs: jobsRouter,
   users: usersRouter,
 });
---
+++ apps/website/src/server/routers/course-registration.ts
@@ -0,0 +1,38 @@
+import { applicationsCourseTable, courseRegistrationTable } from '@bluedot/db';
+import { TRPCError } from '@trpc/server';
+import z from 'zod';
+import db from '../../lib/api/db';
+import { protectedProcedure, router } from '../trpc';
+
+export const courseRegistrationRouter = router({
+  getById: protectedProcedure
+    .input(z.string())
+    .query(async ({ ctx, input }) => {
+      try {
+        const courseRegistration = await db.getFirst(courseRegistrationTable, {
+          filter: {
+            email: ctx.auth.email,
+            courseId: input,
+            decision: 'Accept',
+          },
+        });
+        if (courseRegistration) return courseRegistration;
+
+        const applicationsCourse = await db.get(applicationsCourseTable, { courseBuilderId: input });
+        return await db.insert(courseRegistrationTable, {
+          email: ctx.auth.email,
+          courseApplicationsBaseId: applicationsCourse.id,
+          role: 'Participant',
+          decision: 'Accept',
+        });
+      } catch (error) {
+        throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Database error occurred' });
+      }
+    }),
+  getAll: protectedProcedure.query(async ({ ctx }) => {
+    return db.scan(courseRegistrationTable, {
+      email: ctx.auth.email,
+      decision: 'Accept',
+    });
+  }),
+});
