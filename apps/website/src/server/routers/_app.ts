131619eb8eed8716b7cade89a0d8a70b64b4fb38
Josh Stein <josh.e.stein@gmail.com>
2025-10-14 17:18:18 +0200

    `syncDashboardRouter`

--- apps/website/src/server/routers/_app.ts
+++ apps/website/src/server/routers/_app.ts
@@ -1,9 +1,11 @@
 import { router } from '../trpc';
 import { jobsRouter } from './jobs';
+import { syncDashboardRouter } from './sync-dashboard';
 import { usersRouter } from './users';
 
 export const appRouter = router({
   jobs: jobsRouter,
+  syncDashboard: syncDashboardRouter,
   users: usersRouter,
 });
 
