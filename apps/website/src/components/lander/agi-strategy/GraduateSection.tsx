4f80263e8ea854dc5ac947fe3e986af5faa2c401
alyssia-x <30333400+alyssia-x@users.noreply.github.com>
2025-10-22 13:29:39 -1000

    Improved graduate section responsiveness

--- apps/website/src/components/lander/agi-strategy/GraduateSection.tsx
+++ apps/website/src/components/lander/agi-strategy/GraduateSection.tsx
@@ -25,13 +25,13 @@ const logos = [
 
 const GraduateSection = () => {
   return (
-    <section className="graduate-section w-full max-w-max-width mx-auto px-5 py-8 min-[680px]:px-8 min-[680px]:pt-8 min-[680px]:pb-[33px] min-[680px]:h-[105px] min-[1024px]:px-spacing-x overflow-hidden flex flex-col min-[680px]:justify-end min-[680px]:items-start">
-      <div className="flex flex-col gap-6 items-center min-[680px]:flex-row min-[680px]:gap-6 min-[680px]:h-10 min-[680px]:items-center lg:flex-row">
+    <section className="graduate-section w-full max-w-max-width mx-auto px-5 py-8 min-[680px]:px-8 min-[680px]:pt-8 min-[680px]:pb-[33px] min-[680px]:min-h-[105px] min-[1024px]:px-spacing-x overflow-hidden flex flex-col min-[680px]:justify-end min-[680px]:items-start">
+      <div className="flex flex-col gap-6 items-center min-[680px]:flex-row min-[680px]:gap-6 min-[680px]:min-h-10 min-[680px]:items-center lg:flex-row">
         <div className="flex flex-col gap-4 items-center shrink-0 min-[680px]:flex-row min-[680px]:gap-4 min-[680px]:items-center lg:flex-row lg:items-center lg:gap-2">
           <FaceTiles faces={faces} />
-          <P className="flex-shrink-0 text-center min-[680px]:text-left min-[680px]:w-[197px] min-[680px]:leading-tight lg:text-left lg:w-auto">
+          <P className="flex-shrink-0 text-center min-[680px]:text-left min-[680px]:min-w-[197px] min-[680px]:leading-tight lg:text-left lg:w-auto">
             Our 4000+ alumni shape{' '}
-            <span className="min-[680px]:block min-[680px]:max-[1023px]:block lg:inline">AI's future at:</span>
+            <span className="min-[680px]:block lg:inline">AI's future at:</span>
           </P>
         </div>
         <div className="w-full inline-flex flex-nowrap overflow-hidden justify-center min-[680px]:justify-start min-[680px]:flex-1 lg:justify-start [mask-image:_linear-gradient(to_right,transparent_0,_black_12.63%,_black_80.26%,transparent_100%)]">
