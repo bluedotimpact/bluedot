c5dd643d73908c964c67e7b012c8d898415e9ef6
Josh Stein <27491286+joshestein@users.noreply.github.com>
2025-10-22 11:24:47 +0200

    tRPC migrate `/api/admin` routes (#1463)
    * `adminProcedure`
    
    * `syncDashboardRouter`
    
    * Use tRPC for dashboard requests
    
    * Remove old API handlers
    
    * Rename `hasAccess` -> `hasAdminAccess`
    
    * Rename `syncDashboard` -> `admin`
    
    * Rename `history` -> `syncHistory`
    
    * Use query `isFetching`, don't track `isRefreshing` in state
    
    * Use `refetchInterval` to handle refetching data
    
    * Don't store data in state
    
    * Remove `hasInitiallyLoaded`, always show spinner on data fetch
    
    * Prefer derived state to `useEffect`
    
    * Only refresh when no error
    
    * Simplify `hasSyncRunning`
    
    * Woops
    
    * Fix failing tests
    
    * Don't cast to type `never`
    
    * Use `refetchInterval` not `useEffect`
    
    * `isLoading` false for unauthorized test
    
    * Don't call `useFakeTimers` again
    
    * Use `mutation.isPending` instead of local state
    
    * Use tRPC MSW for tests

--- apps/website/src/server/routers/_app.ts
+++ apps/website/src/server/routers/_app.ts
@@ -1,10 +1,12 @@
 import { router } from '../trpc';
+import { adminRouter } from './admin';
 import { blogsRouter } from './blogs';
 import { jobsRouter } from './jobs';
 import { projectsRouter } from './projects';
 import { usersRouter } from './users';
 
 export const appRouter = router({
+  admin: adminRouter,
   blogs: blogsRouter,
   jobs: jobsRouter,
   projects: projectsRouter,
