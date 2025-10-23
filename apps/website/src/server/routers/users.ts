088feed6e39c21132751caea148a82589382e4ef
Josh Stein <josh.e.stein@gmail.com>
2025-10-23 15:18:32 +0200

    Add procedure for `getUser`

--- apps/website/src/server/routers/users.ts
+++ apps/website/src/server/routers/users.ts
@@ -1,9 +1,26 @@
 import { TRPCError } from '@trpc/server';
+import { userTable } from '@bluedot/db';
 import { updateKeycloakPassword, verifyKeycloakPassword } from '../../lib/api/keycloak';
-import { protectedProcedure, router } from '../trpc';
 import { changePasswordSchema } from '../../lib/schemas/user/changePassword.schema';
+import { protectedProcedure, router } from '../trpc';
+import db from '../../lib/api/db';
 
 export const usersRouter = router({
+  getUser: protectedProcedure
+    .query(async ({ ctx }) => {
+      const existingUser = await db.getFirst(userTable, {
+        filter: { email: ctx.auth.email },
+      });
+      if (existingUser) {
+        // Update lastSeenAt timestamp
+        return db.update(userTable, {
+          id: existingUser.id,
+          lastSeenAt: new Date().toISOString(),
+        });
+      }
+      return existingUser;
+    }),
+
   changePassword: protectedProcedure
     .input(changePasswordSchema)
     .mutation(async ({ ctx, input }) => {
