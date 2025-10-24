0b4a044fca30f605978fa1194260b22ab5e4e91e
Josh Stein <josh.e.stein@gmail.com>
2025-10-24 13:51:17 +0200

    Create a component so we can use hook

--- apps/website/src/pages/login/oauth-callback.tsx
+++ apps/website/src/pages/login/oauth-callback.tsx
@@ -1,41 +1,43 @@
 import { LoginOauthCallbackPage, loginPresets } from '@bluedot/ui';
 import { trpc } from '../../utils/trpc';
 
-export default () => (
-  <LoginOauthCallbackPage
-    loginPreset={loginPresets.keycloak}
-    onLoginComplete={async (auth, redirectTo) => {
-      // Extract UTM params from the redirectTo URL and send them to `api/users/me` to track them in the database
-      let initialUtmSource: string | null = null;
-      let initialUtmCampaign: string | null = null;
-      let initialUtmContent: string | null = null;
-
-      try {
-        const redirectUrl = new URL(redirectTo, window.location.origin);
-        initialUtmSource = redirectUrl.searchParams.get('utm_source');
-        initialUtmCampaign = redirectUrl.searchParams.get('utm_campaign');
-        initialUtmContent = redirectUrl.searchParams.get('utm_content');
-      } catch (error) {
-        // eslint-disable-next-line no-console
-        console.warn('Failed to parse UTM params from redirectTo:', error);
+export default () => {
+  const createUserMutation = trpc.users.create.useMutation({
+    onSuccess: (response) => {
+      if (typeof window !== 'undefined' && window.dataLayer) {
+        window.dataLayer.push({
+          event: 'CompletedSignup',
+          new_customer: response.isNewUser,
+        });
       }
+    },
+  });
 
-      const createUserMutation = trpc.users.create.useMutation({
-        onSuccess: (response) => {
-          if (typeof window !== 'undefined' && window.dataLayer) {
-            window.dataLayer.push({
-              event: 'CompletedSignup',
-              new_customer: response.isNewUser,
-            });
-          }
-        },
-      });
+  return (
+    <LoginOauthCallbackPage
+      loginPreset={loginPresets.keycloak}
+      onLoginComplete={async (auth, redirectTo) => {
+        // Extract UTM params from the redirectTo URL and send them to `api/users/me` to track them in the database
+        let initialUtmSource: string | null = null;
+        let initialUtmCampaign: string | null = null;
+        let initialUtmContent: string | null = null;
 
-      createUserMutation.mutate({
-        initialUtmSource,
-        initialUtmCampaign,
-        initialUtmContent,
-      });
-    }}
-  />
-);
+        try {
+          const redirectUrl = new URL(redirectTo, window.location.origin);
+          initialUtmSource = redirectUrl.searchParams.get('utm_source');
+          initialUtmCampaign = redirectUrl.searchParams.get('utm_campaign');
+          initialUtmContent = redirectUrl.searchParams.get('utm_content');
+        } catch (error) {
+          // eslint-disable-next-line no-console
+          console.warn('Failed to parse UTM params from redirectTo:', error);
+        }
+
+        createUserMutation.mutate({
+          initialUtmSource,
+          initialUtmCampaign,
+          initialUtmContent,
+        });
+      }}
+    />
+  );
+};
