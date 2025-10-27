7de6037b67e74d601f1979886ec52d437b2368a3
Josh Stein <josh.e.stein@gmail.com>
2025-10-27 14:50:50 +0200

    Update route handler to match special FOAI handling

--- apps/website/src/server/routers/course-registrations.ts
+++ apps/website/src/server/routers/course-registrations.ts
@@ -1,7 +1,9 @@
 import { applicationsCourseTable, courseRegistrationTable } from '@bluedot/db';
 import z from 'zod';
+import { TRPCError } from '@trpc/server';
 import db from '../../lib/api/db';
 import { protectedProcedure, router } from '../trpc';
+import { FOAI_COURSE_ID } from '../../lib/constants';
 
 export const courseRegistrationsRouter = router({
   getByCourseId: protectedProcedure
@@ -26,6 +28,8 @@ export const courseRegistrationsRouter = router({
 
   ensureExists: protectedProcedure
     .input(z.object({ courseId: z.string(), source: z.string().trim().max(255).optional() }))
+    // This mutation will create a course registration if one doesn't already exist for FOAI course.
+    // eslint-disable-next-line consistent-return
     .mutation(async ({ ctx, input }) => {
       const { courseId, source } = input;
       const courseRegistration = await db.getFirst(courseRegistrationTable, {
@@ -39,13 +43,23 @@ export const courseRegistrationsRouter = router({
       // If the course registration already exists, return it
       if (courseRegistration) return courseRegistration;
 
-      const applicationsCourse = await db.get(applicationsCourseTable, { courseBuilderId: courseId });
-      return db.insert(courseRegistrationTable, {
-        email: ctx.auth.email,
-        courseApplicationsBaseId: applicationsCourse.id,
-        role: 'Participant',
-        decision: 'Accept',
-        source,
-      });
+      if (courseId === FOAI_COURSE_ID) {
+        const applicationsCourse = await db.getFirst(applicationsCourseTable, {
+          sortBy: 'id',
+          filter: { courseBuilderId: courseId },
+        });
+
+        if (!applicationsCourse) {
+          throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: `Course configuration not found for course: ${courseId}` });
+        }
+
+        return db.insert(courseRegistrationTable, {
+          email: ctx.auth.email,
+          courseApplicationsBaseId: applicationsCourse.id,
+          role: 'Participant',
+          decision: 'Accept',
+          source: source ?? null,
+        });
+      }
     }),
 });
