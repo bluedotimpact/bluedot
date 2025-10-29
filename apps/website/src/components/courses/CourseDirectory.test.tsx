2a4534a7244bfb2aea9d354813390a15376af06f
Josh Stein <josh.e.stein@gmail.com>
2025-10-29 16:10:04 +0200

    Re-use `mockCourse`

--- apps/website/src/components/courses/CourseDirectory.test.tsx
+++ apps/website/src/components/courses/CourseDirectory.test.tsx
@@ -3,54 +3,35 @@ import {
   describe, expect, test,
 } from 'vitest';
 import CourseDirectory from './CourseDirectory';
+import { mockCourse } from '../../__tests__/testUtils';
+
+const mockCourses = [
+  mockCourse({
+    id: 'course-1',
+    title: 'Test Course 1',
+    shortDescription: 'Desc 1',
+    cadence: 'MOOC',
+    level: 'Beginner',
+    averageRating: 4.5,
+    image: 'img1.jpg',
+    path: '/course1',
+    durationDescription: '4 weeks',
+  }),
+  mockCourse({
+    id: 'course-2',
+    title: 'Test Course 2',
+    shortDescription: 'Desc 2',
+    cadence: 'Daily',
+    level: 'Intermediate',
+    averageRating: 4.0,
+    image: 'img2.jpg',
+    path: '/course2',
+    durationDescription: '6 weeks',
+  }),
+];
 
 const defaultProps = {
-  courses: [
-    {
-      id: 'course-1',
-      title: 'Test Course 1',
-      shortDescription: 'Desc 1',
-      cadence: 'MOOC',
-      level: 'Beginner',
-      averageRating: 4.5,
-      image: 'img1.jpg',
-      path: '/course1',
-      durationDescription: '4 weeks',
-      displayOnCourseHubIndex: true,
-      certificationBadgeImage: null,
-      certificationDescription: null,
-      description: null,
-      detailsUrl: null,
-      durationHours: null,
-      slug: null,
-      units: null,
-      publicLastUpdated: null,
-      isNew: null,
-      isFeatured: null,
-    },
-    {
-      id: 'course-2',
-      title: 'Test Course 2',
-      shortDescription: 'Desc 2',
-      cadence: 'Daily',
-      level: 'Intermediate',
-      averageRating: 4.0,
-      image: 'img2.jpg',
-      path: '/course2',
-      durationDescription: '6 weeks',
-      displayOnCourseHubIndex: true,
-      certificationBadgeImage: null,
-      certificationDescription: null,
-      description: null,
-      detailsUrl: null,
-      durationHours: null,
-      slug: null,
-      units: null,
-      publicLastUpdated: null,
-      isNew: null,
-      isFeatured: null,
-    },
-  ],
+  courses: mockCourses,
   loading: false,
 };
 
@@ -69,8 +50,8 @@ describe('CourseDirectory', () => {
     };
     const { container, queryByText } = render(<CourseDirectory {...props} />);
 
-    expect(queryByText(defaultProps.courses[0]!.title!)).toBeNull();
-    expect(queryByText(defaultProps.courses[1]!.title!)).toBeNull();
+    expect(queryByText(mockCourses[0]!.title!)).toBeNull();
+    expect(queryByText(mockCourses[1]!.title!)).toBeNull();
 
     const progressDotsElement = container.querySelector('.progress-dots');
     expect(progressDotsElement).not.toBeNull();
