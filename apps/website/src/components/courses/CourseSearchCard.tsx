4da6771464815a9c0a279a8edf615ad54446da5b
Josh Stein <josh.e.stein@gmail.com>
2025-10-29 16:22:29 +0200

    Remove leftover BEM

--- apps/website/src/components/courses/CourseSearchCard.tsx
+++ apps/website/src/components/courses/CourseSearchCard.tsx
@@ -53,8 +53,8 @@ export const CourseSearchCard: React.FC<CourseSearchCardProps> = ({
         </div>
         {typeof averageRating === 'number' && (
           <div className="flex justify-end mt-4">
-            <div className="course-card__footer-left text-size-xs text-bluedot-black gap-1 flex flex-col items-end">
-              <span className="course-card__rating flex gap-[3px] items-start font-bold">
+            <div className="text-size-xs text-bluedot-black gap-1 flex flex-col items-end">
+              <span className="flex gap-[3px] items-start font-bold">
                 {displayAverageRating(averageRating)}{' '}
                 <FaStar className="mt-px" />
               </span>
