d0690198c97f60091ecf498ecf6504cf6a3f7184
Josh Stein <27491286+joshestein@users.noreply.github.com>
2025-11-07 10:56:21 +0200

    tRPC migrate POST `/api/courses/[courseSlug]/group-switching/index.ts` (#1498)
    * `groupSwitchingRouter`
    
    * Use tRPC for submitting group switch request
    
    * Tests use tRPC MSW
    
    * Delete old API handler
    
    * Use `mutation.isPending` to derive `isSubmitting` state

--- apps/website/src/server/routers/_app.ts
+++ apps/website/src/server/routers/_app.ts
@@ -5,6 +5,7 @@ import { certificatesRouter } from './certificates';
 import { courseRegistrationsRouter } from './course-registrations';
 import { coursesRouter } from './courses';
 import { groupDiscussionsRouter } from './group-discussions';
+import { groupSwitchingRouter } from './group-switching';
 import { jobsRouter } from './jobs';
 import { lumaRouter } from './luma';
 import { meetPersonRouter } from './meet-person';
@@ -18,6 +19,7 @@ export const appRouter = router({
   courseRegistrations: courseRegistrationsRouter,
   courses: coursesRouter,
   groupDiscussions: groupDiscussionsRouter,
+  groupSwitching: groupSwitchingRouter,
   jobs: jobsRouter,
   luma: lumaRouter,
   meetPerson: meetPersonRouter,
