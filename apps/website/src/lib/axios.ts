9ce05105382ad04f460965a0c75f3605268a498f
Will Howard <w.howard256+github@gmail.com>
2025-11-18 07:54:46 +0000

    Prefer NEXT_PUBLIC_SITE_URL to http://localhost:8000 in axios setup (#1629)

--- apps/website/src/lib/axios.ts
+++ apps/website/src/lib/axios.ts
@@ -3,7 +3,7 @@ import { configure } from 'axios-hooks';
 
 // Create axios instance with base URL
 const axiosInstance = axios.create({
-  baseURL: typeof window !== 'undefined' ? window.location.origin : 'http://localhost:8000',
+  baseURL: typeof window !== 'undefined' ? window.location.origin : (process.env.NEXT_PUBLIC_SITE_URL ?? 'http://localhost:8000'),
 });
 
 // Configure axios-hooks to use our configured instance
