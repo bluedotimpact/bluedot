25b8f4a4a7074baed9c5f4b0cc068d05d4f99447
Josh Stein <josh.e.stein@gmail.com>
2025-11-21 10:54:20 +0200

    `ProgressDots` uses `cn` + prop to allow over-riding styles

--- libraries/ui/src/ProgressDots.tsx
+++ libraries/ui/src/ProgressDots.tsx
@@ -1,6 +1,12 @@
-export const ProgressDots: React.FC = () => {
+import { cn } from './utils';
+
+type ProgressDotsProps = {
+  className?: string;
+};
+
+export const ProgressDots: React.FC<ProgressDotsProps> = ({ className }) => {
   return (
-    <div className="progress-dots flex justify-center space-x-2 my-6">
+    <div className={cn('progress-dots flex justify-center space-x-2 my-6', className)}>
       <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '0ms' }} />
       <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '150ms' }} />
       <span className="size-2 bg-bluedot-normal rounded-full animate-bounce" style={{ animationDelay: '300ms' }} />
