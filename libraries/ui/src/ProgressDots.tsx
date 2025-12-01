210e19be79ce613f586ab51e8b341e7b980f0ae4
Josh Stein <josh.e.stein@gmail.com>
2025-12-01 08:31:30 +0200

    Add `items-center` to base style

--- libraries/ui/src/ProgressDots.tsx
+++ libraries/ui/src/ProgressDots.tsx
@@ -7,7 +7,7 @@ type ProgressDotsProps = {
 
 export const ProgressDots: React.FC<ProgressDotsProps> = ({ className, dotClassName }) => {
   return (
-    <div className={cn('progress-dots flex justify-center space-x-2 my-6', className)}>
+    <div className={cn('progress-dots my-6 flex items-center justify-center space-x-2', className)}>
       <span className={cn('size-2 bg-bluedot-normal rounded-full animate-bounce', dotClassName)} style={{ animationDelay: '0ms' }} />
       <span className={cn('size-2 bg-bluedot-normal rounded-full animate-bounce', dotClassName)} style={{ animationDelay: '150ms' }} />
       <span className={cn('size-2 bg-bluedot-normal rounded-full animate-bounce', dotClassName)} style={{ animationDelay: '300ms' }} />
