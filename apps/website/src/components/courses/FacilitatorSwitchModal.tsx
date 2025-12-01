75698338b7ccfa29d2e2baf85b6482cff9a57b7c
Josh Stein <josh.e.stein@gmail.com>
2025-12-01 18:27:39 +0200

    New disclaimer

--- apps/website/src/components/courses/FacilitatorSwitchModal.tsx
+++ apps/website/src/components/courses/FacilitatorSwitchModal.tsx
@@ -31,12 +31,19 @@ const FacilitatorSwitchModal: React.FC<FacilitatorSwitchModalProps> = ({
       bottomDrawerOnMobile
     >
       <div className="w-full max-w-[600px]">
-        <P className="mb-4">
-          This will update the calendar invitation and Course Hub information. This tool <em>does not</em> notify your
-          participants about this change.
-        </P>
-
         <form className="flex flex-col gap-8">
+          <div className="inline-flex items-center justify-between self-stretch rounded-md bg-[#E5EDFE] px-4 py-3">
+            <div className="flex flex-1 items-start justify-start gap-3">
+              <div className="flex items-center justify-start">
+                <InfoIcon className="size-5 shrink-0" />
+              </div>
+              <P className="flex-1 justify-start text-[#1144CC]">
+                Please discuss any changes with your participants beforehand. Any changes will update the calendar
+                invitation and Course Hub information, but not notify your participants.
+              </P>
+            </div>
+          </div>
+
           <div className="flex flex-col gap-2">
             <H1 className="text-size-md font-medium text-[#00114D]">Step 1: Select switch type</H1>
             <P>
@@ -73,3 +80,13 @@ const FacilitatorSwitchModal: React.FC<FacilitatorSwitchModalProps> = ({
 };
 
 export default FacilitatorSwitchModal;
+
+const InfoIcon = ({ className }: { className?: string }) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" className={className}>
+      <circle cx="10" cy="10" r="10" fill="#1D4ED8" />
+      <path d="M10 14V9" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <circle cx="10" cy="6.5" r="0.75" fill="white" />
+    </svg>
+  );
+};
