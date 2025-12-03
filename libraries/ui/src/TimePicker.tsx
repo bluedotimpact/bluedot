7f4e6ff3692d5e2010f24ea55947402895768004
Josh Stein <josh.e.stein@gmail.com>
2025-12-03 12:46:08 +0200

    Allow overriding classes, labels, control

--- libraries/ui/src/TimePicker.tsx
+++ libraries/ui/src/TimePicker.tsx
@@ -1,13 +1,69 @@
 import {
-  TimeField, Label, DateInput, DateSegment,
+  DateInput, DateSegment,
+  Label,
+  TimeField,
+  TimeValue,
 } from 'react-aria-components';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import { parseTime } from '@internationalized/date';
+import { cn } from './utils';
 
-export const TimePicker = () => {
+type TimePickerProps = {
+  label?: string;
+  hideLabel?: boolean;
+  value?: Date | null;
+  onChange?: (value: Date | null) => void;
+  className?: string;
+  labelClassName?: string;
+  inputClassName?: string;
+};
+
+export const TimePicker = ({
+  label = 'Time',
+  hideLabel = false,
+  value,
+  onChange,
+  className,
+  labelClassName,
+  inputClassName,
+}: TimePickerProps) => {
+  // Convert Date to TimeValue via HH:mm format
+  const timeValue = value
+    ? parseTime(`${value.getHours().toString().padStart(2, '0')}:${value.getMinutes().toString().padStart(2, '0')}`)
+    : null;
+
+  // Convert TimeValue to Date object in onChange
+  const handleChange = (newValue: TimeValue | null) => {
+    if (onChange) {
+      if (newValue) {
+        const date = new Date();
+        date.setHours(newValue.hour, newValue.minute, 0, 0);
+        onChange(date);
+      } else {
+        onChange(null);
+      }
+    }
+  };
+
   return (
-    <TimeField>
-      <Label>Event time</Label>
-      <DateInput>
-        {(segment) => <DateSegment segment={segment} />}
+    <TimeField
+      className={cn('group flex w-[200px] flex-col gap-1', className)}
+      value={timeValue}
+      onChange={handleChange}
+    >
+      {!hideLabel && <Label className={cn('cursor-default text-black', labelClassName)}>{label}</Label>}
+      <DateInput
+        className={cn(
+          'flex rounded-lg border border-gray-200 bg-white/90 px-3 py-2 text-gray-700 ring-black transition focus-within:bg-white focus-visible:ring-2',
+          inputClassName,
+        )}
+      >
+        {(segment) => (
+          <DateSegment
+            segment={segment}
+            className="rounded-xs px-0.5 tabular-nums caret-transparent outline-hidden placeholder-shown:italic focus:bg-violet-700 focus:text-white"
+          />
+        )}
       </DateInput>
     </TimeField>
   );
