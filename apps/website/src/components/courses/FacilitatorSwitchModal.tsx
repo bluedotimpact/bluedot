ab57c0a51f816c05f10bc2dca03e72173aefb0ad
Josh Stein <josh.e.stein@gmail.com>
2025-12-04 11:15:20 +0200

    Breathing room

--- apps/website/src/components/courses/FacilitatorSwitchModal.tsx
+++ apps/website/src/components/courses/FacilitatorSwitchModal.tsx
@@ -112,11 +112,11 @@ const FacilitatorSwitchModal: React.FC<FacilitatorSwitchModalProps> = ({
 
     if (submitUpdateMutation.isSuccess) {
       return (
-        <div className="flex flex-col items-center justify-center gap-6 w-full">
+        <div className="flex flex-col items-center justify-center gap-8 w-full">
           <div className="rounded-full bg-bluedot-normal/10 p-4 flex">
             <CheckIcon className="text-bluedot-normal" />
           </div>
-          <div className="flex flex-col gap-6 items-center max-w-[400px]">
+          <div className="flex flex-col gap-4 items-center max-w-[400px]">
             <P className="text-[#13132E] opacity-80">We've updated your group's {selectedDiscussionId ? 'discussion' : 'discussions'}.</P>
             <P className="text-[#13132E] opacity-80 text-center">You should see the changes reflected in the calendar event and Course Hub. Please allow up to 10 minutes.</P>
           </div>
