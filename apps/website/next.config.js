03efe8478c053bdfbe516c9d6ff77e007704ba63
Dewi Erwan <dewierwan@gmail.com>
2025-12-07 10:27:58 +0000

    Add redirect for governance course to AI governance course

--- apps/website/next.config.js
+++ apps/website/next.config.js
@@ -8,6 +8,11 @@ module.exports = withDefaultBlueDotNextConfig({
         destination: '/blog/running-versions-of-our-courses',
         permanent: true,
       },
+      {
+        source: '/courses/governance',
+        destination: '/courses/ai-governance',
+        permanent: true,
+      },
     ];
   },
   headers: [
