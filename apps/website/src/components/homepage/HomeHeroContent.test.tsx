d34eba7d193e4c4bf05c524a0294883ea387e4f1
alyssia-x <30333400+alyssia-x@users.noreply.github.com>
2025-12-10 21:05:06 +0400

    Snapshot updates

--- apps/website/src/components/homepage/HomeHeroContent.test.tsx
+++ apps/website/src/components/homepage/HomeHeroContent.test.tsx
@@ -1,10 +1,20 @@
-import { describe, expect, test } from 'vitest';
+import {
+  describe, expect, test, vi,
+} from 'vitest';
 import { render } from '@testing-library/react';
 import HomeHeroContent from './HomeHeroContent';
+import { TrpcProvider } from '../../__tests__/trpcProvider';
+
+vi.mock('next/router', () => ({
+  useRouter: () => ({
+    pathname: '/',
+    query: {},
+  }),
+}));
 
 describe('HomeHeroContent', () => {
   test('renders as expected', () => {
-    const { container } = render(<HomeHeroContent />);
+    const { container } = render(<HomeHeroContent />, { wrapper: TrpcProvider });
     expect(container).toMatchSnapshot();
   });
 });
