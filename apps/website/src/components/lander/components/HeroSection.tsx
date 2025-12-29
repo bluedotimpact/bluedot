32bb20fd9f9566b3596da821da997d56201ee561
Will Howard <w.howard256+github@gmail.com>
2025-12-29 16:18:28 +0000

    [Feature] Optimise all images *not* on the home page (#1868)
    * Optimise all graduates/ images
    
    * Optimise all lander images
    
    * Add fetchPriority high to all lander hero sections
    
    * Update miscellaneous lander images
    
    * Update snapshots
    
    * Optimise misc/low priority ones
    
    * Optimise stray image only using tests and update snapshots again
    
    * Add checklist for reference for reviewers
    
    * Remove checklist

--- apps/website/src/components/lander/components/HeroSection.tsx
+++ apps/website/src/components/lander/components/HeroSection.tsx
@@ -73,7 +73,8 @@ const HeroSection = ({
 
         {/* Image */}
         <div className="h-80 sm:h-[430px] lg:h-full relative overflow-hidden">
-          <img src={imageSrc} alt={imageAlt} className="size-full object-cover lg:object-center" />
+          {/* Workaround for bug with camelcase `fetchPriority`: https://github.com/facebook/react/issues/25682 */}
+          <img src={imageSrc} alt={imageAlt} className="size-full object-cover lg:object-center" {...{ fetchpriority: 'high' }} />
           {/* Noise layer */}
           <div
             className="absolute inset-0 pointer-events-none bg-contain bg-repeat mix-blend-soft-light"
