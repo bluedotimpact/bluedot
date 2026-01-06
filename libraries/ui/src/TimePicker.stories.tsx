a4d0b2a6ded6fc23a6ade90b4c4f079c13174f34
Josh Stein <josh.e.stein@gmail.com>
2026-01-06 19:12:07 +0200

    Don't allow null value for consistency with calendar day picker

--- libraries/ui/src/TimePicker.stories.tsx
+++ libraries/ui/src/TimePicker.stories.tsx
@@ -17,7 +17,7 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   render: () => {
     const ControlledDemo = () => {
-      const [time, setTime] = useState<Date | null>(null);
+      const [time, setTime] = useState<Date | undefined>(undefined);
 
       return (
         <div className="flex flex-col gap-4">
