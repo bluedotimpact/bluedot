44425ee4539c6cd0f7a802f7d8054602860bc73c
Josh Stein <josh.e.stein@gmail.com>
2026-01-06 19:58:56 +0200

    Parameterize info icon

--- apps/website/src/components/icons/InfoIcon.tsx
+++ apps/website/src/components/icons/InfoIcon.tsx
@@ -1,9 +1,17 @@
-export const InfoIcon = ({ className }: { className?: string }) => {
+export const InfoIcon = ({
+  className,
+  bgFill = '#1D4ED8',
+  fill = 'white',
+}: {
+  className?: string;
+  bgFill?: string;
+  fill?: string;
+}) => {
   return (
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" className={className}>
-      <circle cx="10" cy="10" r="10" fill="#1D4ED8" />
-      <path d="M10 14V9" stroke="white" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <circle cx="10" cy="6.5" r="0.75" fill="white" />
+      <circle cx="10" cy="10" r="10" fill={bgFill} />
+      <path d="M10 14V9" stroke={fill} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+      <circle cx="10" cy="6.5" r="0.75" fill={fill} />
     </svg>
   );
 };
