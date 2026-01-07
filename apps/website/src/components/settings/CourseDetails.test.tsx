dbd0aaadc6134ffc34cfbd8c59dc487f7040654e
Will Howard <w.howard256+github@gmail.com>
2026-01-07 09:20:53 +0000

    tweak test

--- apps/website/src/components/settings/CourseDetails.test.tsx
+++ apps/website/src/components/settings/CourseDetails.test.tsx
@@ -97,7 +97,7 @@ describe('CourseDetails: Participant view', () => {
           endDateTime: Math.floor(currentTimeMs / 1000) + 3 * 60 * 60,
         }),
         unitRecord: null, // No unit record
-        unitFallback: '3 : Advanced Concepts',
+        unitFallback: '3: Advanced Concepts',
         groupDetails: createMockGroup(),
       },
     ];
@@ -114,7 +114,7 @@ describe('CourseDetails: Participant view', () => {
 
     // THEN: The unit title is displayed using the fallback field
     await waitFor(() => {
-      expect(screen.getByText('Unit 3 : Advanced Concepts')).toBeInTheDocument();
+      expect(screen.getByText('Unit 3: Advanced Concepts')).toBeInTheDocument();
     });
   });
 
