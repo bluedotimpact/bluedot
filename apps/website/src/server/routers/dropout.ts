61bd4b899bd81743943dc9df4eb324ac62f05aba
Josh Stein <josh.e.stein@gmail.com>
2026-01-14 16:08:58 +0200

    Dropout router

---
+++ apps/website/src/server/routers/dropout.ts
@@ -0,0 +1,24 @@
+import { dropoutTable } from '@bluedot/db';
+import z from 'zod';
+import db from '../../lib/api/db';
+import { publicProcedure, router } from '../trpc';
+
+export const dropoutRouter = router({
+  dropoutOrDeferral: publicProcedure
+    .input(
+      z.object({
+        applicantId: z.array(z.string().min(1)).min(1),
+        reason: z.string().min(1),
+        isDeferral: z.boolean(),
+      }),
+    )
+    .mutation(async ({ input }) => {
+      const { applicantId, reason, isDeferral } = input;
+
+      return db.insert(dropoutTable, {
+        applicantId,
+        reason,
+        isDeferral,
+      });
+    }),
+});
