44a09855963d41c923f56c7a4209237a53d6a405
Josh Stein <josh.e.stein@gmail.com>
2026-01-14 16:09:26 +0200

    New story for modal

---
+++ apps/website/src/components/courses/DropoutModal.stories.tsx
@@ -0,0 +1,50 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { TRPCError } from '@trpc/server';
+import { trpcStorybookMsw } from '../../__tests__/trpcMswSetup.browser';
+import DropoutModal from './DropoutModal';
+
+const meta = {
+  title: 'website/courses/DropoutModal',
+  component: DropoutModal,
+  parameters: {
+    layout: 'centered',
+  },
+} satisfies Meta<typeof DropoutModal>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    handleClose: () => {},
+    applicantId: 'rec123456789',
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        trpcStorybookMsw.dropout.dropoutOrDeferral.mutation(async ({ input }) => ({
+          id: 'new-dropout-id',
+          applicantId: input.applicantId,
+          reason: input.reason,
+          isDeferral: input.isDeferral,
+        })),
+      ],
+    },
+  },
+};
+
+export const Error: Story = {
+  args: {
+    handleClose: () => {},
+    applicantId: 'rec123456789',
+  },
+  parameters: {
+    msw: {
+      handlers: [
+        trpcStorybookMsw.dropout.dropoutOrDeferral.mutation(async () => {
+          throw new TRPCError({ code: 'INTERNAL_SERVER_ERROR', message: 'Failed to submit request' });
+        }),
+      ],
+    },
+  },
+};
