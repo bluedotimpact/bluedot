07bebdc5f6481a3773a78a5875cbc51ae0456e95
Martin <martinc.listwan@gmail.com>
2026-01-18 13:43:55 -0500

    [feature] Update About and Jobs page heroes to match course schedule style (#1972)
    * Update About and Jobs page heroes to match Course Schedule style
    
    * Missed lint
    
    * Fix broken tests
    
    * Fixing broken tests

--- apps/website/src/__tests__/pages/about.test.tsx
+++ apps/website/src/__tests__/pages/about.test.tsx
@@ -1,10 +1,30 @@
 import { render } from '@testing-library/react';
-import { describe, expect, test } from 'vitest';
+import {
+  beforeEach, describe, expect, Mock, test, vi,
+} from 'vitest';
+import { useRouter } from 'next/router';
 import AboutPage from '../../pages/about';
+import { TrpcProvider } from '../trpcProvider';
+import { server, trpcMsw } from '../trpcMswSetup';
+
+vi.mock('next/router', () => ({
+  useRouter: vi.fn(),
+}));
+
+const mockRouter = {
+  asPath: '/about',
+  pathname: '/about',
+  push: vi.fn(),
+};
+
+beforeEach(() => {
+  (useRouter as unknown as Mock).mockReturnValue(mockRouter);
+});
 
 describe('AboutPage', () => {
   test('should render correctly', () => {
-    const { container } = render(<AboutPage />);
+    server.use(trpcMsw.courses.getAll.query(() => []));
+    const { container } = render(<AboutPage />, { wrapper: TrpcProvider });
     expect(container).toMatchSnapshot();
   });
 });
