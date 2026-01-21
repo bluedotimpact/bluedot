e76c9b7befa74c7c3dab179011c012b65600ad76
Josh Stein <josh.e.stein@gmail.com>
2026-01-21 10:12:09 +0200

    Only allow dropouts from own course

--- apps/website/src/server/routers/dropout.ts
+++ apps/website/src/server/routers/dropout.ts
@@ -1,4 +1,5 @@
-import { dropoutTable } from '@bluedot/db';
+import { courseRegistrationTable, dropoutTable } from '@bluedot/db';
+import { TRPCError } from '@trpc/server';
 import z from 'zod';
 import db from '../../lib/api/db';
 import { protectedProcedure, router } from '../trpc';
@@ -12,8 +13,18 @@ export const dropoutRouter = router({
         isDeferral: z.boolean(),
       }),
     )
-    .mutation(async ({ input }) => {
+    .mutation(async ({ ctx, input }) => {
       const { applicantId, reason, isDeferral } = input;
+
+      const courseRegistration = await db.getFirst(courseRegistrationTable, {
+        filter: {
+          id: applicantId,
+          email: ctx.auth.email,
+        },
+      });
+      if (!courseRegistration) {
+        throw new TRPCError({ code: 'NOT_FOUND', message: 'Course registration not found' });
+      }
 
       return db.insert(dropoutTable, {
         applicantId: [applicantId],
