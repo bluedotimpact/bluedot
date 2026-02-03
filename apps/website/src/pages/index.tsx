240129938c8fb2d51a738427f68c0c9b31abe059
Will Howard <w.howard256+github@gmail.com>
2026-02-03 19:27:59 +0000

    Remove test flag (and trigger a run that should cache node_modules)

--- apps/website/src/pages/index.tsx
+++ apps/website/src/pages/index.tsx
@@ -9,9 +9,6 @@ import EventsSection from '../components/homepage/EventsSection';
 import NewsletterBanner from '../components/homepage/NewsletterBanner';
 import { trpc } from '../utils/trpc';
 
-// eslint-disable-next-line no-underscore-dangle, @typescript-eslint/no-unused-vars
-const _CACHE_TEST = false;
-
 const HomePage = () => {
   const { data: dbTestimonials } = trpc.testimonials.getCommunityMembers.useQuery();
 
