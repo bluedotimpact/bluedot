cee673dce8d6745d918f519555126be68a24b5d4
Josh Stein <josh.e.stein@gmail.com>
2026-02-06 10:44:41 +0200

    `optimistcCourseProgress` utility for optimistic updates

---
+++ apps/website/src/utils/optimisticCourseProgress.ts
@@ -0,0 +1,59 @@
+import type { inferRouterOutputs } from '@trpc/server';
+import type { trpc } from './trpc';
+import type { AppRouter } from '../server/routers/_app';
+
+type CourseProgressData = inferRouterOutputs<AppRouter>['courses']['getCourseProgress'];
+
+export async function optimisticallyUpdateCourseProgress(
+  utils: ReturnType<typeof trpc.useUtils>,
+  courseSlug: string | undefined,
+  unitId: string | undefined,
+  chunkIndex: number | undefined,
+  delta: 1 | -1,
+) {
+  if (!courseSlug) return undefined;
+
+  await utils.courses.getCourseProgress.cancel({ courseSlug });
+  const prev = utils.courses.getCourseProgress.getData({ courseSlug });
+
+  if (!prev) return undefined;
+
+  const newCompleted = prev.courseProgress.completedCount + delta;
+  utils.courses.getCourseProgress.setData(
+    { courseSlug },
+    {
+      ...prev,
+      courseProgress: {
+        ...prev.courseProgress,
+        completedCount: newCompleted,
+        percentage: Math.round((newCompleted / prev.courseProgress.totalCount) * 100),
+      },
+      chunkProgressByUnitId: {
+        ...prev.chunkProgressByUnitId,
+        ...(unitId != null && chunkIndex != null
+          ? {
+            [unitId]:
+                  prev.chunkProgressByUnitId[unitId]?.map((chunkProgress, i) => (i === chunkIndex
+                    ? {
+                      ...chunkProgress,
+                      completedCount: chunkProgress.completedCount + delta,
+                      allCompleted: chunkProgress.completedCount + delta >= chunkProgress.totalCount,
+                    }
+                    : chunkProgress)) ?? [],
+          }
+          : {}),
+      },
+    },
+  );
+  return prev;
+}
+
+export function rollbackCourseProgress(
+  utils: ReturnType<typeof trpc.useUtils>,
+  courseSlug: string | undefined,
+  previousData: CourseProgressData | undefined,
+) {
+  if (courseSlug && previousData) {
+    utils.courses.getCourseProgress.setData({ courseSlug }, previousData);
+  }
+}
