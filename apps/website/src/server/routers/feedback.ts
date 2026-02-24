e1793ce8591c00212e9aa9e7c2cedc8312902832
Josh Stein <josh.e.stein@gmail.com>
2026-02-24 15:13:31 +0200

    Skeleton feedback router

---
+++ apps/website/src/server/routers/feedback.ts
@@ -0,0 +1,20 @@
+import z from "zod";
+import { publicProcedure, router } from "../trpc";
+
+export const feedbackRouter = router({
+  submitBugReport: publicProcedure
+  .input(z.object({
+    description: z.string().min(1).max(5000),
+    email: z.string().email().optional(),
+    recordingUrl: z.string().url().optional(),
+    attachments: z.array(z.object({
+      base64: z.string(),
+      filename: z.string(),
+      mimeType: z.string(),
+    })).optional(),
+  }))
+  .query(async () => {
+    // TODO: (1) insert everything except attachments into airtable, (2) upload attachments using airtable API
+    return null;
+  }),
+});
