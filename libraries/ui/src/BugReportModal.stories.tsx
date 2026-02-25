6974bf02833b60a996be52d3a93d91c4f8a76b10
Josh Stein <josh.e.stein@gmail.com>
2026-02-25 09:22:28 +0200

    Remove useless stories

--- libraries/ui/src/BugReportModal.stories.tsx
+++ libraries/ui/src/BugReportModal.stories.tsx
@@ -36,16 +36,6 @@ export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
-  args: {},
-};
-
-export const SuccessState: Story = {
-  args: {
-    onSubmit: async () => { },
-  },
-};
-
-export const WithVideoRecordingReady: Story = {
   args: {
     onRecordScreen: () => { },
   },
