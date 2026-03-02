87359cd4704635a346192df8942e6a3ec5211418
Josh Stein <josh.e.stein@gmail.com>
2026-03-02 11:56:35 +0200

    Always show 'open discussion doc'

--- apps/website/src/components/courses/GroupDiscussionBanner.tsx
+++ apps/website/src/components/courses/GroupDiscussionBanner.tsx
@@ -158,7 +158,7 @@ const GroupDiscussionBanner: React.FC<GroupDiscussionBannerProps> = ({
       variant: 'secondary',
       url: discussionDocLink,
       target: '_blank',
-      isVisible: discussionIsSoonOrLive || isFacilitator,
+      isVisible: Boolean(discussionDocLink),
       overflowIcon: <DocumentIcon className="mx-auto" />,
     },
     {
