aaa3a48d89b4b1060dbb54f9d7b96e3e78811409
Josh Stein <josh.e.stein@gmail.com>
2026-03-11 12:28:45 +0200

    Stories for `InactiveCoursesBanner`

---
+++ apps/website/src/components/courses/InactiveCoursesBanner.stories.tsx
@@ -0,0 +1,27 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { trpcStorybookMsw } from '../../__tests__/trpcMswSetup.browser';
+import { InactiveCoursesBanner } from './InactiveCoursesBanner';
+
+const meta = {
+  title: 'website/courses/InactiveCoursesBanner',
+  component: InactiveCoursesBanner,
+  tags: ['autodocs'],
+  parameters: {
+    layout: 'padded',
+  },
+} satisfies Meta<typeof InactiveCoursesBanner>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Inactive: Story = {
+  parameters: {
+    msw: {
+      handlers: [
+        trpcStorybookMsw.meetPerson.getInactiveCourseRegistrations.query(() => [
+          { courseRegistrationId: 'rec123456789', courseSlug: 'agi-safety-fundamentals' },
+        ]),
+      ],
+    },
+  },
+};
