844697aaee4151dad607d05240dee87fdb1dee8b
Josh Stein <josh.e.stein@gmail.com>
2026-04-07 17:16:55 +0200

    set to 'virtual'

--- libraries/ui/src/Tooltip.test.tsx
+++ libraries/ui/src/Tooltip.test.tsx
@@ -24,7 +24,7 @@ describe('Tooltip', () => {
 
 describe('HoverTooltip', () => {
   afterEach(() => {
-    setInteractionModality(null);
+    setInteractionModality('virtual');
   });
 
   test('opens on hover and matches snapshot', async () => {
