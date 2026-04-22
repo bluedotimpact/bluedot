2ff3f724527c75ea4b6e3c8f326d63ecdeaac902
Josh Stein <josh.e.stein@gmail.com>
2026-04-22 15:12:47 +0200

    `InfoIcon`

--- apps/website/src/components/icons/InfoIcon.tsx
+++ apps/website/src/components/icons/InfoIcon.tsx
@@ -1,17 +1,14 @@
-export const InfoIcon = ({
-  className,
-  bgFill = '#1D4ED8',
-  fill = 'white',
-}: {
-  className?: string;
+import type { IconProps } from './types';
+
+type InfoIconProps = IconProps & {
   bgFill?: string;
-  fill?: string;
-}) => {
-  return (
-    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" className={className}>
-      <circle cx="10" cy="10" r="10" fill={bgFill} />
-      <path d="M10 14V9" stroke={fill} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
-      <circle cx="10" cy="6.5" r="0.75" fill={fill} />
-    </svg>
-  );
+  fgFill?: string;
 };
+
+export const InfoIcon = ({ size = 20, bgFill = '#1D4ED8', fgFill = 'white', ...props }: InfoIconProps) => (
+  <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="none" width={size} height={size} {...props}>
+    <circle cx="10" cy="10" r="10" fill={bgFill} />
+    <path d="M10 14V9" stroke={fgFill} strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
+    <circle cx="10" cy="6.5" r="0.75" fill={fgFill} />
+  </svg>
+);
