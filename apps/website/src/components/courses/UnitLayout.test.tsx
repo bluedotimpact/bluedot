f35969741252a45741b377abb01a9879ba760a8a
Josh Stein <josh.e.stein@gmail.com>
2026-05-06 09:34:55 +0200

    Remove import

--- apps/website/src/components/courses/UnitLayout.test.tsx
+++ apps/website/src/components/courses/UnitLayout.test.tsx
@@ -1,6 +1,5 @@
 import { render, waitFor } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { useRouter } from 'next/router';
 import {
   describe,
   expect,
