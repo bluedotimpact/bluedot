f9493416cee416764031146f359e4a0242317893
Josh Stein <josh.e.stein@gmail.com>
2026-08-10 12:39:10 +0200

    Add sentry env handling

--- apps/website/src/instrumentation-client.ts
+++ apps/website/src/instrumentation-client.ts
@@ -3,6 +3,8 @@ import * as Sentry from '@sentry/nextjs';
 Sentry.init({
   dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
 
+  environment: process.env.NEXT_PUBLIC_SENTRY_ENVIRONMENT ?? 'development',
+
   tracesSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.1,
 });
 
--- apps/website/src/sentry.server.config.ts
+++ apps/website/src/sentry.server.config.ts
@@ -4,6 +4,8 @@ import env from './lib/api/env';
 Sentry.init({
   dsn: env.SENTRY_DSN,
 
+  environment: env.SENTRY_ENVIRONMENT ?? 'development',
+
   tracesSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.1,
 
   includeLocalVariables: true,
