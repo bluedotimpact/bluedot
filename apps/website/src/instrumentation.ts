cc15aef567c963a5b03ee4b882a15f454441c9c3
Josh Stein <27491286+joshestein@users.noreply.github.com>
2026-08-10 14:18:02 +0200

    Add Sentry error capturing to website (#2864)
    * Add sentry SDK to website
    
    * SENTRY_DSN env var
    
    * update package-lock.json
    
    * Update next config
    
    * Initialise sentry SDKs
    
    * Register server-side SDK
    
    * Update global error handler
    
    * Update turbo env
    
    * Add `SENTRY_ENVIRONMENT` var
    
    * Update env template files with sentry vars
    
    * Add sentry env handling
    
    * Add SENTRY_AUTH_TOKEN to ci/cd
    
    * Update k8s with vars
    
    * Remove extra blank line
    
    * Use `tunnelRoute` to prevent ad-blockers blocking sentry
    
    * Remove `includeLocalVariables`

--- apps/website/src/instrumentation.ts
+++ apps/website/src/instrumentation.ts
@@ -1,9 +1,20 @@
+import * as Sentry from '@sentry/nextjs';
+
 export async function register() {
   // eslint-disable-next-line turbo/no-undeclared-env-vars
   if (process.env.NEXT_RUNTIME === 'nodejs') {
+    await import('./sentry.server.config');
+
     const { registerDefaultInstrumentation } = await import('@bluedot/ui/src/default-config/instrumentation');
     const { default: env } = await import('./lib/api/env');
 
     await registerDefaultInstrumentation(env);
   }
+
+  // eslint-disable-next-line turbo/no-undeclared-env-vars
+  if (process.env.NEXT_RUNTIME === 'edge') {
+    await import('./sentry.edge.config');
+  }
 }
+
+export const onRequestError = Sentry.captureRequestError;
---
+++ apps/website/src/sentry.edge.config.ts
@@ -0,0 +1,10 @@
+import * as Sentry from '@sentry/nextjs';
+import env from './lib/api/env';
+
+Sentry.init({
+  dsn: env.SENTRY_DSN,
+
+  environment: env.SENTRY_ENVIRONMENT ?? 'development',
+
+  tracesSampleRate: process.env.NODE_ENV === 'development' ? 1.0 : 0.1,
+});
