0463b6715947ca0d07928a82ce0131ade34f61ed
Josh Stein <josh.e.stein@gmail.com>
2026-09-01 17:17:06 +0200

    Remove unused react import

--- libraries/ui/src/Avatar.stories.tsx
+++ libraries/ui/src/Avatar.stories.tsx
@@ -1,7 +1,4 @@
 import type { Meta, StoryObj } from '@storybook/react';
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-import React from 'react';
-
 import { Avatar } from './Avatar';
 
 const meta = {
