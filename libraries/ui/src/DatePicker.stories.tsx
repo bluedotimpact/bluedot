2def712f80709a709683a545982b8a0b26dda991
Josh Stein <josh.e.stein@gmail.com>
2026-09-15 17:16:27 +0200

    Remove custom styles story

--- libraries/ui/src/DatePicker.stories.tsx
+++ libraries/ui/src/DatePicker.stories.tsx
@@ -59,21 +59,3 @@ export const HiddenLabel: Story = {
     label: undefined,
   },
 };
-
-export const CustomStyles: Story = {
-  args: {
-    label: 'Custom Styled Date Picker',
-    classNames: {
-      root: 'border border-bluedot-normal rounded-lg p-2',
-      label: 'text-bluedot-normal font-semibold',
-      input: 'text-bluedot-normal placeholder-bluedot-light',
-      button: 'text-bluedot-normal hover:text-bluedot-dark',
-      popover: 'ring-bluedot-normal',
-      calendar: {
-        selected: 'bg-bluedot-normal text-white',
-        today: 'text-bluedot-normal underline',
-        chevron: 'fill-bluedot-normal hover:fill-bluedot-dark',
-      },
-    },
-  },
-};
