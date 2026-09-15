f02f0826fc3c480a7330e6372cef6bc6ac48fb15
Josh Stein <josh.e.stein@gmail.com>
2026-09-15 17:08:10 +0200

    Update selected, disabled stories

--- libraries/ui/src/DatePicker.stories.tsx
+++ libraries/ui/src/DatePicker.stories.tsx
@@ -33,6 +33,21 @@ export const Default: Story = {
   },
 };
 
+export const Selected: Story = {
+  args: {
+    label: 'Pick a date',
+    value: new Date(2026, 6, 24),
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    label: 'Pick a date',
+    value: new Date(2026, 6, 24),
+    disabled: true,
+  },
+};
+
 export const CustomLabel: Story = {
   args: {
     label: 'Select Date',
