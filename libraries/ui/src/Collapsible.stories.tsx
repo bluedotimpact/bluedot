818d8290661eddfb77621c9cb9de2549b3de7fb7
Josh Stein <josh.e.stein@gmail.com>
2026-09-17 10:18:46 +0200

    Story for stacked collapsibles

--- libraries/ui/src/Collapsible.stories.tsx
+++ libraries/ui/src/Collapsible.stories.tsx
@@ -5,10 +5,8 @@ import { Collapsible } from './Collapsible';
 const meta = {
   title: 'ui/Collapsible',
   component: Collapsible,
-  // This component will have an automatically generated Autodocs entry: https://storybook.js.org/docs/writing-docs/autodocs
   tags: ['autodocs'],
   parameters: {
-    // More on how to position stories at: https://storybook.js.org/docs/configure/story-layout
     layout: 'fullscreen',
   },
   args: {},
@@ -20,8 +18,24 @@ type Story = StoryObj<typeof meta>;
 export const Default: Story = {
   args: {
     title: 'Is this collapsible?',
-    children: [
-      <p key="answer">Yes, it is!</p>,
-    ],
+    children: <p>Yes, it is!</p>,
   },
+};
+
+export const Stacked: Story = {
+  args: {
+    title: 'Introductions to AI safety',
+    children: <p>Start here if you are new to the field.</p>,
+  },
+  render: (args) => (
+    <div>
+      <Collapsible {...args} />
+      <Collapsible title="Introductions to ML engineering">
+        <p>Practical resources for building models.</p>
+      </Collapsible>
+      <Collapsible title="Podcasts and newsletters">
+        <p>Stay up to date with the field.</p>
+      </Collapsible>
+    </div>
+  ),
 };
