f195adbf2e2af98969cdd288edec86b26c71750d
Josh Stein <josh.e.stein@gmail.com>
2026-09-17 14:18:05 +0200

    `CloseIcon` conforms to existing Icon semantics

--- libraries/ui/src/icons/CloseIcon.tsx
+++ libraries/ui/src/icons/CloseIcon.tsx
@@ -1,8 +1,15 @@
-import type React from 'react';
 import { cn } from '../utils';
+import type { IconProps } from './types';
 
-export const CloseIcon: React.FC<{ size?: number; className?: string }> = ({ size = 24, className }) => (
-  <svg className={cn('fill-none stroke-2', className)} width={size} height={size} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+export const CloseIcon = ({ size = 24, className, ...props }: IconProps) => (
+  <svg
+    className={cn('fill-none stroke-2', className)}
+    width={size}
+    height={size}
+    viewBox="0 0 24 24"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
     <path d="M6 6L18 18M6 18L18 6" stroke="currentColor" strokeLinecap="round" />
   </svg>
 );
