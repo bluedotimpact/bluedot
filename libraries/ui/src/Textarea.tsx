92c11e8633aa3ae6084e56ac6aabcc52110fc064
Josh Stein <josh.e.stein@gmail.com>
2026-09-18 15:03:17 +0200

    should be `p-3 rounded-surface`

--- libraries/ui/src/Textarea.tsx
+++ libraries/ui/src/Textarea.tsx
@@ -11,7 +11,7 @@ export const Textarea: React.ForwardRefExoticComponent<TextareaProps> = forwardR
   return (
     <textarea
       className={cn(
-        'w-full px-2 py-1 border-2 border-subtle rounded-sm text-size-sm leading-6 text-primary bg-raised placeholder:text-placeholder',
+        'w-full p-3 border border-subtle rounded-surface text-size-sm leading-6 text-primary bg-raised placeholder:text-placeholder',
         'focus-visible:outline-2 focus-visible:-outline-offset-2 focus-visible:outline-focus',
         'disabled:bg-tint disabled:text-disabled disabled:cursor-not-allowed',
         className,
